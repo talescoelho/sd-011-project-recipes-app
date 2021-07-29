a2d2e363438ea310a0b303b2ecc5d4e79fb62010
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-07-29 19:15:25 -0300

    Adiciona requisito 2,3,4,5

---
+++ src/Components/Forms.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function Forms() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const btnDisabled = () => {
+    const five = 5;
+    if (/^\w+@\w+.com(.br)?$/.test(email) && password.length > five) {
+      setDisabled(false);
+    }
+  };
+
+  const changeInput = ({ target: { name, value } }) => {
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    btnDisabled();
+  };
+
+  return (
+    <>
+      <input
+        type="email"
+        name="email"
+        placeholder="email"
+        data-testid="email-input"
+        onChange={ changeInput }
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        data-testid="password-input"
+        onChange={ changeInput }
+      />
+      <button disabled={ disabled } type="button" data-testid="login-submit-btn">
+        Entrar
+      </button>
+    </>
+  );
+}
+
+export default Forms;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Forms from '../Components/Forms';
 
 function Login() {
   return (
-    <>
-
-    </>
+    <div>
+      <Forms />
+    </div>
   );
 }
 
