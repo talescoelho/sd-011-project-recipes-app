c94f6b22361a3d74835aca334c88ed51a23733a1
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-29 17:23:57 -0300

    Correcoes lint

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,49 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
 function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [btnDisable, setBtnDisable] = useState(true)
-
-  function validateLoginEntries() {
-    const numberOfCharacters = 6;
-    const regex = /\w+@\w+.com(.br)?/;
-
-    if (email !== '' && regex.test(email) && password.length > numberOfCharacters) {
-      setBtnDisable(false);
-    } else {
-      setBtnDisable(true);
-    }
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [btnDisable, setBtnDisable] = useState(true);
 
   useEffect(() => {
-    validateLoginEntries()
-  }, [email, password])
+    function validateLoginEntries() {
+      const numberOfCharacters = 6;
+      const regex = /\w+@\w+.com(.br)?/;
+
+      if (email !== '' && regex.test(email) && password.length > numberOfCharacters) {
+        setBtnDisable(false);
+      } else {
+        setBtnDisable(true);
+      }
+    }
+    validateLoginEntries();
+  }, [email, password]);
 
   return (
     <div>
-      <label>
+      <label htmlFor="email">
         Email:
         <input
           data-testid="email-input"
           type="email"
+          id="email"
           onChange={ (e) => setEmail(e.target.value) }
         />
       </label>
-      <label>
+      <label htmlFor="password">
         Senha:
         <input
           data-testid="password-input"
           type="password"
+          id="password"
           onChange={ (e) => setPassword(e.target.value) }
         />
       </label>
       <button
         disabled={ btnDisable }
         data-testid="login-submit-btn"
-        type="buttton"
+        type="button"
       >
-          Entrar
+        Entrar
       </button>
     </div>
   );
