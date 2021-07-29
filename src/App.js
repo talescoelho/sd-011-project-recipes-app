b16d2ebb0bc77b3c3c86b8a710cc02c693815f15
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-07-29 17:46:38 -0300

    Revert "Requisitos 1 a 5"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
+import rockGlass from './images/rockGlass.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="meals">
-      <Login />
+      <span className="logo">TRYBE</span>
+      <object
+        className="rocksGlass"
+        type="image/svg+xml"
+        data={ rockGlass }
+      >
+        Glass
+      </object>
     </div>
   );
 }
