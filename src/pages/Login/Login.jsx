31eb1d0ac8298422529f45c243b83b0b75844edb
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-07-29 18:18:43 -0300

    Requisito 7 feito

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -19,6 +19,7 @@ export default function Login() {
   const loginHandle = () => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
   };
 
   return (
