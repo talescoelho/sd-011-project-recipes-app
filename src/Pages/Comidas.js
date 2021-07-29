4df6d7fb994ef8041b4854c091b0be270b5a4934
Jessica Arruda <sicarruda@gmail.com>
2021-07-29 18:14:27 -0300

    requisito 9

--- src/Pages/Comidas.js
+++ src/Pages/Comidas.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Comidas() {
   return (
     <div>
-      lala
+      <Header />
     </div>
   );
 }
