244d935c44439fbd1b1f3387e2f0cc15063db734
Marco Galindo <65686699+marcoglnd@users.noreply.github.com>
2021-07-29 18:37:53 -0300

    Revert "Main group 16 req9"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './routes/Routes';
-import Provider from './context/Provider';
 
 function App() {
   return (
-    <Provider>
-      <Routes />
-    </Provider>
+    <div className="meals">
+      <span className="logo">TRYBE</span>
+      <object
+        className="rocksGlass"
+        type="image/svg+xml"
+        data={ rockGlass }
+      >
+        Glass
+      </object>
+    </div>
   );
 }
 
