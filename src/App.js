b669c64bd64b7362d5d32f20ccad94635f5082ee
YanParoni <yan.almeida.paroni@gmail.com>
2021-07-29 18:07:52 -0300

    adiciona pages explorar e bebidas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
+import Bebidas from './pages/Bebidas';
+import Explorar from './pages/Explorar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Comidas } />
+        <Route exact path="/bebidas" component={ Bebidas } />
+        <Route exact path="/explorar" component={ Explorar } />
       </Switch>
     </div>
   );
