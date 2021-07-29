d1027cb43fa09f6ef45d78bb217ea41b3114c8f7
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-29 18:54:03 -0300

    Implementa testes | Adiciona requisito 5 | Resolve ESLint

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,27 +1,55 @@
-import React /* , { useState } */ from 'react';
+import React, { useState, useEffect } from 'react';
+import credentialsVerify from '../Helpers/credentialsVerify';
 
 function Login() {
-  // const [user, setUser] = useState({
-  //   login: '',
-  //   password: '',
-  // });
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [disabled, setDisabled] = useState(true);
+
+  function handleSubmit(e) {
+    setUser({
+      ...user, [e.target.name]: e.target.value,
+    });
+  }
+
+  useEffect(() => {
+    setDisabled(credentialsVerify(user.email, user.password));
+  }, [user]);
 
   return (
     <div>
       <h1>Login</h1>
       <section>
-        <label htmlFor="login">
-          <input data-testid="email-input" id="login" type="email" placeholder="Email" />
+        <label htmlFor="email">
+          <input
+            onChange={ (e) => handleSubmit(e) }
+            name="email"
+            data-testid="email-input"
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
         </label>
         <label htmlFor="password">
           <input
+            onChange={ (e) => handleSubmit(e) }
+            name="password"
             data-testid="password-input"
             id="password"
             type="password"
             placeholder="Senha"
           />
         </label>
-        <button type="button" data-testid="login-submit-btn">Entrar</button>
+        <button
+          disabled={ disabled }
+          type="button"
+          data-testid="login-submit-btn"
+        >
+          Entrar
+        </button>
       </section>
     </div>
   );
--- src/Tests/Login.test.js
+++ src/Tests/Login.test.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 // import App from '../App';
 import Login from '../Pages/Login';
+
+const EMAIL_INPUT = 'email-input';
+const PASS_INPUT = 'password-input';
+const LOGIN_BUTTON = 'login-submit-btn';
 
 describe('Verificações da tela de Login', () => {
   it('Verifica se há dois inputs na tela de Login', () => {
     const { getByTestId } = render(<Login />);
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
-
+    const emailInput = getByTestId(EMAIL_INPUT);
+    const passwordInput = getByTestId(PASS_INPUT);
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
   it('Verifica se há um botão de login na tela de Login', () => {
     const { getByTestId } = render(<Login />);
-    const loginButton = getByTestId('login-submit-btn');
-
+    const loginButton = getByTestId(LOGIN_BUTTON);
     expect(loginButton).toBeInTheDocument();
+  });
+
+  it('Verifica se o botão desabilita quando informações errada são inseridas', () => {
+    const { getByTestId } = render(<Login />);
+
+    const emailInput = getByTestId(EMAIL_INPUT);
+    const passwordInput = getByTestId(PASS_INPUT);
+    const loginButton = getByTestId(LOGIN_BUTTON);
+
+    fireEvent.change(emailInput, { target: { value: 'grupo33' } });
+    fireEvent.change(passwordInput, { target: { value: '33' } });
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('Verifica se o botão habilita quando informações corretas são inseridas', () => {
+    const { getByTestId } = render(<Login />);
+
+    const emailInput = getByTestId(EMAIL_INPUT);
+    const passwordInput = getByTestId(PASS_INPUT);
+    const loginButton = getByTestId(LOGIN_BUTTON);
+
+    fireEvent.change(emailInput, { target: { value: 'email@betrybe.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'senhaSegura123' } });
+    expect(loginButton).not.toBeDisabled();
   });
 });
