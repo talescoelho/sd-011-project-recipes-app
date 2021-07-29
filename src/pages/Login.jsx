9a14bc3f615d7c6d4aa902336ca17ebaff8763a9
Gisele Costa <jikinha@gmail.com>
2021-07-29 20:50:15 -0300

    Requisitos 5, 6, 7 e 8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  // const checkEmailAndPass = (e, p) => {
-  //   const validEmail = /^[a-zA-Z]+@[a-zA-Z]+\.[com]{3,}$/i;
-  //   const passLength = 6;
-  //   return validEmail.test(e) && p.length >= passLength;
-  // };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkEmailAndPass = (e, p) => {
+    const validEmail = /^[a-zA-Z]+@[a-zA-Z]+\.[com]{3,}$/i;
+    const passLength = 6;
+    return validEmail.test(e) && p.length > passLength;
+  };
+
+  const setLocalStorage = (emailUser) => {
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', '1');
+    localStorage.setItem('user', JSON.stringify({ email: emailUser }));
+  };
 
   return (
     <div>
-      <label htmlFor="email-input">
-        Email:
-        <input type="email" data-testid="email-input" id="email-input" />
-      </label>
+      <input
+        type="email"
+        data-testid="email-input"
+        placeholder="Digite seu email"
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        placeholder="Digite sua senha"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
 
-      <label htmlFor="password-input">
-        Senha:
-        <input type="password" data-testid="password-input" />
-      </label>
-
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        // disabled={ email && pass ? !(checkEmailAndPass(email, pass)) : true }
-        // onClick={ () => (credentials(email)) }
-      >
-        Entrar
-      </button>
+      <Link to="/comidas">
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          value="Entrar"
+          disabled={ email && password ? !(checkEmailAndPass(email, password)) : true }
+          onClick={ () => setLocalStorage(email) }
+        />
+      </Link>
     </div>
   );
 }
