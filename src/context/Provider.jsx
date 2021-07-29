01fbd7ee3f7706cb42cca5a6129377336354342b
Ghislaine <glenox79@gmail.com>
2021-07-29 17:30:15 -0300

    requisito 02

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Context from './Context';
 
-export default function Provider() {
+export default function Provider({ children }) {
+  const [user, seUser] = useState({
+    email: '',
+    password: '',
+  });
   const myContext = {};
   return (
+
     <Context.Provider value={ myContext }>
       { children }
     </Context.Provider>
