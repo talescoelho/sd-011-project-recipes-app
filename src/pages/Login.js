85d81f1c802bc0e4ff899124ae803d4ec6378a14
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-07-29 17:49:53 -0300

    adiciona input de email

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Login() {
   return (
     <main>
-      <h1>Tela de Login</h1>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            type="email"
+            placeholder="example@example.com"
+            required
+            data-testid="email-input"
+          />
+        </label>
+      </form>
     </main>
   );
 }
