80707ab29a92520ac10e2abe6631343a60ef99df
Marco Galindo <marco1408@gmail.com>
2021-07-29 17:54:31 -0300

    criando rotas para bebidas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Bebidas from '../pages/Bebidas';
 import Comidas from '../pages/Comidas';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/comidas" component={ Comidas } />
+      <Route path="/bebidas" component={ Bebidas } />
     </Switch>
   );
 }
