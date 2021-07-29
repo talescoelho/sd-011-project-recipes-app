b20be9fe51b24b95b70ca5d4a07762a03fcbf99c
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-29 18:34:11 -0300

    Requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState(true);
+
+  function ableButton() {
+    const numMin = 6;
+    const regex = /\w+@\w+.com(.br)?/;
+    if (regex.test(email) && password.length >= numMin) {
+      setStatus(false);
+    } else {
+      setStatus(true);
+    }
+  }
+
+  function loginInput({ target }) {
+    setEmail(target.value);
+    ableButton();
+  }
+
+  function passwordInput({ target }) {
+    setPassword(target.value);
+    ableButton();
+  }
+
   return (
     <div>
-      <input type="text" data-testid="email-input" placeholder="email" />
-      <input type="password" data-testid="password-input" placeholder="password" />
-      <button data-testid="login-submit-btn" type="button">Login</button>
+      Login
+      <input
+        type="email"
+        placeholder="Email"
+        data-testid="email-input"
+        onChange={ loginInput }
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        data-testid="password-input"
+        onChange={ passwordInput }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ status }
+      >
+        Entrar
+      </button>
     </div>
   );
 }
