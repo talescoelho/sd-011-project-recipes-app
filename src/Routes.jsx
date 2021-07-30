e9dfee4e61c9ebefc88db988d297265130260b36
Matheus Costa <mathws.costag@gmail.com>
2021-07-30 16:16:55 -0300

    Torna as rotas dinamicas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Foods from './pages/Foods';
+import InitialScreen from './pages/InitialScreen';
 import Profile from './pages/Profile';
-import Cocktails from './pages/Cocktails';
 import Details from './pages/Details';
 import Explore from './pages/Explore';
-import ExploreFoods from './pages/ExploreFoods';
-import ExploreCocktails from './pages/ExploreCocktails';
+import ExploreDynamic from './pages/ExploreDynamic';
 import Ingredients from './pages/Ingredients';
 import OriginLocal from './pages/OriginLocal';
 import DoneRecipes from './pages/DoneRecipes';
@@ -17,16 +15,32 @@ export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/comidas" component={ Foods } />
+      <Route exact path="/comidas" render={ () => <InitialScreen type="Comidas" /> } />
+      <Route exact path="/bebidas" render={ () => <InitialScreen type="Bebidas" /> } />
       <Route exact path="/perfil" component={ Profile } />
-      <Route exact path="/bebidas" component={ Cocktails } />
       <Route exact path="/comidas/:id" component={ Details } />
       <Route exact path="/bebidas/:id" component={ Details } />
       <Route exact path="/explorar" component={ Explore } />
-      <Route exact path="/explorar/comidas" component={ ExploreFoods } />
-      <Route exact path="/explorar/bebidas" component={ ExploreCocktails } />
-      <Route exact path="/explorar/comidas/ingredientes" component={ Ingredients } />
-      <Route exact path="/explorar/bebidas/ingredientes" component={ Ingredients } />
+      <Route
+        exact
+        path="/explorar/comidas"
+        render={ () => (<ExploreDynamic type="Explorar Comidas" />) }
+      />
+      <Route
+        exact
+        path="/explorar/bebidas"
+        render={ () => (<ExploreDynamic type="Explorar Bebidas" />) }
+      />
+      <Route
+        exact
+        path="/explorar/comidas/ingredientes"
+        render={ () => <Ingredients type="comidas" /> }
+      />
+      <Route
+        exact
+        path="/explorar/bebidas/ingredientes"
+        render={ () => <Ingredients type="bebidas" /> }
+      />
       <Route exact path="/explorar/comidas/area" component={ OriginLocal } />
       <Route exact path="/receitas-feitas" component={ DoneRecipes } />
       <Route exact path="/receitas-favoritas" component={ FavouriteRecipes } />
--- src/pages/ExploreDynamic.jsx
+++ src/pages/ExploreDynamic.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-export default function ExploreFoods() {
-  const name = 'Explorar Comidas';
+export default function ExploreDynamic({ type }) {
   const showSearchButton = false;
   return (
     <div>
-      <Header pageName={ name } showSearchButton={ showSearchButton } />
-      ExploreFoods
+      <Header pageName={ type } showSearchButton={ showSearchButton } />
+      { type }
       <Footer />
     </div>
   );
