0008394bca5f0327fadfe8516918a829d25d41d9
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-07-30 16:31:59 -0300

    Corrigi Lint - req - 2 ao 24

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DrinksSearchBar from './DrinksSearchBar';
 import FoodsSearchBar from './FoodsSearchBar';
-import PropTypes from 'prop-types';
 
 function SearchBar({ drinks, foods }) {
   return (
