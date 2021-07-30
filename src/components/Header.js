9037730cf5353526ee618e806dc35123027f045f
Marcelo <marsleite@gmail.com>
2021-07-30 19:01:54 -0300

    requirement 12 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
@@ -7,15 +7,35 @@ import searchIcon from '../images/searchIcon.svg';
 function Header({ value }) {
   const history = useHistory();
   const { pageName, setIcon } = value;
+  const [showInput, setShowInput] = useState(false);
+
+  function isVerified() {
+    if (showInput === false) {
+      return setShowInput(true);
+    }
+    return setShowInput(false);
+  }
+
+  function hiddenInput() {
+    if (showInput) {
+      return (
+        <input type="text" data-testid="search-input" />
+      );
+    }
+  }
 
   function handleButton() {
     if (setIcon) {
       return (
-        <button
-          type="button"
-        >
-          <img data-testid="search-top-btn" src={ searchIcon } alt="buscar" />
-        </button>
+        <>
+          <button
+            type="button"
+            onClick={ isVerified }
+          >
+            <img data-testid="search-top-btn" src={ searchIcon } alt="buscar" />
+          </button>
+          { hiddenInput() }
+        </>
       );
     }
   }
