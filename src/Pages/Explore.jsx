dcc3402d7ed57809b710932371af1cac0f59645c
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-07-30 19:08:58 -0300

    Adiciona tela de Explore

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 
@@ -6,6 +7,16 @@ function Explore() {
   return (
     <div>
       <Header pageTitle="Explorar" />
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </div>);
 }
