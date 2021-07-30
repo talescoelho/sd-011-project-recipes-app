23797903fde5d28a79435739df311dee70043204
IsleneGomes <islenegomes@gmail.com>
2021-07-30 14:26:33 -0300

    Atualizando PR

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
-import Header from '../components/Header';
 
 export default function Provider({ children }) {
   const [food, setFood] = useState([]);
@@ -10,7 +9,6 @@ export default function Provider({ children }) {
   return (
     <Context.Provider value={ { drink, food, setFood, setDrink } }>
       { children }
-      <Header />
     </Context.Provider>
   );
 }
