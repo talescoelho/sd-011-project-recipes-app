b980bf675afa2a261eb6c8916a351eb1d02e5480
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-30 11:53:33 -0300

    Implementa arquivo Explore.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import React from 'react';
+
+// Pages Components \/
 import Login from './Pages/Login';
 import Foods from './Pages/Foods';
 import Drinks from './Pages/Drinks';
 import Profile from './Pages/Profile';
+import Explore from './Pages/Explore';
+// Pages Components /\
+
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,6 +21,7 @@ function App() {
         <Route exact path="/comidas" component={ Foods } />
         <Route exact path="/bebidas" component={ Drinks } />
         <Route exact path="/perfil" component={ Profile } />
+        <Route exact path="/explorar" component={ Explore } />
       </Switch>
     </BrowserRouter>
   );
