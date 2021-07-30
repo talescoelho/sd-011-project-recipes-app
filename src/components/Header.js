3258780b479eca882605c99d941b1c3b30dd5dfc
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-07-30 17:00:42 -0400

    Requisito 9, 11, 12, feito.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
-import profileIcon from '../images/profileIcon.svg'
+import profileIcon from '../images/profileIcon.svg';
 import SearchInput from './SearchInput';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchInput: false,
+    };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange() {
+    const { searchInput } = this.state;
+    if (searchInput === true) {
+      this.setState({
+        searchInput: false,
+      });
+    } else {
+      this.setState({
+        searchInput: true,
+      });
+    }
+  }
+
   render() {
-    const { title, lupa } = this.props;
+    const { title, search } = this.props;
+    const { searchInput } = this.state;
     return (
       <main>
         <Link to="/perfil">
@@ -15,15 +40,23 @@ class Header extends Component {
           </button>
         </Link>
         <span data-testid="page-title">{ title }</span>
-        { lupa === 'desligado' ? null : (
-          <button type="button">
-              <img data-testid="search-top-btn" src={ searchIcon } alt="lupa" />
+        { search === 'off' ? null : (
+          <button
+            type="button"
+            onClick={ this.handleSearchChange }
+          >
+            <img data-testid="search-top-btn" src={ searchIcon } alt="lupa" />
           </button>
         )}
-        <SearchInput />
+        {searchInput === false ? null : (<SearchInput />)}
       </main>
     );
   }
 }
 
 export default Header;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  search: PropTypes.string.isRequired,
+};
