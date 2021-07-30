502762217f119c0c7f0bf3ebd577c82205389d45
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-07-30 16:40:27 -0300

    Add placeholders

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,9 +7,9 @@ import mealIcon from '../../images/mealIcon.svg';
 const Footer = () => {
   return (
     <footer data-testid="footer">
-      <input type="image" src={drinkIcon} onClick={ <Redirect to="/bebidas"/> } data-testid="drinks-bottom-btn"></input>
-      <input type="image" src={exploreIcon} onClick={ <Redirect to="/explorar"/> } data-testid="explore-bottom-btn"></input>
-      <input type="image" src={mealIcon} onClick={ <Redirect to="/comidas"/> } data-testid="food-bottom-btn"></input>
+      <input type="image" src={drinkIcon} placeholder="Drink Icon" onClick={ <Redirect to="/bebidas"/> } data-testid="drinks-bottom-btn"></input>
+      <input type="image" src={exploreIcon} placeholder="Search Icon" onClick={ <Redirect to="/explorar"/> } data-testid="explore-bottom-btn"></input>
+      <input type="image" src={mealIcon} placeholder="Food Icon" onClick={ <Redirect to="/comidas"/> } data-testid="food-bottom-btn"></input>
     </footer>
   );
 }
