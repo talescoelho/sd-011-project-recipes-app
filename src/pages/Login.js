c43d3ee0e6dd8456cbf47dbefcd065209685934b
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-07-30 16:03:30 -0300

    adiciona validacao do formulario de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
+const minPasswordLength = 6;
+
+// Email Regex Source: http://jsfiddle.net/ghvj4gy9/
+// eslint-disable-next-line max-len
+const username = '^(([^<>()[\\]\\.,;:\\s@"]+(\\.[^<>()[\\]\\.,;:\\s@"]+)*)|(".+"))';
+const domainName = '@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}';
+const domain = '\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$';
+const EMAIL_REGEX = new RegExp([username, domainName, domain].join(''));
+
 function Login() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [isFormValid, setIsFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    const formValidation = () => {
+      const isValid = EMAIL_REGEX.test(email) && password.length > minPasswordLength;
+      setIsFormValid(isValid);
+    };
+
+    formValidation();
+  }, [email, password]);
+
   return (
     <main>
       <h1>Login</h1>
@@ -11,6 +33,8 @@ function Login() {
             id="email"
             type="email"
             placeholder="example@example.com"
+            value={ email }
+            onChange={ ({ target: { value } }) => setEmail(value) }
             required
             data-testid="email-input"
           />
@@ -21,11 +45,19 @@ function Login() {
             id="password"
             type="password"
             placeholder="Senha"
+            value={ password }
+            onChange={ ({ target: { value } }) => setPassword(value) }
             required
             data-testid="password-input"
           />
         </label>
-        <button type="button" data-testid="login-submit-btn">Entrar</button>
+        <button
+          type="button"
+          disabled={ !isFormValid }
+          data-testid="login-submit-btn"
+        >
+          Entrar
+        </button>
       </form>
     </main>
   );
