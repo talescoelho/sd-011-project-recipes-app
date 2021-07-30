16c4fd3a28274f146e3d37a83272ff293cdb43cf
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-30 13:07:09 -0300

    prepare conditions to redirect

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { fetchRecipesAPIAction } from '../redux/actions';
 
-function SearchBar({ recipeType, fetch }) {
+function SearchBar({ recipesData, recipeType, fetch }) {
   const [searchInput, setSearchInput] = useState('');
   const [radioInput, setRadioInput] = useState('');
 
@@ -26,51 +27,62 @@ function SearchBar({ recipeType, fetch }) {
   };
 
   return (
-    <form>
-      Search Bar
-      <label htmlFor="search-input">
-        <input
-          placeholder="Buscar Receita"
-          data-testid="search-input"
-          onChange={ ({ target: { value } }) => setSearchInput(value) }
-        />
-      </label>
-      <label htmlFor="radio-options">
-        Ingrediente
-        <input
-          name="radio-options"
-          type="radio"
-          data-testid="ingredient-search-radio"
-          onChange={ () => setRadioInput('ingredient') }
-        />
-        Nome
-        <input
-          name="radio-options"
-          type="radio"
-          data-testid="name-search-radio"
-          onChange={ () => setRadioInput('name') }
-        />
-        Primeira letra
-        <input
-          name="radio-options"
-          type="radio"
-          data-testid="first-letter-search-radio"
-          onChange={ () => setRadioInput('firstLetter') }
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="exec-search-btn"
-        onClick={ handleClick }
-      >
-        Buscar
-      </button>
-    </form>
+    <div>
+      {}
+      <form>
+        Search Bar
+        <label htmlFor="search-input">
+          <input
+            placeholder="Buscar Receita"
+            data-testid="search-input"
+            onChange={ ({ target: { value } }) => setSearchInput(value) }
+          />
+        </label>
+        <label htmlFor="radio-options">
+          Ingrediente
+          <input
+            name="radio-options"
+            type="radio"
+            data-testid="ingredient-search-radio"
+            onChange={ () => setRadioInput('ingredient') }
+          />
+          Nome
+          <input
+            name="radio-options"
+            type="radio"
+            data-testid="name-search-radio"
+            onChange={ () => setRadioInput('name') }
+          />
+          Primeira letra
+          <input
+            name="radio-options"
+            type="radio"
+            data-testid="first-letter-search-radio"
+            onChange={ () => setRadioInput('firstLetter') }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ handleClick }
+        >
+          Buscar
+        </button>
+        {recipesData[recipeType]
+        && recipesData[recipeType].length === 1
+        && console.log('só uma receita')}
+
+        {recipesData[recipeType]
+        && recipesData[recipeType].length > 1
+        && console.log('mais de uma receita')}
+      </form>
+    </div>
   );
 }
 
 const mapStateToProps = (state) => ({
   recipeType: state.RecipesReducer.recipeType,
+  recipesData: state.RecipesReducer.recipesData,
 });
 
 const mapDispatchToProps = (dispatch) => ({
