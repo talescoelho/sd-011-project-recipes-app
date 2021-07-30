23ed5bd9060cae4030543357e0f81e0a3a1fef87
Arlesson Moura <arlessonmss@gmail.com>
2021-07-30 16:50:30 -0300

    Fix: requirements 9 to 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,89 +1,82 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
       disabled: true,
-      searchBar: '',
-    }
+    };
     this.centralHeader = this.centralHeader.bind(this);
     this.profileButton = this.profileButton.bind(this);
     this.searchButton = this.searchButton.bind(this);
-    this.renderProfile = this.renderProfile.bind(this);
     this.showSearch = this.showSearch.bind(this);
-    this.handleState = this.handleState.bind(this);
-  }
-
-  handleState({ target }) {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  renderProfile() {
-    return (<Redirect to="/perfil" />);
-  }
-
-  showSearch() {
-    const { disabled } = this.state;
-    if (disabled = true) {
-      this.setState({
-        disabled: false,
-      });
-    } else {
-      this.setState({
-        disabled: true,
-      });
-    }
-  }
+  };
 
   profileButton() {
     return (
-      <button
-      type="button"
-      data-testid="profile-top-btn"
-      onClick={ () => this.renderProfile() }
-      >
-      <img src="./images/profileIcon.svg" alt="profileIcon" />
-      </button>
+      <Link to="/perfil">
+        <button
+          type="button"
+          data-testid="profile-top-btn"
+        >
+          <img src="./images/profileIcon.svg" alt="profileIcon" />
+        </button>
+      </Link>
     );
   }
 
   searchButton() {
     return (
       <button
-      type="button"
-      data-testids="search-top-btn"
-      onClick={ () => this.showSearch() }
+        type="button"
+        data-testids="search-top-btn"
+        onClick={ () => this.showSearch() }
       >
-      <img src="./images/searchIcon.svg" alt="searchIcon" />
+        <img src="./images/searchIcon.svg" alt="searchIcon" />
       </button>
     );
   }
 
+  showSearch() {
+    const { disabled } = this.state;
+      if (disabled === true) {
+        this.setState({
+          disabled: false,
+        });
+      } else {
+        this.setState({
+          disabled: true,
+        });
+      }
+    }
+
+
   centralHeader() {
-    const { disabled, searchBar } = this.state;
+const { disabled } = this.state;
+const { title } = this.props;
     return (
       <>
-        <h1 data-testid="page-title" >Cuisine Disgrâce!</h1>
-        <input type="text" value={ searchBar }  name="searchBar" disabled={ disabled } />
+        <h1 data-testid="page-title">{ title }</h1>
+        { !disabled ? <SearchBar /> : null  }
       </>
     );
   }
 
   render() {
-  return (
-    <header>
-      { this.profileButton() }
-      { this.centralHeader() }
-      { this.searchButton() }
-    </header>
-  );
+    return (
+      <header>
+        { this.profileButton() }
+        { this.centralHeader() }
+        { this.searchButton() }
+      </header>
+    );
+  }
 }
-}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}.isRequired;
 
 export default Header;
