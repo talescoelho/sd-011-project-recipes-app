a907d3be0675d7950bdfb5742501351bb9fe3d45
Pedro Castro <pa.coelho.castro@gmail.com>
2021-07-30 17:36:54 -0300

    Group-17-explore

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import '../App.css';
 import Header from '../components/Header';
@@ -8,7 +9,28 @@ class Explorar extends Component {
     return (
       <div>
         <Header title="Explorar" />
-        Explorar
+        <div>
+          <Link
+            to="explorar/comidas"
+          >
+            <button
+              type="button"
+              data-testid="explore-food"
+            >
+              Explorar Comidas
+            </button>
+          </Link>
+          <Link
+            to="explorar/bebidas"
+          >
+            <button
+              type="button"
+              data-testid="explore-drinks"
+            >
+              Explorar Bebidas
+            </button>
+          </Link>
+        </div>
         <Footer />
       </div>
     );
