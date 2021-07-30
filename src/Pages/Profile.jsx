c0f003500e9e09f12adb848b35b481486d86f198
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-07-30 16:28:39 -0300

    Adiciona footer

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 function Profile() {
   return (
     <div>
       <Header pageTitle="Perfil" />
+      <Footer />
     </div>);
 }
 
