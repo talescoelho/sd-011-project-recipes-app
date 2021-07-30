cc6c8c53f043ba982ae2b0f8725ab8321689bce1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-07-30 16:46:40 -0300

    adiciona testes do requisito 05

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,23 +2,32 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Login from '../pages/Login';
 
+const emailInputTestId = 'email-input';
+const passwordInputTestId = 'password-input';
+const submitButtonButtonTestId = 'login-submit-btn';
+
+const invalidPassword = 'senha';
+const validPassword = '1234567';
+const invalidEmail = 'email';
+const validEmail = 'email@email.com';
+
 describe('Requirement 02', () => {
   it('should have an email input', () => {
     render(<Login />);
 
-    expect(screen.getByTestId('email-input')).toBeInTheDocument();
+    expect(screen.getByTestId(emailInputTestId)).toBeInTheDocument();
   });
 
   it('should have an password input', () => {
     render(<Login />);
 
-    expect(screen.getByTestId('password-input')).toBeInTheDocument();
+    expect(screen.getByTestId(passwordInputTestId)).toBeInTheDocument();
   });
 
   it('should have a button with text "Entrar"', () => {
     render(<Login />);
 
-    const submitButton = screen.getByTestId('login-submit-btn');
+    const submitButton = screen.getByTestId(submitButtonButtonTestId);
 
     expect(submitButton).toBeInTheDocument();
     expect(submitButton).toHaveTextContent('Entrar');
@@ -28,23 +37,68 @@ describe('Requirement 02', () => {
 describe('Requirement 03', () => {
   it('should be able to type an email', () => {
     render(<Login />);
-    const inputEmail = screen.getByTestId('email-input');
+    const inputEmail = screen.getByTestId(emailInputTestId);
     fireEvent.change(inputEmail, { target: {
-      value: 'email@email.com',
-      type: 'email',
+      value: validEmail,
     } });
-    expect(inputEmail.value).toBe('email@email.com');
+    expect(inputEmail.value).toBe(validEmail);
   });
 });
 
 describe('Requirement 04', () => {
   it('should be able to type a password', () => {
     render(<Login />);
-    const inputPassword = screen.getByTestId('password-input');
+    const inputPassword = screen.getByTestId(passwordInputTestId);
     fireEvent.change(inputPassword, { target: {
-      value: '1234567',
-      type: 'password',
+      value: validPassword,
     } });
-    expect(inputPassword.value).toBe('1234567');
+    expect(inputPassword.value).toBe(validPassword);
+  });
+});
+
+describe('Requirement 05', () => {
+  it('should be disabled if email is invalid', () => {
+    render(<Login />);
+    const inputEmail = screen.getByTestId(emailInputTestId);
+    fireEvent.change(inputEmail, { target: {
+      value: invalidEmail,
+    } });
+    const inputPassword = screen.getByTestId(passwordInputTestId);
+    fireEvent.change(inputPassword, { target: {
+      value: validPassword,
+    } });
+
+    const submitButton = screen.getByTestId(submitButtonButtonTestId);
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should be disabled if password is invalid', () => {
+    render(<Login />);
+    const inputEmail = screen.getByTestId(emailInputTestId);
+    fireEvent.change(inputEmail, { target: {
+      value: validEmail,
+    } });
+    const inputPassword = screen.getByTestId(passwordInputTestId);
+    fireEvent.change(inputPassword, { target: {
+      value: invalidPassword,
+    } });
+
+    const submitButton = screen.getByTestId(submitButtonButtonTestId);
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should be enabled if email and password are valid', () => {
+    render(<Login />);
+    const inputEmail = screen.getByTestId(emailInputTestId);
+    fireEvent.change(inputEmail, { target: {
+      value: validEmail,
+    } });
+    const inputPassword = screen.getByTestId(passwordInputTestId);
+    fireEvent.change(inputPassword, { target: {
+      value: validPassword,
+    } });
+
+    const submitButton = screen.getByTestId(submitButtonButtonTestId);
+    expect(submitButton).not.toBeDisabled();
   });
 });
