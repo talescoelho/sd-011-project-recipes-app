2663b9d3a1423130964e25f873fe5edc8eaad140
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-07-30 11:29:34 -0300

    req barra de busca

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import { LoginPage, Home, NotFound } from './pages';
+import ProviderSearch from './ProviderSearch';
 
 function App() {
   return (
-    <main>
-      <Switch>
-        <Route exact path="/" render={ (props) => <LoginPage { ...props } /> } />
-        <Route exact path="/home" render={ (props) => <Home { ...props } /> } />
-        <Route component={ NotFound } />
-      </Switch>
-    </main>
+    <ProviderSearch>
+      <main>
+        <Switch>
+          <Route exact path="/" render={ (props) => <LoginPage { ...props } /> } />
+          <Route exact path="/home" render={ (props) => <Home { ...props } /> } />
+          <Route component={ NotFound } />
+        </Switch>
+      </main>
+    </ProviderSearch>
   );
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 export default function LoginPage() {
   return (
     <div>
       <h1> Hello, This is my login Page</h1>
+      <SearchBar />
     </div>
   );
 }
