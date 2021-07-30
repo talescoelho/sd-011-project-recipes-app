932d6f20f7a8bb23ce12d5ee9ad315141bd9d299
lucasSCsantos <luc.cristovam10@gmail.com>
2021-07-30 18:14:09 -0300

    teste com withReduxAndRouter

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import SearchBar from '../components/SearchBar';
-import recipes from '../reducers';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import rootReducers from '../reducers';
 
 const THREE = 3;
 
-const renderWithRedux = (
-  component,
-  { initialState, store = createStore(combineReducers({ recipes }), initialState) } = {},
-) => (
-  {
-    ...render(<Provider store={ store }>{component}</Provider>),
-    store,
-  }
-);
+const renderWithReduxAndRouter = (component, {
+  initialState = {},
+  store = createStore(rootReducers, initialState),
+  initialEntries = ['/'],
+  history = createMemoryHistory({ initialEntries }),
+} = {}) => ({
+  ...render(
+    <Router history={history}>
+      <Provider store={store}>
+        {component}
+      </Provider>,
+    </Router>,
+  ),
+  store,
+  history,
+});
 
 describe('Testa o component SearchBar', () => {
-  const { getByTestId } = renderWithRedux(<SearchBar />);
+  const { getByTestId } = renderWithReduxAndRouter(<SearchBar />);
   const inputBar = getByTestId('search-input');
   const nameRadio = getByTestId('ingredient-search-radio');
   const firstLetterRadio = getByTestId('name-search-radio');
@@ -47,7 +56,7 @@ describe('Testa o component SearchBar', () => {
   });
 
   test('Verifique se existem 3 radio buttons', () => {
-    const { getAllByRole } = renderWithRedux(<SearchBar />);
+    const { getAllByRole } = renderWithReduxAndRouter(<SearchBar />);
     const radios = getAllByRole('radio');
     expect(radios).toHaveLength(THREE);
   });
