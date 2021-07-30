e472d12b6c098bcc7e0f18853337770ae31b9e7f
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-07-30 16:19:02 -0300

    Add data-test ids for the elements in footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer data-testid="footer">
+      <button data-testid="drinks-bottom-btn"></button>
+      <button data-testid="explore-bottom-btn"></button>
+      <button data-testid="food-bottom-btn"></button>
+    </footer>
+  );
+}
+
+export default Footer;
