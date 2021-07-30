577c666021ebe6ad311652fe1212be918a8f64b5
heyset <c.inaciomatheus@gmail.com>
2021-07-30 17:40:31 -0300

    Adiciona validacao

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,17 @@ function Login() {
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
 
+  function validateField(field, type) {
+    const MIN_PASSWORD_LENGTH = 7;
+
+    const validators = {
+      email: (value) => /(.+)@(.+)\.(.+)/.test(value),
+      password: (value) => value.length >= MIN_PASSWORD_LENGTH,
+    };
+
+    return validators[type](field);
+  }
+
   return (
     <Layout>
       <main>
@@ -26,6 +37,10 @@ function Login() {
           <button
             data-testid="login-submit-btn"
             type="submit"
+            disabled={
+              !validateField(emailInput, 'email')
+              || !validateField(passwordInput, 'password')
+            }
           >
             Entrar
           </button>
