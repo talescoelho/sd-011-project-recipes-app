b8cebe80b0fbbda3ed4b78731a2e472fccef13ba
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-07-30 17:09:47 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
-import MenuInferior from './components/MenuInferior';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/bebidas" />
-        <Route path="/comidas" />
-        <Route path="/explorar" />
       </Switch>
-      <MenuInferior />
     </BrowserRouter>
   );
 }
