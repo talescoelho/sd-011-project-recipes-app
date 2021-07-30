eb09d32d853d4210f0d4bb3f32ef2f61a83926c4
Matheus Costa <mathws.costag@gmail.com>
2021-07-30 01:37:53 -0300

    Cria testes para o localStorage

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -12,16 +12,21 @@ describe('Testes da tela de login', () => {
   it('A rota inicial é a tela de login', () => {
     const { history, getByText } = renderWithReduxAndRouter(<Routes />);
     const { pathname } = history.location;
+
     expect(pathname).toBe('/');
+
     const loginText = getByText(/Login/i);
+
     expect(loginText).toBeInTheDocument();
   });
-  it('É possui digitar o email e a senha', () => {
+  it('É possível digitar o email e a senha', () => {
     const { getByTestId } = renderWithReduxAndRouter(<Routes />);
     const passwordInput = getByTestId('password-input');
     const emailInput = getByTestId('email-input');
+
     fireEvent.change(emailInput, { target: { value: invalidEmail } });
     fireEvent.change(passwordInput, { target: { value: validPassword } });
+
     expect(emailInput).toHaveValue(invalidEmail);
     expect(passwordInput).toHaveValue(validPassword);
   });
@@ -30,17 +35,26 @@ describe('Testes da tela de login', () => {
     const passwordInput = getByTestId('password-input');
     const emailInput = getByTestId('email-input');
     const loginButton = getByTestId('login-submit-btn');
+
     expect(loginButton).toBeDisabled();
+
     fireEvent.change(emailInput, { target: { value: invalidEmail } });
     fireEvent.change(passwordInput, { target: { value: validPassword } });
+
     expect(loginButton).toBeDisabled();
+
     fireEvent.change(emailInput, { target: { value: validEmail } });
     fireEvent.change(passwordInput, { target: { value: invalidPassword } });
+
     expect(loginButton).toBeDisabled();
+
     fireEvent.change(emailInput, { target: { value: validEmail } });
     fireEvent.change(passwordInput, { target: { value: validPassword } });
+
     expect(loginButton).not.toBeDisabled();
+
     fireEvent.click(loginButton);
+
     expect(history.location.pathname).toBe('/comidas');
     expect(localStorage.user).toBe(JSON.stringify({ email: validEmail }));
     expect(localStorage.cocktailsToken).toBe('1');
