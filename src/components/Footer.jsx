b4343eba8d4ab049674b7ded6f218473b6fe14bc
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-07-29 22:04:21 -0300

    Mocking features

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <Link to="/">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Icon Dring"
+        />
+      </Link>
+      <Link to="/">
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="Icon Explore"
+        />
+      </Link>
+      <Link to="/">
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="Icon Meal"
+        />
+      </Link>
+
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+function MainPage() {
+  return (
+    <Footer />
+  );
+}
+
+export default MainPage;
