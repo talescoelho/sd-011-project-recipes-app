6036ed9957e45911d94939dd75242350d07d63fa
Gustavo Lemes <gustavolemes997@gmail.com>
2021-07-30 17:04:09 -0300

    Requisito 12 e 13

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <section>
+      <input data-testid="search-input" type="text" placeholder="Buscar Receita" />
+      <label htmlFor="">
+        Ingrediente
+        <input data-testid="ingredient-search-radio" type="radio" />
+      </label>
+      <label htmlFor="">
+        Nome
+        <input data-testid="name-search-radio" type="radio" />
+      </label>   
+      <label htmlFor="">
+        Primeira Letra
+        <input data-testid="first-letter-search-radio" type="radio" />
+      </label>
+      <button data-testid="exec-search-btn" type="button">Buscar</button>
+    </section>
+  );
+}
+
+export default SearchBar;
