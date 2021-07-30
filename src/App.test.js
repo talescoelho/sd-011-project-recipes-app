1ac86f27c639ec83690811d0fb825829187e17c0
Marco Galindo <marco1408@gmail.com>
2021-07-30 20:08:46 -0300

    importando fire event

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import Login from './pages/Login';
 import userEvent from '@testing-library/user-event';
