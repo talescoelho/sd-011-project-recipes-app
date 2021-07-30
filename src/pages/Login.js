e96af06197a923f9d30de939be00c9b47774b1a4
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-30 17:55:25 -0300

    create the form for the login elements

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      valid: true,
+    };
+    this.handleChangeForm = this.handleChangeForm.bind(this);
+  }
+
+  
+  handleChangeForm({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.validateLogin();
+  }
+
+
+  validateLogin() {
+    const { email, password } = this.state;
+    const minSize = 5;
+    const re = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+    if (password.length >= minSize && re.test(email)) {
+      this.setState({
+        valid: false,
+      });
+    } else {
+      this.setState({
+        valid: true,
+      });
+    }
+  }
+  
+  renderLoginForm() {
+    const { email, password, valid } = this.state;
+    return (
+      <form>
+        <div className="input">
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChangeForm }
+          />
+        </div>
+        <div className="input">
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua Senha"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChangeForm }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-btn"
+            disabled={ valid }
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
+    );
+  }
+
   render() {
     return (
       <div>
-        Login
+        <h1>Login</h1>
+        {this.renderLoginForm()}
       </div>
     );
   }
