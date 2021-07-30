8f26db9a3c1dbba7ccad682ad010d4bfabcef997
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-30 15:41:10 -0300

    configure redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import foodReducers from './foodReducer';
+
+
+const reducer = combineReducers({foodReducers});
+
+export default reducer;
