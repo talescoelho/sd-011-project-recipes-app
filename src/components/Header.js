698e7264713ab02c36bb1ae7bcee36d9e658bfb8
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-07-30 09:26:15 -0300

    Header - Add requisito 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import { Link } from 'react-router-dom';
 
 function Header({ title, icon }) {
   const renderSearchButtonIcon = () => (
-    <button
-      type="button"
-    >
-      <img src={ searchIcon } alt="icone de uma lupa" data-testid="search-top-btn" />
-    </button>
+      <button
+        type="button"
+      >
+        <img src={ searchIcon } alt="icone de uma lupa" data-testid="search-top-btn" />
+      </button>
   );
 
   return (
     <header>
-      <button
-        type="button"
-      >
-        <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
-      </button>
+      <Link to="/perfil">
+        <button
+          type="button"
+        >
+          <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+        </button>
+      </Link>
       <h3 data-testid="page-title">{`${title}`}</h3>
       {icon && renderSearchButtonIcon()}
     </header>
