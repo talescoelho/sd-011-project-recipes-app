8c37298c01097aa05f57afccdb7b85d20279ff26
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-07-30 14:11:03 -0300

    Fixed errors

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Forms() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [disabled, setDisabled] = useState(true)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const btnDisabled = () => {
-    const five = 5
+    const five = 5;
     if (/^\w+@\w+.com(.br)?$/.test(email) && password.length > five) {
-      setDisabled(false)
+      setDisabled(false);
     }
-  }
+  };
 
   const changeInput = ({ target: { name, value } }) => {
-    if (name === 'email') setEmail(value)
-    if (name === 'password') setPassword(value)
-    btnDisabled()
-  }
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    btnDisabled();
+  };
 
   const buttonClick = () => {
-    localStorage.setItem('mealsToken', 1)
-    localStorage.setItem('cocktailsToken', 1)
-    localStorage.setItem('user', JSON.stringify({ email }))
-  }
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
 
   return (
     <>
@@ -32,19 +32,19 @@ function Forms() {
         name="email"
         placeholder="email"
         data-testid="email-input"
-        onChange={changeInput}
+        onChange={ changeInput }
       />
       <input
         type="password"
         name="password"
         placeholder="password"
         data-testid="password-input"
-        onChange={changeInput}
+        onChange={ changeInput }
       />
       <Link to="/comidas">
         <button
-          disabled={disabled}
-          onClick={buttonClick}
+          disabled={ disabled }
+          onClick={ buttonClick }
           type="button"
           data-testid="login-submit-btn"
         >
@@ -52,7 +52,7 @@ function Forms() {
         </button>
       </Link>
     </>
-  )
+  );
 }
 
-export default Forms
+export default Forms;
