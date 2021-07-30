5ea7dc56e48c1861615364bc36ddea5d70123561
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-30 17:49:25 -0300

    Configuraçoes gerais pagina.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Login} />
+        {/* <Route path="/comidas" component={} />
+        <Route path="/comidas:id-" component={} />
+        <Route path="/bebidas" component={} />
+        <Route path="/bebidas:id" component={} />
+        <Route path="/explorar" component={} />
+        <Route path="/explorar/comidas" component={} />
+        <Route path="/explorar/bebidas" component={} />
+        <Route path="/explorar/comidas/ingredientes" component={} />
+        <Route path="/explorar/bebidas/ingredientes" component={} />
+
+        <Route path="/explorar/comidas/area" component={} />
+        <Route path="/perfil" component={} />
+        <Route path="/receitas-feitas" component={} />
+        <Route path="/receitas-favoritas" component={} />
+        <Route path="*" component={NotFound} /> */}
+      </Switch>
+    </BrowserRouter>
+  
   );
 }
 
