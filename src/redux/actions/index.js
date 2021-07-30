dce40da071514b11b928aaaba4426e4c954b90cd
Juliana Happatsch <jhappatsch@gmail.com>
2021-07-30 14:45:13 -0300

    Página Login completa, sem testes

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,13 +1,7 @@
 // actions a serem alteradas conforme necessidade. (exportadas apenas como exemplo)
-const NEW_ACTION = 'NEW_ACTION';
-const OTHER_NEW_ACTION = 'OTHER_NEW_ACTION';
+export const ADD_EMAIL = 'ADD_EMAIL';
 
-export const newAction = (state) => ({
-  type: NEW_ACTION,
-  state,
-});
-
-export const otherNewAction = (state) => ({
-  type: OTHER_NEW_ACTION,
-  state,
+export const addEmail = (email) => ({
+  type: ADD_EMAIL,
+  email,
 });
