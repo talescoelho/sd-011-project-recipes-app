1056ac2e74dbcd72c1f59c97b1cfea9101acac61
Luiz Otavio <luiz.otavio273@gmail.com>
2021-07-30 15:42:55 -0300

    req10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Perfil from './components/Perfil';
 
 function App() {
   return (
-    <div className="meals">
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/perfil" component={ Perfil } />
+      </Switch>
       <Header />
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
   return (
     <header>
-      <img
-        src={ profileIcon }
-        alt="user"
-        data-testid="profile-top-btn"
-      />
+      <Link to="/perfil">
+        <img
+          src={ profileIcon }
+          alt="user"
+          data-testid="profile-top-btn"
+        />
+      </Link>
       <h2 data-testid="page-title">
         Comida
       </h2>
