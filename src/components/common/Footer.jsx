396dbff0bb47a9dd80363cb07b5e380d204b8f8f
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-07-30 16:30:53 -0300

    Add events that redirect to other pages in the inputs

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
@@ -6,9 +7,9 @@ import mealIcon from '../../images/mealIcon.svg';
 const Footer = () => {
   return (
     <footer data-testid="footer">
-      <input type="image" src={drinkIcon} data-testid="drinks-bottom-btn"></input>
-      <input type="image" src={exploreIcon} data-testid="explore-bottom-btn"></input>
-      <input type="image" src={mealIcon} data-testid="food-bottom-btn"></input>
+      <input type="image" src={drinkIcon} onClick={ <Redirect to="/bebidas"/> } data-testid="drinks-bottom-btn"></input>
+      <input type="image" src={exploreIcon} onClick={ <Redirect to="/explorar"/> } data-testid="explore-bottom-btn"></input>
+      <input type="image" src={mealIcon} onClick={ <Redirect to="/comidas"/> } data-testid="food-bottom-btn"></input>
     </footer>
   );
 }
