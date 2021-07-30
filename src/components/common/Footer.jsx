48795b8687dda96be59d66c262d6eea2f434b61e
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-07-30 17:21:36 -0300

    Corrects lint problems - extension was not working properly

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,14 +4,27 @@ import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
-const Footer = () => {
-  return (
-    <footer data-testid="footer">
-      <Link to="/bebidas" data-testid="drinks-bottom-btn"><img src={drinkIcon} alt="Drink Icon"></img></Link>
-      <Link to="/explorar"  data-testid="explore-bottom-btn"><img src={exploreIcon} alt="Search Icon"></img></Link>
-      <Link to="/comidas" data-testid="food-bottom-btn"><img src={mealIcon} alt="Food Icon"></img></Link>
-    </footer>
-  );
-}
+const Footer = () => (
+  <footer data-testid="footer">
+    <Link
+      to="/bebidas"
+      data-testid="drinks-bottom-btn"
+    >
+      <img src={ drinkIcon } alt="Drink Icon" />
+    </Link>
+    <Link
+      to="/explorar"
+      data-testid="explore-bottom-btn"
+    >
+      <img src={ exploreIcon } alt="Search Icon" />
+    </Link>
+    <Link
+      to="/comidas"
+      data-testid="food-bottom-btn"
+    >
+      <img src={ mealIcon } alt="Food Icon" />
+    </Link>
+  </footer>
+);
 
 export default Footer;
