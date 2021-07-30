c6be426fe2ddf14b59167adb00f8dd3d71a44ff6
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-07-30 11:22:43 -0300

    Menu Inferior - Req 19

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer data-testid="footer">
+      <p>Menu inferior</p>
+      <Link to="/bebidas" data-testid="drinks-bottom-btn">Drinks</Link>
+      <br />
+      <Link to="/explorar" data-testid="explore-bottom-btn">Explorar</Link>
+      <br />
+      <Link to="/comidas" data-testid="food-bottom-btn">Foods</Link>
+      <br />
+    </footer>
+  );
 }
 
 export default Footer;
