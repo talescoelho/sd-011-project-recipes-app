e607923e5e8af2e73584f836589c9a5f5aaca6a5
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-07-30 12:22:39 -0300

    requisito 14 completo

--- src/components/FoodsSearchBar.js
+++ src/components/FoodsSearchBar.js
@@ -5,6 +5,22 @@ function FoodsSearchBar() {
   console.log(searchText);
   const [input, setInput] = useState('');
 
+  function conditionEndpoint(text) {
+    let endpoint;
+    if (input === 'ingredient') {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`;
+    } else if (input === 'name') {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`;
+    } else if (input === 'firstLetter' && searchText.length > 1) {
+      return alert('Sua busca deve conter somente 1 (um) caracter');
+    } else if (input === 'firstLetter') {
+      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${text}`;
+    } else if (!input) return null;
+    return fetch(endpoint)
+      .then((response) => response.json())
+      .then((results) => console.log(results));
+  }
+
   return (
     <div>
       <input
@@ -13,7 +29,6 @@ function FoodsSearchBar() {
         onChange={ ({ target: { value } }) => setSearchText(value) }
       />
       <label htmlFor="ingredient">
-        Ingredientes
         <input
           type="radio"
           name="search-foods"
@@ -22,10 +37,10 @@ function FoodsSearchBar() {
           data-testid="ingredient-search-radio"
           onChange={ ({ target: { value } }) => setInput(value) }
         />
+        Ingredientes
       </label>
 
       <label htmlFor="name">
-        Nome
         <input
           type="radio"
           name="search-foods"
@@ -34,10 +49,10 @@ function FoodsSearchBar() {
           data-testid="name-search-radio"
           onChange={ ({ target: { value } }) => setInput(value) }
         />
+        Nome
       </label>
 
       <label htmlFor="firstLetter">
-        Primeira letra
         <input
           type="radio"
           name="search-foods"
@@ -46,11 +61,13 @@ function FoodsSearchBar() {
           data-testid="first-letter-search-radio"
           onChange={ ({ target: { value } }) => setInput(value) }
         />
+        Primeira letra
       </label>
       <br />
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ () => conditionEndpoint(searchText) }
       >
         Buscar
       </button>
