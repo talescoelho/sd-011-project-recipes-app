6028a6d4dc5ff3c725250e32db9d2116aca65e9d
Daniel Leon <daniel.radialista@gmail.com>
2021-07-30 18:47:09 -0300

    trabalhando nos requisitos da tela perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+// import RecipeAppContext from '../provider/RecipeAppProvider';
 
 function Perfil() {
+  // const [setLogin] = useContext(RecipeAppContext);
+
   return (
     <div>
       <h1>My Perfil Page</h1>
       <Header title="Perfil" />
+      <p data-testid="profile-email">Email</p>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
 
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
