3595b9bb278e1a749dad69a53c6bf22b941ba019
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-07-30 17:46:40 -0300

    Requisito 21 realizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
-import profileIcon from '../images/profileIcon.svg'
+import profileIcon from '../images/profileIcon.svg';
 import SearchInput from './SearchInput';
 
 class Header extends Component {
@@ -17,7 +18,7 @@ class Header extends Component {
         <span data-testid="page-title">{ title }</span>
         { lupa === 'desligado' ? null : (
           <button type="button">
-              <img data-testid="search-top-btn" src={ searchIcon } alt="lupa" />
+            <img data-testid="search-top-btn" src={ searchIcon } alt="lupa" />
           </button>
         )}
         <SearchInput />
@@ -25,5 +26,10 @@ class Header extends Component {
     );
   }
 }
+
+Header.propTypes = {
+  title: propTypes.string.isRequired,
+  lupa: propTypes.string.isRequired,
+};
 
 export default Header;
--- src/pages/ComidasDetalhes.js
+++ src/pages/ComidasDetalhes.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 
 class ComidasDetalhes extends Component {
   render() {
-    const lupa = 'desligado';
     return (
       <div>
         <h1>Detalhes de Comida</h1>
-        <Header lupa={ lupa } />
-        <Footer />
       </div>
     );
   }
