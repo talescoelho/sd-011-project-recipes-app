17762ce4af978dde1f7121d916055c4ce23e5a46
fernando lasmar <f3rnandolasmar@gmail.com>
2021-07-30 21:08:58 -0300

    requisito 70 ao 73

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default class Perfil extends Component {
   render() {
+    // const { email } = JSON.parse(localStorage.getItem('user'));
     return (
       <div>
         <Header title="Perfil" />
