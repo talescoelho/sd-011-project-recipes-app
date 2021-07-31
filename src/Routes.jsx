4f3ceaf1d72147d3aa6eefef73a5de2c94889676
Hamander <1hamander@gmail.com>
2021-07-31 12:26:47 -0300

    Lint Fixes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -17,22 +17,82 @@ import {
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/perfil" component={ Perfil } />
-      <Route exact path="/explorar" component={ Explore } />
-      <Route exact path="/comidas" component={ Foods } />
-      <Route exact path="/bebidas" component={ Drinks } />
-      <Route exact path="/receitas-feitas" component={ RecipesMade } />
-      <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
-      <Route exact path="/explorar/comidas" component={ FoodExplore } />
-      <Route exact path="/explorar/bebidas" component={ DrinksExplore } />
-      <Route exact path="/explorar/comidas/area" component={ ExploreOrigin } />
-      <Route exact path="/comidas/:id-da-receitas" />
-      <Route exact path="/bebidas/:id-da-receitas" />
-      <Route exact path="/explorar/comidas/ingredientes" component={ ExploreIngredients } />
-      <Route exact path="/explorar/bebidas/ingredientes" component={ ExploreIngredients } />
-      <Route exact path="/comidas/:id-da-receitas/in-progress" />
-      <Route exact path="/bebidas/:id-da-receitas/in-progress" />
+      <Route
+        exact
+        path="/"
+        component={ Login }
+      />
+      <Route
+        exact
+        path="/perfil"
+        component={ Perfil }
+      />
+      <Route
+        exact
+        path="/explorar"
+        component={ Explore }
+      />
+      <Route
+        exact
+        path="/comidas"
+        component={ Foods }
+      />
+      <Route
+        exact
+        path="/bebidas"
+        component={ Drinks }
+      />
+      <Route
+        exact
+        path="/receitas-feitas"
+        component={ RecipesMade }
+      />
+      <Route
+        exact
+        path="/receitas-favoritas"
+        component={ FavoriteRecipes }
+      />
+      <Route
+        exact
+        path="/explorar/comidas"
+        component={ FoodExplore }
+      />
+      <Route
+        exact
+        path="/explorar/bebidas"
+        component={ DrinksExplore }
+      />
+      <Route
+        exact
+        path="/explorar/comidas/area"
+        component={ ExploreOrigin }
+      />
+      <Route
+        exact
+        path="/comidas/:id-da-receitas"
+      />
+      <Route
+        exact
+        path="/bebidas/:id-da-receitas"
+      />
+      <Route
+        exact
+        path="/explorar/comidas/ingredientes"
+        component={ ExploreIngredients }
+      />
+      <Route
+        exact
+        path="/explorar/bebidas/ingredientes"
+        component={ ExploreIngredients }
+      />
+      <Route
+        exact
+        path="/comidas/:id-da-receitas/in-progress"
+      />
+      <Route
+        exact
+        path="/bebidas/:id-da-receitas/in-progress"
+      />
     </Switch>
   </BrowserRouter>
 );
