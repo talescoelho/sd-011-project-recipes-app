1870d6a8947430d6c63d731432a4fbe8a6a7ac6f
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-30 22:25:18 -0300

    Requisito 28

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import ButtonDrinksCategories from '../components/ButtonDrinksCategories';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Drinks() {
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  async function fetchFoods() {
+  async function fetchDrinks() {
     const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
     const response = await fetch(endpoint);
     const json = await response.json();
@@ -14,16 +14,47 @@ export default function Drinks() {
   }
 
   useEffect(() => {
-    fetchFoods();
+    fetchDrinks();
   }, []);
 
-  const maxArray = 12;
+  async function categoriesDrinks() {
+    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+    const response = await fetch(endpoint);
+    const json = await response.json();
+    setCategories(json);
+  }
+
+  useEffect(() => {
+    categoriesDrinks();
+  }, []);
+
+  async function searchByCategory(param) {
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${param}`;
+    const response = await fetch(endpoint);
+    const json = await response.json();
+    setData(json);
+  }
+
+  const maxArrayDrinks = 12;
+  const maxArrayCategories = 5;
+
   return (
     <main>
       <Header title="Bebidas" />
-      <ButtonDrinksCategories />
+      {categories.length === 0 ? <p>Loading</p>
+        : categories.drinks.slice(0, maxArrayCategories).map((categorie, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <button
+              type="button"
+              data-testid={ `${categorie.strCategory}-category-filter` }
+              onClick={ () => searchByCategory(categorie.strCategory) }
+            >
+              {categorie.strCategory}
+            </button>
+          </div>
+        ))}
       {data.length === 0 ? <p>Loading</p>
-        : data.drinks.slice(0, maxArray).map((drink, index) => (
+        : data.drinks.slice(0, maxArrayDrinks).map((drink, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
             <img
               src={ drink.strDrinkThumb }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import ButtonFoodsCategories from '../components/ButtonFoodsCategories';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Foods() {
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   async function fetchFoods() {
     const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
@@ -17,14 +17,44 @@ function Foods() {
     fetchFoods();
   }, []);
 
-  const maxArray = 12;
+  async function categoriesFood() {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+    const response = await fetch(endpoint);
+    const json = await response.json();
+    setCategories(json);
+  }
+
+  useEffect(() => {
+    categoriesFood();
+  }, []);
+
+  async function searchByCategory(param) {
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${param}`;
+    const response = await fetch(endpoint);
+    const json = await response.json();
+    setData(json);
+  }
+
+  const maxArrayFoods = 12;
+  const maxArrayCategories = 5;
 
   return (
     <main>
       <Header title="Comidas" />
-      <ButtonFoodsCategories />
+      {categories.length === 0 ? <p>Loading</p>
+        : categories.meals.slice(0, maxArrayCategories).map((categorie, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <button
+              type="button"
+              data-testid={ `${categorie.strCategory}-category-filter` }
+              onClick={ () => searchByCategory(categorie.strCategory) }
+            >
+              {categorie.strCategory}
+            </button>
+          </div>
+        ))}
       {data.length === 0 ? <p>Loading</p>
-        : data.meals.slice(0, maxArray).map((food, index) => (
+        : data.meals.slice(0, maxArrayFoods).map((food, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
             <img
               src={ food.strMealThumb }
