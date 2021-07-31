e1ba6f8bdda5ecb84cffe86918f8923d8889d0f3
juliofilizzola <juliofilizzola@Hotmail.com>
2021-07-31 11:13:21 -0300

    create page HomeRecipe

---
+++ src/pages/HomeRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function HomeRecipe() {
+  return (
+    <div>
+      <Header title="Comidas" />
+    </div>
+  );
+}
+
+export default HomeRecipe;
