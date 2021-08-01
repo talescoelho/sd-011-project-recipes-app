c9f4585d0426c5419afdc1a8d9367c3b7a98c2fc
Alberto Candido <albertocandido98@gmail.com>
2021-08-01 18:19:30 -0300

    Inicia tela de receitas favoritas

--- src/pages/FavoriteRecipes/index.js
+++ src/pages/FavoriteRecipes/index.js
@@ -3,6 +3,14 @@ import React from 'react';
 const FavoriteRecipes = () => (
   <div>
     Sou página de receitas favoritas.
+    <button type="button" data-testid="filter-by-all-btn">All</button>
+    <button type="button" data-testid="filter-by-food-btn">Food</button>
+    <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+    <button type="button" data-testid="filter-by-all-btn">All</button>
+    <button type="button" data-testid="filter-by-all-btn">All</button>
+    <button type="button" data-testid="filter-by-all-btn">All</button>
+    <button type="button" data-testid="filter-by-all-btn">All</button>
+
   </div>
 );
 
