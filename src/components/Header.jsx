45cb7039954f2ff924d91b5fdc73e306e13d0c93
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-07-31 21:46:56 -0300

    Requisito 12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, searchButton }) {
+  const [searchBar, setSearchBar] = useState(false);
+
+  function handleSearchBar() {
+    setSearchBar(!searchBar);
+  }
+
   return (
     <header>
       <Link to="perfil">
@@ -14,9 +20,14 @@ function Header({ title, searchButton }) {
       </Link>
       <p data-testid="page-title">{ title }</p>
       {searchButton && (
-        <button type="button">
-          <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
-        </button>
+        <div>
+          <button type="button" onClick={ handleSearchBar }>
+            <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
+          </button>
+          {searchBar && (
+            <input type="text" placeholder="Buscar Receita" data-testid="search-input" />
+          )}
+        </div>
       )}
     </header>
   );
