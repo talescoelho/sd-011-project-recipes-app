6bac8f37ffbc2755a27ba6ab14a51adf0752640b
Marconi <marconieng@gmail.com>
2021-08-01 14:32:33 -0300

    Adds requirements from 19 to 24 related to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
@@ -6,33 +7,39 @@ import mealIcon from '../images/mealIcon.svg';
 function Footer() {
   return (
     <footer data-testid="footer" style={ { position: 'fixed', bottom: 0 } }>
-      <button
-        type="button"
-      >
-        <img
-          src={ drinkIcon }
-          alt="Imagem do ícone de acesso às bebidas"
-          data-testid="drinks-bottom-btn"
-        />
-      </button>
-      <button
-        type="button"
-      >
-        <img
-          src={ exploreIcon }
-          alt="Imagem do ícone de acesso aos conteúdos do app"
-          data-testid="explore-bottom-btn"
-        />
-      </button>
-      <button
-        type="button"
-      >
-        <img
-          src={ mealIcon }
-          alt="Imagem do ícone de acesso às comidas"
-          data-testid="food-bottom-btn"
-        />
-      </button>
+      <Link to="/bebidas">
+        <button
+          type="button"
+        >
+          <img
+            src={ drinkIcon }
+            alt="Imagem do ícone de acesso às bebidas"
+            data-testid="drinks-bottom-btn"
+          />
+        </button>
+      </Link>
+      <Link to="/explorar">
+        <button
+          type="button"
+        >
+          <img
+            src={ exploreIcon }
+            alt="Imagem do ícone de acesso aos conteúdos do app"
+            data-testid="explore-bottom-btn"
+          />
+        </button>
+      </Link>
+      <Link to="/comidas">
+        <button
+          type="button"
+        >
+          <img
+            src={ mealIcon }
+            alt="Imagem do ícone de acesso às comidas"
+            data-testid="food-bottom-btn"
+          />
+        </button>
+      </Link>
     </footer>
   );
 }
--- src/pages/ExploreFoodByArea.jsx
+++ src/pages/ExploreFoodByArea.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreFoodByArea() {
   return (
     <div>
       <Header title="Explorar Origem" searchButton />
+      <Footer />
     </div>
   );
 }
