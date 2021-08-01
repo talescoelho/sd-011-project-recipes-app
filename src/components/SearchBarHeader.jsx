81c75b9f0242e28797d4e3a4ff603c1a1db70ba9
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-08-01 19:36:33 -0300

    Requisitos 17 e 18

--- src/components/SearchBarHeader.jsx
+++ src/components/SearchBarHeader.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
 
-function SearchBarHeader(foodOrDrink) {
+function SearchBarHeader({ foodOrDrink }) {
   const [filterRadio, setFilterRadio] = useState('s');
   const [filterText, setFilterText] = useState('');
   const [filteredItem, setFilteredItem] = useState([]);
 
   const history = useHistory();
+  const maxLength = 12;
 
   async function fetchFood() {
     let endPoint = `https://www.themealdb.com/api/json/v1/1/search.php?${filterRadio}=${filterText}`;
@@ -16,11 +18,14 @@ function SearchBarHeader(foodOrDrink) {
     const request = await fetch(endPoint);
     const response = await request.json();
     const data = response.meals;
-    setFilteredItem(data);
-    setFilterText([]);
-    document.getElementById('form').reset();
-    if (data.length === 1) {
-      history.push(`/comidas/${data[0].idMeal}`);
+    if (data !== null) {
+      setFilteredItem(data);
+      document.getElementById('form').reset();
+      if (data.length === 1) {
+        history.push(`/comidas/${data[0].idMeal}`);
+      }
+    } else {
+      alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
     }
   }
 
@@ -32,12 +37,30 @@ function SearchBarHeader(foodOrDrink) {
     const request = await fetch(endPoint);
     const response = await request.json();
     const data = response.drinks;
-    setFilteredItem(data);
-    setFilterText([]);
-    document.getElementById('form').reset();
-    if (data.length === 1) {
-      history.push(`/bebidas/${data[0].idDrink}`);
+    if (data !== null) {
+      setFilteredItem(data);
+      document.getElementById('form').reset();
+      if (data.length === 1) {
+        history.push(`/bebidas/${data[0].idDrink}`);
+      }
+    } else {
+      alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
     }
+  }
+
+  function renderRecipes(item, index) {
+    return (
+      <div data-testid={ `${index}-recipe-card` } key={ index }>
+        <img
+          src={ foodOrDrink === 'Comidas' ? item.strMealThumb : item.strDrinkThumb }
+          alt="food_image"
+          data-testid={ `${index}-card-img` }
+        />
+        <p data-testid={ `${index}-card-name` }>
+          {foodOrDrink === 'Comidas' ? item.strMeal : item.strDrink}
+        </p>
+      </div>
+    );
   }
 
   useEffect(() => {
@@ -51,55 +74,67 @@ function SearchBarHeader(foodOrDrink) {
   }, [filteredItem]);
 
   return (
-    <form id="form">
-      <input
-        type="text"
-        placeholder="Buscar Receita"
-        data-testid="search-input"
-        onChange={ (e) => setFilterText(e.target.value) }
-      />
-      <label htmlFor="ingredient">
+    <main>
+
+      <form id="form">
         <input
-          type="radio"
-          id="ingredient"
-          name="radiosFilter"
-          data-testid="ingredient-search-radio"
-          value="i"
-          onClick={ (e) => setFilterRadio(e.target.value) }
+          type="text"
+          placeholder="Buscar Receita"
+          data-testid="search-input"
+          onChange={ (e) => setFilterText(e.target.value) }
         />
-        Ingrediente
-      </label>
-      <label htmlFor="name">
-        <input
-          type="radio"
-          id="name"
-          name="radiosFilter"
-          data-testid="name-search-radio"
-          value="s"
-          onClick={ (e) => setFilterRadio(e.target.value) }
-        />
-        Nome
-      </label>
-      <label htmlFor="firstLetter">
-        <input
-          type="radio"
-          id="firstLetter"
-          name="radiosFilter"
-          data-testid="first-letter-search-radio"
-          value="f"
-          onClick={ (e) => setFilterRadio(e.target.value) }
-        />
-        Primeira letra
-      </label>
-      <button
-        type="button"
-        data-testid="exec-search-btn"
-        onClick={ foodOrDrink.foodOrDrink === 'Comidas' ? fetchFood : fetchDrink }
-      >
-        Buscar
-      </button>
-    </form>
+        <label htmlFor="ingredient">
+          <input
+            type="radio"
+            id="ingredient"
+            name="radiosFilter"
+            data-testid="ingredient-search-radio"
+            value="i"
+            onClick={ (e) => setFilterRadio(e.target.value) }
+          />
+          Ingrediente
+        </label>
+        <label htmlFor="name">
+          <input
+            type="radio"
+            id="name"
+            name="radiosFilter"
+            data-testid="name-search-radio"
+            value="s"
+            onClick={ (e) => setFilterRadio(e.target.value) }
+          />
+          Nome
+        </label>
+        <label htmlFor="firstLetter">
+          <input
+            type="radio"
+            id="firstLetter"
+            name="radiosFilter"
+            data-testid="first-letter-search-radio"
+            value="f"
+            onClick={ (e) => setFilterRadio(e.target.value) }
+          />
+          Primeira letra
+        </label>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ foodOrDrink === 'Comidas' ? fetchFood : fetchDrink }
+        >
+          Buscar
+        </button>
+      </form>
+      <div>
+        {filteredItem.length > 1 && (
+          filteredItem.filter((item, index) => index < maxLength).map(renderRecipes)
+        )}
+      </div>
+    </main>
   );
 }
 
 export default SearchBarHeader;
+
+SearchBarHeader.propTypes = {
+  foodOrDrink: PropTypes.string.isRequired,
+};
