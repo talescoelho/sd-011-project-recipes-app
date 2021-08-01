4abc2310f9d99f9c4b971d814cf9eb5aa2539980
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-01 10:16:09 -0300

    editing RenderCategoriesDrinks component

--- src/components/RenderCategoriesDrinks.js
+++ src/components/RenderCategoriesDrinks.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function RenderCategoriesDrinks() {
+function RenderCategoriesDrinks(whoCategory) {
+  const MagicMikeDance = 12;
+
   return (
-    <div>
-      
-    </div>
-  )
+    whoCategory && whoCategory.slice(0, MagicMikeDance).map((itemCard, index) => (
+
+      <div key={ index } data-testid={ `${index}-recipe-card` } className="card">
+        <img
+          src={ itemCard.strDrinkThumb }
+          data-testid={ `${index}-card-img` }
+          alt={ itemCard.strDrinks }
+        />
+        <div className="card-body">
+          <p data-testid={ `${index}-card-name` }>{ itemCard.strDrink }</p>
+        </div>
+      </div>))
+  );
 }
 
 export default RenderCategoriesDrinks;
