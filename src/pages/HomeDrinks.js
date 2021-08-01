830b1ba18cb3196375aedce508e00fc63eebea0f
juliofilizzola <juliofilizzola@Hotmail.com>
2021-07-31 23:34:13 -0300

    Rendering buttons on drinks page

--- src/pages/HomeDrinks.js
+++ src/pages/HomeDrinks.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
-import { fetchDrinkMain } from '../redux/actions';
+import { fetchDrinkMain, fetchRecipesListDrinks } from '../redux/actions';
 import Header from '../components/Header';
 
 function HomeDrinks() {
   const urlFetch = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  const urlFetchList = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
   const [isLoading, setIsLoading] = React.useState(true);
+  const [drinksList, setDrinksList] = React.useState([]);
   const [drinks, setDrinks] = React.useState([]);
 
   const dispatch = useDispatch();
   const MagicMikeDance = 12;
   const fetchRecipesMainF = (url) => dispatch(fetchDrinkMain(url));
+  const fetchListApi = (url) => dispatch(fetchRecipesListDrinks(url));
 
   const handlerCard = async () => {
     const response = await fetchRecipesMainF(urlFetch);
-    if (response.length > 1) {
+    const responseList = await fetchListApi(urlFetchList);
+    if (response.length > 1 && responseList.length > 1) {
       setDrinks([...response]);
+      console.log(responseList);
+      setDrinksList([...responseList]);
       setIsLoading(false);
     }
   };
@@ -28,6 +34,18 @@ function HomeDrinks() {
   return (
     <div>
       <Header title="Bebidas" />
+      { !isLoading && (drinksList.slice(0, 5).map((itemList, index) => (
+        <div key={ index }>
+          <button
+            data-testid={ `${itemList.strCategory}-category-filter` }
+            type="button"
+          >
+            {' '}
+            { itemList.strCategory }
+            {' '}
+          </button>
+        </div>
+      )))}
       <div>
         {isLoading ? <p>loading...</p>
           : (drinks.slice(0, MagicMikeDance).map((itemCard, index) => (
