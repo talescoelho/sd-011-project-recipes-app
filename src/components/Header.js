e259dae6d9c5b3223b9c673d15d695086ca348e6
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-08-02 16:08:21 -0300

    requirement 9 and 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,67 @@ import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: false,
+      title: '',
+    }
+  }
+componentDidMount() {
+  const { location } = this.props;
+  const pagesTitles  = {
+    '/perfil': () => this.setState({
+      search: false,
+      title: 'Perfil'
+    }),
+    '/comidas': () => this.setState({
+      search: true,
+      title: 'Comidas'
+    }),
+    
+    '/bebidas': () => this.setState({
+      search: true,
+      title: 'Bebidas',
+    }),
+    '/explorar': () => this.setState({
+      search: false,
+      title: 'Explorar',
+    }),
+    '/explorar/comidas/ingredientes': () => this.setState({
+      search: false,
+      title: 'Explorar Ingredientes',
+    }),
+    '/explorar/bebidas/ingredientes': () => this.setState({
+      search: false,
+      title: 'Explorar Ingredientes',
+    }),
+    '/explorar/comidas/area': () => this.setState({
+      search: true,
+      title: 'Explorar Origem',
+    }),
+    '/receitas-feitas': () => this.setState({
+      search: false,
+      title: 'Receitas Feitas',
+    }),
+    '/receitas-favoritas': () => this.setState({
+      search: false,
+      title: 'Receitas Favoritas',
+    }),
+    '/explorar/comidas': () => this.setState({
+      search: false,
+      title: 'Explorar Comidas',
+    }),
+    '/explorar/bebidas': () => this.setState({
+      search: false,
+      title: 'Explorar Bebidas',
+    }),
+  }
+  pagesTitles[location.pathname]();
+}
   render() {
+    // const { match } = this.props;
+    
     return (
       <header>
         <Link to="/perfil">
@@ -13,13 +73,14 @@ class Header extends Component {
             <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
           </button>
         </Link>
-        <h1 data-testid="page-title">Comidas</h1>
-        <button
+        <h1 data-testid="page-title">{ this.state.title }</h1>
+        { this.state.search 
+        ? <button
           onClick={ () => <SearchBar /> }
           type="button"
         >
           <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
-        </button>
+        </button> : ''}
       </header>
     );
   }
