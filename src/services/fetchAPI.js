1d6f14ce90c9a8b6524dd98503bbda467d00583b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-08-02 17:17:45 -0300

    requirement 13

---
+++ src/services/fetchAPI.js
@@ -0,0 +1,7 @@
+// const urlIngredients = 'https://www.themealdb.com/api/json/v1/1/filter.php?i={ingrediente}';
+// const urlName = 'https://www.themealdb.com/api/json/v1/1/search.php?s={nome}';
+// const urlFirstLetter = 'https://www.themealdb.com/api/json/v1/1/search.php?f={primeira-letra}';
+
+// export async function fetchAPI(type) {
+// const response = await fetch()
+// }
