a925ad93c4604188cb90eea7c907932282826b37
Raphael <raphaellfpg@gmail.com>
2021-08-02 17:07:43 -0300

    Feat: Tela de Explorar

--- src/Pages/Explorar.js
+++ src/Pages/Explorar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Explorar({ title }) {
   return (
     <div>
       <Header title={ title } />
+      <Link to="/explorar/comidas">
+        <button
+          data-testid="explore-food"
+          type="button"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
     </div>
   );
 }
