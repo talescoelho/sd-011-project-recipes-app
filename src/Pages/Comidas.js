631366920d904172758d999bd8d089bf5940d81b
Raphael <raphaellfpg@gmail.com>
2021-08-02 15:53:26 -0300

    Footer completo

--- src/Pages/Comidas.js
+++ src/Pages/Comidas.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import RenderRecipes from '../components/RenderRecipes';
+import Footer from '../components/Footer';
 
 function Comidas({ title }) {
   const { data } = useSelector((state) => state.fetchReceitas);
@@ -10,6 +11,7 @@ function Comidas({ title }) {
     <div>
       <Header title={ title } />
       { (Object.keys(data).length > 0) && <RenderRecipes />}
+      <Footer />
     </div>
   );
 }
