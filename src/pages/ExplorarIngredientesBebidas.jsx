e42d798a6a04af7bc776d514fb32c29bfb23930e
IsleneGomes <islenegomes@gmail.com>
2021-08-02 20:19:58 -0300

    Adiciona Footer em demais componentes

--- src/pages/ExplorarIngredientesBebidas.jsx
+++ src/pages/ExplorarIngredientesBebidas.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default class ExplorarIngredientesBebidas extends Component {
@@ -6,6 +7,7 @@ export default class ExplorarIngredientesBebidas extends Component {
     return (
       <div>
         <Header title="Explorar Ingredientes" />
+        <Footer />
       </div>
     );
   }
