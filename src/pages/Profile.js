25dcc6a383d524a78fb4b44ef4cc0069381a9df2
Marcelo <marsleite@gmail.com>
2021-08-02 09:35:18 -0300

    requirement 82 and 83 done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,10 +7,33 @@ export default function Profile() {
     pageName: 'Perfil',
     setIcon: false,
   };
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header value={ pageTitle } />
-      FOODS
+      <div>
+        <h3 data-testid="profile-email">
+          { user !== null ? user.email : 'email' }
+        </h3>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Recitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Recitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+      </div>
       <FooterMenu />
     </div>
   );
