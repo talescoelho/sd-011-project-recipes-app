660b73fe2eb7e5dca82114fee9bea937ca56d0cb
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-02 18:47:10 -0300

    Feature: renderiza card contendo imagem e nome dos ingredientes de comida e bebidas

--- src/Pages/ExplorarBebidasIngredientes.js
+++ src/Pages/ExplorarBebidasIngredientes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import IngredientsDetails from '../components/IngredientsDetails';
 
 function ExplorarBebidasIngredientes({ title }) {
   return (
     <div>
       <Header title={ title } />
+      <IngredientsDetails />
     </div>
   );
 }
---
+++ src/components/IngredientsDetails.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRecipes } from '../redux/slices/fetchReceitas';
+import './IngredientsDetails.css';
+
+function IngredientsDetails() {
+  const { data } = useSelector((state) => state.fetchReceitas);
+  const dispatch = useDispatch();
+
+  const { pathname } = window.location;
+  const currentURL = pathname.split('/')[2];
+  const recipeTypeDictionary = {
+    comidas: 'themealdb',
+    bebidas: 'thecocktaildb',
+  };
+
+  useEffect(() => {
+    const URL = `https://www.${recipeTypeDictionary[currentURL]}.com/api/json/v1/1/list.php?i=list`;
+    dispatch(getRecipes(URL));
+  }, [dispatch, currentURL]);
+
+  if (data.length !== 0) {
+    const limitCards = 12;
+    let ingredientKey = 'strIngredient';
+    let recipeType = data.meals;
+    if (currentURL === 'bebidas') {
+      recipeType = data.drinks;
+      ingredientKey = 'strIngredient1';
+    }
+
+    return (
+      <section className="ingredients-container">
+        {recipeType.slice(0, limitCards).map((ingredient, index) => (
+          <div
+            data-testid={ `${index}-ingredient-card` }
+            key={ index }
+            className="ingredient-card"
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ `https://www.${recipeTypeDictionary[currentURL]}.com/images/ingredients/${ingredient[ingredientKey]}-Small.png` }
+              alt={ ingredient[ingredientKey] }
+            />
+            <p
+              data-testid={ `${index}-card-name` }
+            >
+              {ingredient[ingredientKey]}
+            </p>
+          </div>
+        ))}
+      </section>
+    );
+  }
+  return null;
+}
+
+export default IngredientsDetails;
