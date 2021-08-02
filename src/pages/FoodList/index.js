4acfd3ef75e13a8617de46728a4141cc59497694
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-02 15:41:01 -0300

    finaliza os requisitos 17 e 18

--- src/pages/FoodList/index.js
+++ src/pages/FoodList/index.js
@@ -3,24 +3,30 @@ import { connect } from 'react-redux';
 import FooterMenu from '../../components/FooterMenu';
 import Header from '../../components/Header';
 
-const FoodList = ({ receiveData, fetched }) => (
-  <div>
-    <Header />
-    { fetched && receiveData ? receiveData.meals
-      .map((meal, index) => (
-        <div key={ index }>
-          <h2 data-testid={ `${index}-card-name` }>{ meal.strMeal }</h2>
-          <img
-            src={ meal.strMealThumb }
-            alt={ meal.strMeal }
-            data-testid={ `${index}-card-img` }
-          />
-          <h3 data-testid={ `${index}-recipe-card` }>{ meal.strInstructions }</h3>
-        </div>
-      )) : <p>teste</p> }
-    <FooterMenu />
-  </div>
-);
+const FoodList = ({ receiveData, fetched }) => {
+  if (receiveData.meals === null) {
+    // eslint-disable-next-line no-alert
+    alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+  }
+  return (
+    <div>
+      <Header />
+      { fetched && receiveData.meals !== null ? receiveData.meals
+        .map((meal, index) => (
+          <div key={ index }>
+            <h2 data-testid={ `${index}-card-name` }>{ meal.strMeal }</h2>
+            <img
+              src={ meal.strMealThumb }
+              alt={ meal.strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <h3 data-testid={ `${index}-recipe-card` }>{ meal.strInstructions }</h3>
+          </div>
+        )) : <p>Pesquise por uma comida.</p> }
+      <FooterMenu />
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => ({
   receiveData: state.searchBarReducer.receiveData,
