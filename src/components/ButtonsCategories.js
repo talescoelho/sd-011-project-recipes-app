53b66ca830ae20e68381fea3acc7cfe6d1871023
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-02 17:06:59 -0300

    [Tela Principal de Receita] Requisito 28 e 29

--- src/components/ButtonsCategories.js
+++ src/components/ButtonsCategories.js
@@ -1,28 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GlobalContext from '../context/GlobalContext';
 
 function ButtonsCategories({ categoryName }) {
+  const { setCatalog } = useContext(GlobalContext);
+
   const [categories, setCategories] = useState({ loading: ['true'] });
+  const [URLCategoryToSearch, setURLCategoryToSearch] = useState('');
+  const [filterActive, setFilterActive] = useState(false);
 
   useEffect(() => {
     function fetchAPI() {
       if (categoryName === 'Comidas') {
         fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
           .then((response) => response.json())
-          .then((result) => {
-            setCategories(result);
-          });
+          .then((result) => setCategories(result));
       }
       if (categoryName === 'Bebidas') {
         fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
           .then((response) => response.json())
-          .then((result) => {
-            setCategories(result);
-          });
+          .then((result) => setCategories(result));
       }
     }
     fetchAPI();
   }, [categoryName]);
+
+  useEffect(() => {
+    console.log(URLCategoryToSearch);
+    if (URLCategoryToSearch && filterActive) {
+      if (categoryName === 'Comidas') {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${URLCategoryToSearch}`)
+          .then((response) => response.json())
+          .then((result) => setCatalog(result));
+      }
+      if (categoryName === 'Bebidas') {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${URLCategoryToSearch}`)
+          .then((response) => response.json())
+          .then((result) => setCatalog(result));
+      }
+    } else {
+      if (categoryName === 'Comidas') {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+          .then((response) => response.json())
+          .then((result) => setCatalog(result));
+      }
+      if (categoryName === 'Bebidas') {
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+          .then((response) => response.json())
+          .then((result) => setCatalog(result));
+      }
+    }
+  }, [URLCategoryToSearch, setCatalog, filterActive, categoryName]);
+
+  function resetFetchAPI() {
+    console.log('teste');
+    if (categoryName === 'Comidas') {
+      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+        .then((response) => response.json())
+        .then((result) => setCatalog(result));
+    }
+    if (categoryName === 'Bebidas') {
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+        .then((response) => response.json())
+        .then((result) => setCatalog(result));
+    }
+  }
 
   const supplyIdentity = Object.keys(categories)[0];
 
@@ -31,12 +73,22 @@ function ButtonsCategories({ categoryName }) {
 
   return (
     <div>
-      <button type="button">All</button>
+      <button
+        type="button"
+        onClick={ () => resetFetchAPI() }
+      >
+        All
+      </button>
       {categoriesLimited.map((category, index) => (
         <button
           data-testid={ `${category.strCategory}-category-filter` }
           type="button"
           key={ index }
+          name={ category.strCategory }
+          onClick={ ({ target }) => {
+            setFilterActive(!filterActive);
+            setURLCategoryToSearch(target.name);
+          } }
         >
           {category.strCategory}
         </button>
