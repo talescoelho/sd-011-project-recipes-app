3e5c056a6293153e6bdb09da1e9a2b2f37934b8e
benlopes <rubenssouss@hotmail.com>
2021-08-02 15:02:16 -0300

    add css

--- src/Pages/Bebidas.js
+++ src/Pages/Bebidas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cards from '../components/Cards';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../App.css';
 
 function Bebidas() {
   return (
--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Cards from '../components/Cards';
+import '../App.css';
 
 // Usei o history para pegar o caminho da rota e fazer a condição
 
