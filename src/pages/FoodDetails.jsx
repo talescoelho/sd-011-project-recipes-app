0bc0480424ecf0e678019baa39345a13648352cf
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-02 15:35:02 -0300

    tela de detalhes

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -9,15 +9,75 @@ export default function FoodDetails() {
   useEffect(() => {
     const getFood = async () => {
       const data = await fetchFood(params.id);
-      console.log(data);
       setFood(data);
     };
     getFood();
   }, [params.id]);
 
+  function returnRecipe() {
+    if (food) {
+      return food.meals[0];
+    }
+    return '';
+  }
+
+  function listIngradient() {
+    const retorno = [];
+    for (let index = 1; index <= 20; index++) {
+      if (returnRecipe()[`strIngredient${index}`] != '') {
+        retorno.push(
+          <li key={ index }>
+            {' '}
+            {returnRecipe()[`strIngredient${index}`]}
+            {' '}
+            -
+            {' '}
+            {returnRecipe()[`strMeasure${index}`]}
+          </li>,
+        );
+      }
+    }
+
+    return retorno;
+  }
+
   return (
     <div>
-      a
+      <button type="button" data-testid="share-btn">
+        Compartilhar
+      </button>
+      <button type="button" data-testid="favorite-btn">
+        Favoritar
+      </button>
+      <div>
+        <h1 data-testid="recipe-title">{returnRecipe().strMeal}</h1>
+        <img
+          data-testid="recipe-photo"
+          src={ returnRecipe().strMealThumb }
+          alt="img"
+        />
+        <p data-testid="instructions">{returnRecipe().strInstructions}</p>
+        <p data-testid="recipe-category">
+          Categoria:
+          {returnRecipe().strCategory}
+        </p>
+        <p data-testid={ `${listIngradient()}-ingredient-name-and-measure` }>
+          {listIngradient()}
+        </p>
+        <p>
+          Vídeo:
+          <a
+            data-testid="video"
+            href={ returnRecipe().strYoutube }
+            target="blank"
+          >
+            Receita
+          </a>
+        </p>
+        <button type="button" data-testid="start-recipe-btn">
+          Iniciar Receita
+        </button>
+      </div>
     </div>
   );
 }
--- src/services/FoodAPI.js
+++ src/services/FoodAPI.js
@@ -1,15 +1,12 @@
 // eslint-disable-next-line import/prefer-default-export
 export const fetchFood = async (id) => {
-  const URL = `www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-  // const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=potato';
+  const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
   const response = await fetch(URL);
-  console.log(response);
-  // const json = await response.json();
-  // return json;
+  const json = await response.json();
 
-  // try {
-  //   return results;
-  // } catch (error) {
-  //   return error;
-  // }
+  try {
+    return json;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
