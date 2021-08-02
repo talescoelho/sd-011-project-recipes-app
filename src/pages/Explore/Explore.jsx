b3eeed8d511e743041dbb2b32ec2a62366cfbb2d
Vinicius <viniciusfod@gmail.com>
2021-08-02 16:14:41 -0300

    main-group-37-Explore-page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,14 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../globalComponents/Footer';
 import Header from '../../globalComponents/Header';
 
 function Explore({ match }) {
+  const history = useHistory();
   return (
     <>
       <Header title="Explorar" match={ match } />
       <div>
-        Explore
+        <button
+          type="button"
+          data-testid="explore-food"
+          onClick={ () => history.push('/explorar/comidas') }
+        >
+          Explorar Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explorar/bebidas') }
+        >
+          Explorar Bebidas
+        </button>
       </div>
       <Footer />
     </>
