6b3c9bbeaf999eee1a5e760781cc1989552cc008
Michel Pereira <michelbecauro@gmail.com>
2021-08-02 17:05:09 -0300

    Requisito 9 - Feature e Testes(RTL)

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Recipes() {
   return (
-    <main data-testid="recipes-page">
-      <h1>Tela Principal de Receitas</h1>
-    </main>
+    <>
+      <header>
+        <h1 data-testid="page-title">Tela Principal de Receitas</h1>
+        <button type="button" data-testid="profile-top-btn"> Profile</button>
+        <button type="button" data-testid="search-top-btn"> Search</button>
+      </header>
+      <main data-testid="recipes-page">
+        <h2> Corpo da Tela </h2>
+      </main>
+    </>
   );
 }
 
