16adcf83a4839d07ef6c9e9512fd75bccc475e71
Michelle <mls.srbek@gmail.com>
2021-08-02 18:21:11 -0300

    redireciona para detalhes da receita

--- src/pages/ExploreDynamic.jsx
+++ src/pages/ExploreDynamic.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -12,3 +13,7 @@ export default function ExploreDynamic({ type }) {
     </div>
   );
 }
+
+ExploreDynamic.propTypes = {
+  type: PropTypes.string.isRequired,
+};
