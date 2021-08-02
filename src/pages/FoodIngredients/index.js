94ec9a43a527371ab8e79679b73cd7f1562e262a
Alberto Candido <albertocandido98@gmail.com>
2021-08-02 17:21:35 -0300

    Inicia tela de explorar receitas por ingredientes

--- src/pages/FoodIngredients/index.js
+++ src/pages/FoodIngredients/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 
-const FoodInProgress = () => (
-  <div>
-    Sou página de ingredientes de um prato.
-  </div>
-);
+const FoodInProgress = () => {
+  const { data, request } = useFetch();
+
+  React.useEffect(() => {
+    request('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+  }, [request]);
+
+  if (!data) {
+    return null;
+  }
+  const magicNumber = 10;
+  const filteredData = data.filter((item, index) => index < magicNumber);
+
+  return (
+    <div>
+      { filteredData.map((item, index) => {
+        const { strIngredient } = item;
+        return (
+          <div key={ index }>
+            <img src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient}-Small.png ` } alt={ strIngredient } />
+            <h3>{strIngredient}</h3>
+          </div>
+        );
+      }) }
+    </div>
+  );
+};
 
 export default FoodInProgress;
