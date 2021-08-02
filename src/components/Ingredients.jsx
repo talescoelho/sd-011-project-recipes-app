5e35c394b524be0e174ace5d3e4bc3d993e57a88
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2021-08-02 19:20:45 -0300

    começando o req 33

---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Ingredients(params) {
+  return (
+    <div>
+      Igredientes = {" strIngredient1 cada igrediente e quantidade tem sua chave "}
+    </div>
+  );
+}
