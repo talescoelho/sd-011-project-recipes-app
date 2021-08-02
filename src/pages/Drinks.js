96f909db45db9c43a6a02cdb85220eb3f812b8c6
Maran <maranmorimoto@gmail.com>
2021-08-02 17:38:32 -0300

    just saving project

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,24 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
+import { fetchDrinksLetter } from '../services/DrinksApiServices';
 
 export default function Drinks() {
   const pageTitle = {
     pageName: 'Bebidas',
     setIcon: true,
   };
+
+  const [firstDrink, setFirstDrink] = useState([]);
+
+  useEffect(() => {
+    const response = async () => {
+      const data = await fetchDrinksLetter('a');
+      return setFirstDrink(data);
+    };
+    response();
+  }, []);
+
   const history = useHistory();
   const { recipesDb, redirect } = useContext(RecipesContext);
   const limits = 12;
-  return (
-    <div>
-      <Header value={ pageTitle } />
-      { redirect ? history.push(`/bebidas/${recipesDb.map((drink) => drink.idDrink)}`) : (
+
+  function handleDrinks() {
+    if (recipesDb.length === 0) {
+      return (
         <div>
           {
-            recipesDb.map((drink, index) => (// requisito 17, card com limite de 12
+            firstDrink.map((drink, index) => (// requisito 17, card com limite de 12
               (index < limits) && (
                 <div key={ index }>
                   <div data-testid={ `${index}-recipe-card` }>
@@ -36,7 +48,36 @@ export default function Drinks() {
             ))
           }
         </div>
-      ) }
+      );
+    }
+    return (
+      <div>
+        {
+          recipesDb.map((drink, index) => (// requisito 17, card com limite de 12
+            (index < limits) && (
+              <div key={ index }>
+                <div data-testid={ `${index}-recipe-card` }>
+                  <img
+                    src={ drink.strDrinkThumb }
+                    data-testid={ `${index}-card-img` }
+                    alt={ drink.strDrink }
+                  />
+                </div>
+                <div>
+                  <span data-testid={ `${index}-card-name` }>{ drink.strDrink }</span>
+                </div>
+              </div>
+            )
+          ))
+        }
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header value={ pageTitle } />
+      { redirect ? history.push(`/bebidas/${recipesDb.map((drink) => drink.idDrink)}`) : handleDrinks() }
       <FooterMenu />
     </div>
   );
--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,24 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
+import { fetchMealsLetter } from '../services/MealApiService';
 
 export default function Food() {
   const pageTitle = {
     pageName: 'Comidas',
     setIcon: true,
   };
+
+  const [firstFood, setFirstFood] = useState([]);
+
+  useEffect(() => {
+    const response = async () => {
+      const data = await fetchMealsLetter('c');
+      return setFirstFood(data);
+    };
+    response();
+  }, []);
+
   const history = useHistory();
   const { recipesDb, redirect } = useContext(RecipesContext);
   const limits = 12;
-  return (
-    <div>
-      <Header value={ pageTitle } />
-      { redirect ? history.push(`/comidas/${recipesDb.map((meal) => meal.idMeal)}`) : (
+
+  function handleMeals() {
+    if (recipesDb.length === 0) {
+      return (
         <div>
           {
-            recipesDb.map((meal, index) => (
+            firstFood.map((meal, index) => (
               (index < limits) && (
                 <div key={ index }>
                   <div data-testid={ `${index}-recipe-card` }>
@@ -36,7 +48,36 @@ export default function Food() {
             ))
           }
         </div>
-      ) }
+      );
+    }
+    return (
+      <div>
+        {
+          recipesDb.map((meal, index) => (
+            (index < limits) && (
+              <div key={ index }>
+                <div data-testid={ `${index}-recipe-card` }>
+                  <img
+                    src={ meal.strMealThumb }
+                    data-testid={ `${index}-card-img` }
+                    alt={ meal.strMeal }
+                  />
+                </div>
+                <div>
+                  <span data-testid={ `${index}-card-name` }>{ meal.strMeal }</span>
+                </div>
+              </div>
+            )
+          ))
+        }
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Header value={ pageTitle } />
+      { redirect ? history.push(`/comidas/${recipesDb.map((meal) => meal.idMeal)}`) : handleMeals()}
+      ;
       <FooterMenu />
     </div>
   );
