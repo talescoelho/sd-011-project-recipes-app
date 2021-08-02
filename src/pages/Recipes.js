0632e6b7c855e1a9f65a01b1ed658a8247c98133
Michel Pereira <michelbecauro@gmail.com>
2021-08-02 17:24:22 -0300

    Update src/pages/Recipes.js
    Co-authored-by: Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,8 +6,8 @@ function Recipes() {
     <>
       <header>
         <h1 data-testid="page-title">Tela Principal de Receitas</h1>
-        <button type="button" data-testid="profile-top-btn"> Profile</button>
-        <button type="button" data-testid="search-top-btn"> Search</button>
+        <button type="button" data-testid="profile-top-btn">Profile</button>
+        <button type="button" data-testid="search-top-btn">Search</button>
       </header>
       <main data-testid="recipes-page">
         <h1>Tela Principal de Receitas</h1>
