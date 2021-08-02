dd216b0d491738cd4f3880fab4540ce8a30bc8a6
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-01 22:12:23 -0700

    Finishing the second part of 16th requiriment

--- src/pages/bebidas/index.jsx
+++ src/pages/bebidas/index.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import Header from '../../components/Header';
 
-export default class Bebidas extends Component {
+class Bebidas extends Component {
   render() {
+    const { allRecipes } = this.props;
+    if (allRecipes.length === 1) {
+      return <Redirect to={ `/bebidas/${allRecipes[0].idDrink}` } />;
+    }
     return (
       <div>
         <Header title="Bebidas" mode="bebidas" hasSearchBar />
         Main Bebidas
+        {allRecipes.map((item) => (
+          <div>
+            {item.idDrink}
+          </div>
+        ))}
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  allRecipes: state.recipes.allRecipes,
+});
+
+export default connect(mapStateToProps)(Bebidas);
