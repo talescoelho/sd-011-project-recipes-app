52761737e5643b0ab574e92255bea5fc59d9d34b
Vinicius <viniciusfod@gmail.com>
2021-08-02 15:55:27 -0300

    main-group-37-Profile-Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,43 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../globalComponents/Footer';
 import Header from '../../globalComponents/Header';
 
 function Profile({ match }) {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+  const logOut = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header title="Perfil" match={ match } />
       <div>
-        Profile
+        <span data-testid="profile-email">{ email }</span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => logOut() }
+        >
+          Sair
+        </button>
       </div>
       <Footer />
     </>
