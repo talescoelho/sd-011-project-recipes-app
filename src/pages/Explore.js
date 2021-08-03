a9b32d9408c8648e6e1b8f9d8611415a80dcfcd3
Diego Figueirêdo <8kball@gmail.com>
2021-08-03 12:30:06 -0300

    Requirements 67 ~ 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
 import Header from '../components/Header';
 
@@ -6,6 +7,8 @@ export default function Explore() {
   return (
     <div>
       <Header title="Explorar" renderButton />
+      <Link to="/explorar/comidas" data-testid="explore-food">Explorar Comidas</Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">Explorar Bebidas</Link>
       <LowerMenu />
     </div>
   );
