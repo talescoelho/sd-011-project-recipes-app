bd9c07fd023dba1a96d19cedd3806c942b3d37dd
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-03 18:25:10 -0300

    putting data-testid in tegs

--- src/pages/Favorite/FavoriteMeals.js
+++ src/pages/Favorite/FavoriteMeals.js
@@ -1,12 +1,19 @@
+import React from 'react';
+
 function FavoriteMeals() {
   const getFavoriteLocalStorage = () => {
     const Local = JSON.parse(localStorage.getItem('favoriteRecipes'));
     console.log(Local);
     if (Local) {
-      return Local.map((item, index) => (<div key={ index }>
-        <span data-testid="${index}-horizontal-top-text"> </span>
-       <h2> {item.name}</h2>
-                                         </div>));
+      return Local.map((item, index) => (
+        <div key={ index }>
+          <span data-testid={ `${index}-horizontal-top-text` }> </span>
+          <h2>
+            {' '}
+            {item.name}
+          </h2>
+          <img src={ item.image } alt={ item.name } data-testid={ `${index}-horizontal-image` } />
+        </div>));
     }
   };
   return (
