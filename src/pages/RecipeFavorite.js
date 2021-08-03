fd37ba52af75c7be058a7df8d3325c1e04ce2763
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-03 17:58:48 -0300

    creat Favorite Button e Faovorite pages

--- src/pages/RecipeFavorite.js
+++ src/pages/RecipeFavorite.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../components/Header';
 
 export default function RecipeFavorite() {
+  const [type, setType] = useState('');
+
+  const setTypeFavorite = ({ target }) => {
+    const { value } = target;
+    setType(value);
+  };
+
+  const getFavoriteLocalStorage = () => {
+    const Local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    console.log(Local);
+    if (Local) {
+      return Local.map((item, index) => (<div key={ index }>
+        <span data-testid="${index}-horizontal-top-text"> </span>
+       <h2> {item.name}</h2>
+                                         </div>));
+    }
+  };
+
   return (
     <div>
-      ola
+      <Header title="Receitas Favoritas" />
+      <button type="button" data-testid="filter-by-all-btn" value="" onClick={ setTypeFavorite }>All</button>
+      <button type="button" data-testid="filter-by-food-btn" value="Food" onClick={ setTypeFavorite }>Food</button>
+      <button type="button" data-testid="filter-by-drink-btn" value="Drinks" onClick={ setTypeFavorite }>Drinks</button>
+      <div>
+        { getFavoriteLocalStorage() }
+      </div>
     </div>
   );
 }
