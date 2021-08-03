3370f1d6c3947c4683c0dd4a6b4bba2402d6eb50
Ghislaine <glenox79@gmail.com>
2021-08-03 09:56:09 -0300

    Requisitos 70~73 | falta concluir 74

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-export default class ExplorarBebidas extends Component {
-  render() {
-    return (
-      <div>
-        <Header title="Explorar Bebidas" />
-        <Footer />
-      </div>
-    );
-  }
+export default function ExplorarBebidas() {
+  const [cocktail, setCocktail] = useState([]);
+
+  useEffect(() => {
+    const getExploreCocktails = async () => {
+      const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      const data = await fetch(endpoint);
+      const { results } = await data.json();
+      setCocktail(results);
+    };
+    getExploreCocktails();
+  }, []);
+
+  return (
+    <div>
+      <Header title="Explorar Bebidas" />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link
+        to={ {
+          pathname: `/bebidas/${cocktail}`,
+        } }
+      >
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
+      <Footer />
+    </div>
+  );
 }
