c1b58c97420cd354a6a6f9792c43b058e58e4db6
guinhoanjos <iago.anjos@hotmail.com>
2021-08-03 15:37:08 -0300

    criado componente pra renderizar

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import MealsCards from '../components/MealsCards';
 
 function FavoriteRecipes() {
+  const receitasFavoritas = JSON.parse(localStorage.getItem('favorites'));
+  console.log(receitasFavoritas);
   return (
-    <h3>
-      comidas favoritas
-    </h3>
+    <div>
+      <h3>
+        comidas favoritas
+      </h3>
+      <MealsCards meals={ receitasFavoritas } />
+    </div>
   );
 }
 
