c13b74c3f1ab2f53cb50e82e51bbb244508c1788
Marconi <marconieng@gmail.com>
2021-08-03 20:18:57 -0300

    Adds requirements from 82 to 87 regarding profile screen

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const localStorageEmail = localStorage.getItem('user');
+  const emailParse = JSON.parse(localStorageEmail);
+
+  const handleClick = () => {
+    localStorage.clear();
+  };
+
   return (
     <div>
       <Header title="Perfil" />
+      <p data-testid="profile-email">{ emailParse.email }</p>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClick }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
