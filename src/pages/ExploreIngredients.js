f78598d9955d6747dc8fdfee8c6a57d1f1737a7d
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-03 17:49:52 -0300

    Requisito 21 - adiciona o menu inferior em outras telas

--- src/pages/ExploreIngredients.js
+++ src/pages/ExploreIngredients.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function ExploreIngredients() {
   return (
-    <main>
-      <h1>Tela de Explorar Ingredientes</h1>
-    </main>
+    <>
+      <main>
+        <h1>Tela de Explorar Ingredientes</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
