4867e016914952048d170a1b386b7736f2a60b68
jorgemeyrelles <jotaengpuc@gmail.com>
2021-08-03 09:45:42 -0300

    [FEAT] inclusao de button no card e comp no foodrecipes

--- src/pages/foods/FoodRecipes.js
+++ src/pages/foods/FoodRecipes.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import InProgressCard from '../../components/InProgressCard';
 
 export default function FoodRecipes() {
   return (
     <div>
       Detalhes da comida
+      <InProgressCard />
     </div>
   );
 }
