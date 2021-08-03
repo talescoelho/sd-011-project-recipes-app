4d23bad2c9066a0f0bb83ec80eabccec479eb2cd
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-03 16:54:31 -0300

    Add login test

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { render, cleanup } from '@testing-library/react';
-import { createStore, combineReducers } from 'redux';
+import { screen, cleanup, fireEvent } from '@testing-library/react';
+import { renderWithRouterAndStore } from './helper/testConfig';
 import Login from '../pages/Login';
-import emailReducer from '../redux/reducers/emailReducer';
-
-const renderWithRedux = (component, { initialState,
-  store = createStore(combineReducers({ emailReducer }), initialState) } = {}) => (
-  { ...render(<Provider store={ store }>{component}</Provider>),
-    store });
 
 describe('Login Page tests', () => {
   beforeEach(cleanup);
 
   it('Verify inputs and button IDs', () => {
-    const { getByTestId } = renderWithRedux(<Login />);
-    const emailInput = getByTestId('email-input');
-    const passwordInput = getByTestId('password-input');
-    const button = getByTestId('login-submit-btn');
+    renderWithRouterAndStore(<Login />);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByTestId('login-submit-btn');
 
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  it('Verify food page redirect', () => {
+    const { history } = renderWithRouterAndStore(<Login />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByTestId('login-submit-btn');
+
+    expect(button).toHaveAttribute('disabled');
+
+    fireEvent.change(emailInput, { target: { value: 'teste@teste.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(button).not.toHaveAttribute('disabled');
+
+    fireEvent.click(button);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/comidas');
   });
 });
