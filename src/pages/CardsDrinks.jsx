2a27195bea496d074780c4dde2fc8b1f18bcf5d5
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-08-02 22:26:07 -0300

    adiciona features da tela de detalhes

---
+++ src/pages/CardsDrinks.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { fetchDrink } from '../services/DrinksApi';
+import '../styles/FoodDetails.css';
+
+export default function FoodDrinks() {
+  const params = useParams();
+  const [drink, setDrink] = useState();
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+    },
+  };
+
+  useEffect(() => {
+    const getDrink = async () => {
+      const data = await fetchDrink(params.id);
+      setDrink(data);
+      console.log(data);
+    };
+    getDrink();
+  }, [params.id]);
+
+  function returnDrink(index) {
+    if (drink) {
+      return drink.drinks[index];
+    }
+    return '';
+  }
+
+  function cardsDrinks() {
+    const drinks = [];
+    const drinksMax = 6;
+
+    for (let index = 1; index <= drinksMax; index += 1) {
+      if (returnDrink(index) !== '') {
+        drinks.push(
+          <div className="card">
+            <img className="imgdrinks" src={ returnDrink(index).strDrinkThumb } alt="imagem drink" />
+            <p>{returnDrink(index).strAlcoholic}</p>
+            <p>
+              {returnDrink(index).strDrink}
+              {' '}
+            </p>
+          </div>,
+        );
+      }
+    }
+
+    return drinks;
+  }
+  return (
+    <Carousel responsive={ responsive }>
+      { cardsDrinks() }
+    </Carousel>
+  );
+}
---
+++ src/services/DrinksApi.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/prefer-default-export
+export const fetchDrink = async () => {
+  const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  const response = await fetch(URL);
+  const json = await response.json();
+  try {
+    return json;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
