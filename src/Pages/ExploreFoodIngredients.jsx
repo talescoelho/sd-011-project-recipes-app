7d77937999db3246dd7aa0b7002713abbaec9cdc
Gustavo Lemes <gustavolemes997@gmail.com>
2021-08-03 17:16:45 -0300

    Requisito 75 e 76

--- src/Pages/ExploreFoodIngredients.jsx
+++ src/Pages/ExploreFoodIngredients.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import IngredientCard from '../Components/IngredientCard';
+import { getMealsIngredients } from '../Services/mealAPI';
 
 function ExploreFoodIngredients() {
+  const [ingredients, setIngredients] = useState();
+
+  React.useEffect(() => {
+    const fetch = async () => {
+      let data = await getMealsIngredients();
+      const TWELVE = 12;
+
+      data = data.filter((_, index) => index < TWELVE);
+      setIngredients(data);
+    }
+    fetch();
+  }, []);
+
+  if (!ingredients) return <p>Loading...</p>;
+
   return (
     <div>
       <Header pageTitle="Explorar Ingredientes" />
+      {ingredients.map(({strIngredient}, index) => (
+        <IngredientCard
+          name={ strIngredient }
+          index={ index }
+          thumb={`https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png`}
+        />
+      ))}
       <Footer />
     </div>);
 }
