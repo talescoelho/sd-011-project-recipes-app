bc6e58f09c40412dc451dc705cfe96ffdc7c3c0a
PH Assis <pieit@live.com>
2021-08-03 20:04:20 -0300

    fixed some bugs

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function DrinkDetails() {
   return (
-    <div />
+    <div>
   );
 }
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Dropdown, MenuItem } from 'react-bootstrap';
 import Header from '../components/Header';
 
 export default function User() {
   return (
-    <div>
+    <>
       <Header pageName="Perfil" />
-      <h1> Hello, this is my User Page</h1>
-    </div>
+      <main>
+        <h1> Hello, this is my User Page</h1>
+      </main>
+    </>
   );
 }
