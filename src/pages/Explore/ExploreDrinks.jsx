00edbcac9a261b72e145f7a4886db003fef2f33b
FredericoCanoeira <fcanoeira@gmail.com>
2021-08-04 00:41:32 +0100

    Requisitos 70 a 73

--- src/pages/Explore/ExploreDrinks.jsx
+++ src/pages/Explore/ExploreDrinks.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../globalComponents/Footer';
 import Header from '../../globalComponents/Header';
 
@@ -8,7 +9,26 @@ function ExploreDrinks({ match }) {
     <>
       <Header title="Explorar Bebidas" match={ match } />
       <div>
-        Drinks
+        <Link to="bebidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
       </div>
       <Footer />
     </>
--- src/pages/Explore/ExploreFoods.jsx
+++ src/pages/Explore/ExploreFoods.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../globalComponents/Footer';
 import Header from '../../globalComponents/Header';
 
@@ -8,7 +9,28 @@ function ExploreFoods({ match }) {
     <>
       <Header title="Explorar Comidas" match={ match } />
       <div>
-        Explore
+        <Link to="comidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="comidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
       </div>
       <Footer />
     </>
