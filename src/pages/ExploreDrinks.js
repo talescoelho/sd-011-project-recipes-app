19da80284043ee9f3ea78fec34d7109d1abe88d3
Diego Figueirêdo <8kball@gmail.com>
2021-08-03 13:05:30 -0300

    Get random meal

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
 import Header from '../components/Header';
 
@@ -6,6 +7,18 @@ export default function ExploreDrinks() {
   return (
     <div>
       <Header title="Explorar Bebidas" renderButton />
+      <Link
+        data-testid="explore-by-ingredient"
+        to="/explorar/bebidas/ingredientes"
+      >
+        Por Ingredientes
+      </Link>
+      <Link
+        data-testid="explore-surprise"
+        to="/bebidas/178319"
+      >
+        Me Surpreenda!
+      </Link>
       <LowerMenu />
     </div>
   );
--- src/pages/ExploreMeals.js
+++ src/pages/ExploreMeals.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
 import Header from '../components/Header';
+// import { fetchDefaultFoodsFromMealsDB } from '../services';
 
 export default function ExploreMeals() {
+  const [randomMealId] = useState('52771');
+
+  // const getRandomMeal = async () => {
+  //   const meals = await fetchDefaultFoodsFromMealsDB();
+  //   const randomMealIndex = Math.floor(Math.random() * meals.length);
+  //   setRandomMealId(meals[randomMealIndex].idMeal);
+  // };
+
+  // useEffect(() => getRandomMeal(), []);
+
   return (
     <div>
       <Header title="Explorar Comidas" renderButton />
+      <Link
+        data-testid="explore-by-ingredient"
+        to="/explorar/comidas/ingredientes"
+      >
+        Por Ingredientes
+      </Link>
+      <Link
+        data-testid="explore-by-area"
+        to="/explorar/comidas/area"
+      >
+        Por Local de Origem
+      </Link>
+      <Link
+        data-testid="explore-surprise"
+        to={ `/comidas/${randomMealId}` }
+      >
+        Me Surpreenda!
+      </Link>
       <LowerMenu />
     </div>
   );
