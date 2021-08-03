dd39dd216124814c319ae7ce3e5a1f389fa0d261
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-08-03 13:17:54 -0300

    Requisitos 28, 29 e 30

--- src/components/MainRecipes.jsx
+++ src/components/MainRecipes.jsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react';
 function MainRecipes({ foodOrDrink }) {
   const [mainItems, setMainItems] = useState();
   const [categories, setCategories] = useState();
+  const [buttonFilter, setButtonFilter] = useState();
   const [loading, setLoading] = useState(true);
+  const [filterButtonActive, setFilterButtonActive] = useState(false);
 
   const maxLengthItems = 12;
   const maxLengthCategories = 5;
@@ -30,16 +32,39 @@ function MainRecipes({ foodOrDrink }) {
       endPointItems = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
       endPointCategory = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
     }
+    if (filterButtonActive) {
+      endPointItems = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${buttonFilter}`;
+      if (foodOrDrink === 'Bebidas') {
+        endPointItems = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${buttonFilter}`;
+      }
+    }
     const requireItems = await fetch(endPointItems);
     const responseItems = await requireItems.json();
-    const dataItems = foodOrDrink === 'Bebidas' ? responseItems.drinks : responseItems.meals;
+    const dataItems = foodOrDrink === 'Bebidas' ? responseItems.drinks
+      : responseItems.meals;
     const requireCategory = await fetch(endPointCategory);
     const responseCategory = await requireCategory.json();
-    const dataCategory = foodOrDrink === 'Bebidas' ? responseCategory.drinks : responseCategory.meals;
+    const dataCategory = foodOrDrink === 'Bebidas' ? responseCategory.drinks
+      : responseCategory.meals;
     setMainItems(dataItems);
     setCategories(dataCategory);
     setLoading(false);
   }
+
+  function handleClick(e) {
+    if (e.target.value === buttonFilter) {
+      setFilterButtonActive(false);
+      setLoading(true);
+      return;
+    }
+    setLoading(true);
+    setButtonFilter(e.target.value);
+    setFilterButtonActive(true);
+  }
+
+  useEffect(() => {
+    fetchMainRecipes();
+  }, [buttonFilter, filterButtonActive]);
 
   useEffect(() => {
     fetchMainRecipes();
@@ -47,25 +72,30 @@ function MainRecipes({ foodOrDrink }) {
 
   return (
     <div>
-      <div>
-        {loading ? <span>Carregando...</span> : (
-          categories.filter((item, index) => index < maxLengthCategories)
-            .map((item, index) => (
-              <button
-                key={ index }
-                type="button"
-                data-testid={ `${item.strCategory}-category-filter` }
-              >
-                { item.strCategory }
-              </button>
-            ))
-        )}
-      </div>
-      <div>
-        {loading ? <span>Carregando...</span> : (
-          mainItems.filter((item, index) => index < maxLengthItems).map(renderRecipes)
-        )}
-      </div>
+      {loading ? <span>Carregando...</span> : (
+        <div>
+          <div>
+            {(
+              categories.filter((item, index) => index < maxLengthCategories)
+                .map((item, index) => (
+                  <button
+                    key={ index }
+                    type="button"
+                    data-testid={ `${item.strCategory}-category-filter` }
+                    value={ item.strCategory }
+                    onClick={ (e) => handleClick(e) }
+                  >
+                    { item.strCategory }
+                  </button>
+                ))
+            )}
+          </div>
+          <div>
+            {mainItems
+              .filter((item, index) => index < maxLengthItems).map(renderRecipes) }
+          </div>
+        </div>
+      )}
     </div>
   );
 }
