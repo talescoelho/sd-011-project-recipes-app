4f84915b3281e0370fdf6e0749e3aac3f0a22986
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-03 17:14:49 -0300

    Retira codigo comentado

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -19,22 +19,6 @@ describe('Requirement 19 ', () => {
 });
 
 describe('Requirement 20', () => {
-/*   it('Should have a footer be fixed to the bottom of the page', () => {
-    render(<Footer />);
-    // https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
-    // https://the-teacher.medium.com/how-to-test-a-react-components-css-styles-with-react-testing-library-rtl-styled-components-43f744334528
-    const footerClass = document.getElementsByClassName(footer);
-    console.log(footerClass);
-    const styleFooter = window.getComputedStyle(footerClass);
-    expect(styleFooter.bottom).toBe('0');
-    console.log(style.position);
-    // https://github.com/testing-library/jest-dom/tree/v4.2.4#tohavestyle
-    expect(screen.getByTestId(footer)).toHaveStyle(`
-      bottom: 0;
-      position: 'fixed';
-    `);
-  }); */
-
   it('Should have a correct icons', () => {
     render(<Footer />);
 
