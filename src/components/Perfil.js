ad38f031d35f2eb70692f63f0c0e9dda2413d936
Luiz Otavio <luiz.otavio273@gmail.com>
2021-08-03 18:04:13 -0300

    req 82

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Perfil() {
   return (
-    <h2 data-testid="page-title">Perfil</h2>
+    <>
+      <h2 data-testid="page-title">Perfil</h2>
+      <span data-testid="profile-email">Email</span>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
+    </>
   );
 }
 
