b1a768585343a585bbaed3d90f89737ab8706b1f
Alexandre <naitwa.alexandre@gmail.com>
2021-08-03 16:58:20 -0300

    requisito 33

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import RecipesContext from '../context/RecipesContext';
 
-function Details({ match: { url, params: { id } } }) {
+function Details() {
+  const { id } = useParams();
+  const { mealsData } = useContext(RecipesContext);
+  const singleMeal = mealsData.filter((meal) => meal.idMeal === id);
+  const {
+    strMealThumb: thumbnail,
+    strMeal: name,
+    strCategory: category,
+  } = singleMeal[0] || [];
+
+  console.log(singleMeal);
   return (
-    // teste
-    <div>
-      { url.includes('comidas') ? <p>É uma comida</p> : <p>É uma bebida</p> }
-      <p>
-        O id dessa receita é
-        <span>{ id }</span>
-      </p>
-    </div>
+    <article>
+      <div>
+        <img data-testid="recipe-photo" src={ thumbnail } alt={ name } />
+        <h2 data-testid="recipe-title">{ name }</h2>
+        <p data-testid="recipe-category">{ category }</p>
+        <div className="buttons">
+          <button data-testid="share-btn" type="button">
+            compartilhar
+          </button>
+          <button data-testid="favorite-btn" type="button">
+            favoritar
+          </button>
+        </div>
+      </div>
+    </article>
   );
 }
-
-Details.propTypes = {
-  match: PropTypes.objectOf().isRequired,
-};
 
 export default Details;
