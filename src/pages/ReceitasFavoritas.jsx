fba0ca6c25108ac9f8234065746e4ddf6bbcaef7
LeoFuna <leo.funa@gmail.com>
2021-08-03 16:57:22 -0300

    Requisito 66

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import FiltersDoneAndFavorites from '../components/FiltersDoneAndFavorites';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
@@ -52,6 +53,11 @@ export default class ReceitasFavoritas extends Component {
     });
   }
 
+  redirectToRecipeDetails(id, foodOrDrink) {
+    const { history } = this.props;
+    history.push(`/${foodOrDrink}/${id}`);
+  }
+
   shareLinkClick(id, foodOrDrink) {
     const magicNumber = 2000;
     navigator.clipboard.writeText(`${window.location.origin}/${foodOrDrink}/${id}`);
@@ -67,13 +73,20 @@ export default class ReceitasFavoritas extends Component {
       if (item.type === 'comida') {
         return (
           <div key={ item.id }>
-            <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ item.image }
-              alt="imagem comida"
-              style={ { width: '50px' } }
-            />
+            <div
+              onClick={ () => this.redirectToRecipeDetails(item.id, 'comidas') }
+              onKeyDown={ () => this.redirectToRecipeDetails(item.id, 'comidas') }
+              role="button"
+              tabIndex="0"
+            >
+              <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ item.image }
+                alt="imagem comida"
+                style={ { width: '50px' } }
+              />
+            </div>
             <p data-testid={ `${index}-horizontal-top-text` }>
               { item.area }
               {' '}
@@ -103,13 +116,20 @@ export default class ReceitasFavoritas extends Component {
       }
       return (
         <div key={ item.id }>
-          <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            src={ item.image }
-            alt="imagem bebida"
-            style={ { width: '50px' } }
-          />
+          <div
+            onClick={ () => this.redirectToRecipeDetails(item.id, 'bebidas') }
+            onKeyDown={ () => this.redirectToRecipeDetails(item.id, 'bebidas') }
+            role="button"
+            tabIndex="0"
+          >
+            <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ item.image }
+              alt="imagem bebida"
+              style={ { width: '50px' } }
+            />
+          </div>
           <p data-testid={ `${index}-horizontal-top-text` }>{ item.alcoholicOrNot }</p>
           <button onClick={ () => this.shareLinkClick(item.id, 'bebidas') } type="button">
             <img
@@ -140,3 +160,9 @@ export default class ReceitasFavoritas extends Component {
     );
   }
 }
+
+ReceitasFavoritas.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
