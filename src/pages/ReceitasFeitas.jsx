d1c8ad02ab0d86281a8089d30855d30babd32e8e
fernando lasmar <f3rnandolasmar@gmail.com>
2021-08-03 19:29:37 -0300

    requisito 54 ao 57

--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,12 +1,134 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import FiltersDoneAndFavorites from '../components/FiltersDoneAndFavorites';
+import shareIcon from '../images/shareIcon.svg';
 
 export default class ReceitasFeitas extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemsToRender: [],
+      shareButton: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setItemsToRender();
+  }
+
+  setItemsToRender() {
+    this.setState({
+      itemsToRender: JSON.parse(localStorage.getItem('doneRecipes')),
+    });
+  }
+
+  shareLinkClick(id, foodOrDrink) {
+    const magicNumber = 2000;
+    navigator.clipboard.writeText(`${window.location.origin}/${foodOrDrink}/${id}`);
+    this.setState({ shareButton: true });
+    setTimeout(() => this.setState({
+      shareButton: false,
+    }), magicNumber);
+  }
+
+  renderItems() {
+    const { itemsToRender, shareButton } = this.state;
+    return itemsToRender.map((item, index) => {
+      if (item.type === 'comida') {
+        return (
+          <div key={ index }>
+            <img
+              src={ item.image }
+              alt="recipe representation"
+              data-testid={ `${index}-horizontal-image` }
+            />
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {item.area}
+              {' '}
+              -
+              {' '}
+              {item.category}
+            </p>
+            <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
+            <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
+            {
+              item.tags.map(
+                (tag, index2) => {
+                  const finalIndex = 2;
+                  if (index2 < finalIndex) {
+                    return (
+                      <p
+                        key={ index2 }
+                        data-testid={ `${index}-${tag}-horizontal-tag` }
+                      >
+                        {tag}
+                      </p>);
+                  }
+                  return console.log('teste');
+                },
+
+              )
+            }
+            <button
+              onClick={ () => this.shareLinkClick(item.id, 'comidas') }
+              type="button"
+            >
+              <img
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="share"
+              />
+            </button>
+            {shareButton ? <span style={ { color: 'red' } }>Link copiado!</span> : null}
+          </div>
+        );
+      }
+      return (
+        <div key={ index }>
+          <img
+            src={ item.image }
+            alt="recipe representation"
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p data-testid={ `${index}-horizontal-top-text` }>{item.alcoholicOrNot}</p>
+          <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
+          <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
+
+          <button
+            onClick={ () => this.shareLinkClick(item.id, 'bebidas') }
+            type="button"
+          >
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              alt="share"
+            />
+          </button>
+          {shareButton ? <span style={ { color: 'red' } }>Link copiado!</span> : null}
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
         <Header title="Receitas Feitas" />
+        <FiltersDoneAndFavorites />
+        { this.renderItems() }
       </div>
     );
   }
 }
+
+// [{
+//   id: id-da-receita,
+//   type: comida-ou-bebida,
+//   area: area-da-receita-ou-texto-vazio,
+//   category: categoria-da-receita-ou-texto-vazio,
+//   alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
+//   name: nome-da-receita,
+//   image: imagem-da-receita,
+//   doneDate: quando-a-receita-foi-concluida,
+//   tags: array-de-tags-da-receita-ou-array-vazio
+// }]
