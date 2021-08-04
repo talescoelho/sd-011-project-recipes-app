ec6f0c8b48184bfd4c8731d00fa6a1e6943330de
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-04 15:45:44 -0300

    Link on explore food page

--- src/pages/explore/ExploreRecipes.js
+++ src/pages/explore/ExploreRecipes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 
@@ -6,6 +7,32 @@ export default function ExploreRecipes() {
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <div>
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            data-testid="explore-by-ingredient"
+            type="button"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            data-testid="explore-by-area"
+            type="button"
+          >
+            Por Area
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            data-testid="explore-surprise"
+            type="button"
+          >
+            Por Area
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
