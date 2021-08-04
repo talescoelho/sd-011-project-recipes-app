1a707c7947d5fc40a230999360630cde7cefebc5
Cris Lustosa <cmlustosa@gmail.com>
2021-08-04 15:38:42 -0300

    Adiciona requisitos da tela de perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Layout } from '../components';
 import { useTheme } from '../hooks';
 
@@ -11,11 +12,39 @@ function Perfil() {
       color: colors.text400,
     },
   };
+  const emailUser = localStorage.getItem('user');
+  const history = useHistory();
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
 
   return (
     <Layout title="Perfil">
       <main style={ styles.main }>
-        <p>ola mundo</p>
+        <h2 data-testid="page-title">Perfil</h2>
+        <span data-testid="profile-email">{ `Email${emailUser} `}</span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logout }
+        >
+          Sair
+        </button>
       </main>
     </Layout>
   );
