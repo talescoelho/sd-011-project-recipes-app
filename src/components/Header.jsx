ed2a2f0aff72f71664252ce5e2a87a87fb9d2615
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-04 16:26:12 -0300

    corrigido erro da tela search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ function Header({ title, search = true }) {
             />
           </button>)}
       </header>
-      { renderSearchBar && <SearchBar /> }
+      { renderSearchBar && <SearchBar title={ title } /> }
     </div>
   );
 }
