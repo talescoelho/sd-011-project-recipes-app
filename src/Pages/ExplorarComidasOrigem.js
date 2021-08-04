5cc6f1aba0cdb354b417c407f1b8a7b64b46bea7
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-04 15:20:29 -0300

    Fix: Retira página de explorar comidas por origem

--- src/Pages/ExplorarComidasOrigem.js
+++ src/Pages/ExplorarComidasOrigem.js
@@ -1,26 +1,27 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 //  import { useSelector, useDispatch } from 'react-redux';
-import { getRecipes } from '../redux/slices/fetchReceitas';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import RenderFoodAreaOptions from '../components/RenderFoodAreaOptions';
+// import { getRecipes } from '../redux/slices/fetchReceitas';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import RenderFoodAreaOptions from '../components/RenderFoodAreaOptions';
 
 function ExplorarComidasOrigem({ title }) {
   // const { foodAreaList } = useSelector((state) => state.fetchReceitas);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getRecipes('foodAreaList'));
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getRecipes('foodAreaList'));
+  // }, [dispatch]);
 
-  return (
-    <div>
-      <Header title={ title } />
-      <RenderFoodAreaOptions />
-      <Footer />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Header title={ title } />
+  //     <RenderFoodAreaOptions />
+  //     <Footer />
+  //   </div>
+  // );
+  return null
 }
 
 export default ExplorarComidasOrigem;
