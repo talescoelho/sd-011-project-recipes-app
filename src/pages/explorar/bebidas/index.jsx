db56ee9317b90fdfa8edabd76e24b8581f567284
Savio Moraes <smmsavio@hotmail.com>
2021-08-04 19:11:53 -0300

    end page explore_drinks_or_meals

--- src/pages/explorar/bebidas/index.jsx
+++ src/pages/explorar/bebidas/index.jsx
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../../../components/Footer';
 import Header from '../../../components/Header';
+import randomFetchApiDrink from '../../../services/randomFetchApiDrink';
 
-export default class ExplorarBebidas extends Component {
-  render() {
-    return (
-      <div>
-        <Header title="Explorar Bebidas" />
-        Explorar bebidas
-        <Footer />
-      </div>
-    );
-  }
+export default function ExplorarBebidas() {
+  const history = useHistory();
+  const handleClick = async () => {
+    const randomFetchDrink = await randomFetchApiDrink();
+    const id = randomFetchDrink[0].idDrink;
+    history.push(`/bebidas/${id}`);
+  };
+
+  return (
+    <div>
+      <Header title="Explorar Bebidas" />
+      <main>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleClick }
+        >
+          Me Surpreenda!
+        </button>
+      </main>
+      <Footer />
+    </div>
+  );
 }
