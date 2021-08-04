0e75170d5d98777b78940a8e709adc1c79cc03ba
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-08-04 18:58:55 -0300

    Requisitos 82-86: redirecionar rotas

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Perfil() {
   const userEmail = JSON.parse(localStorage.user);
   console.log(userEmail.email);
+  const history = useHistory();
+  const redirectToDone = () => history.push('/receitas-feitas');
+  const redirectToFavorites = () => history.push('/receitas-favoritas');
   return (
     <div>
       <Header title="Perfil" search={ false } />
       <h2 data-testid="profile-email">{ userEmail.email }</h2>
-      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      <button type="button" data-testid="profile-logout-btn">Sair</button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ redirectToDone }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ redirectToFavorites }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
