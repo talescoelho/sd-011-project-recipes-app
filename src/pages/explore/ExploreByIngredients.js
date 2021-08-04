9a6c783f2ddade90cc12ea5018fdac4c5722beae
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-04 17:18:55 -0300

    create function fetchIngredients

--- src/pages/explore/ExploreByIngredients.js
+++ src/pages/explore/ExploreByIngredients.js
@@ -3,9 +3,21 @@ import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 
 export default function ExploreByIngredients() {
+  const [data, setData] = React.useState([]);
+  const fetchIngredients = async () =>{
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    const response = await fetch(url);
+    const dataFetch = await response.json();
+    setData([...dataFetch.meals]);
+  };
+
+  React.useEffect(() => {
+    fetchIngredients();
+  }, []);
   return (
     <div>
       <Header title="Explorar Ingredientes" />
+      
       <Footer />
     </div>
   );
