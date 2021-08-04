6be4f2c0c899ed81d3bf485138a2a9f9c2f661c3
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-04 16:47:49 -0300

    redirecting the surprise button on the explore drinks page

--- src/pages/explore/ExploreDrinks.js
+++ src/pages/explore/ExploreDrinks.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 
 export default function ExploreDrinks() {
+  const [surprise, setSurprise] = React.useState('');
+  const [redirect, setRedirect] = React.useState(false);
+  const handlerSurprise = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const data = await response.json();
+    setSurprise(`/comidas/${data.drinks[0].idDrink}`);
+    setRedirect(true);
+  };
   return (
     <div>
       <Header title="Explorar Bebidas" />
@@ -16,14 +25,15 @@ export default function ExploreDrinks() {
             Por Ingredientes
           </button>
         </Link>
-        <Link to="/explorar/bebidas/ingredientes">
-          <button
-            data-testid="explore-surprise"
-            type="button"
-          >
-            Me Surpreenda!
-          </button>
-        </Link>
+        <button
+          data-testid="explore-surprise"
+          type="button"
+          onClick={ handlerSurprise }
+        >
+          Me Surpreenda!
+        </button>
+        {redirect && <Redirect to={ surprise } />}
+
       </div>
       <Footer />
     </div>
