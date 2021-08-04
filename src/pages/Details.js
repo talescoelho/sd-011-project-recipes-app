30def9ca72173e2cc0ff121ffe6aae8d4aa9f0c1
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-08-04 17:13:19 -0300

    Requisito 40

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -25,10 +25,27 @@ function Details({ match: { url, params: { id } } }) {
     setRecommended(searchRecommendedRecipe);
   };
 
+  // Verifica se a receita foi finalizada a partir do id dela no localStorage
   const verifyRecipeIsDone = () => {
     const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
     if (doneRecipes.length > 0) {
       return doneRecipes.some((recipe) => recipe.id === id);
+    }
+    return false;
+  };
+
+  // Verifica se a receita está "em progresso" a partir do id no localStorage
+  const checkRecipeInProgress = () => {
+    const recipeInProgress = JSON.parse(localStorage.getItem('inProgressRecipes')) || {};
+    if (Object.keys(recipeInProgress).length > 0) {
+      if (url.includes('comidas')) {
+        return Object.keys(recipeInProgress.meals)
+          .some((recipeId) => recipeId === id);
+      }
+      if (url.includes('bebidas')) {
+        return Object.keys(recipeInProgress.cocktails)
+          .some((recipeId) => recipeId === id);
+      }
     }
     return false;
   };
@@ -49,7 +66,7 @@ function Details({ match: { url, params: { id } } }) {
         type="button"
         data-testid="start-recipe-btn"
       >
-        Iniciar Receita
+        {checkRecipeInProgress() ? 'Continuar Receita' : 'Iniciar Receita' }
       </button>
 
     </div>
