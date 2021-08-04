3d050ff40c804b18acee96e5aed2bea7a651d5dc
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-04 16:42:43 -0300

    doing the surprise button redirect

--- src/pages/explore/ExploreRecipes.js
+++ src/pages/explore/ExploreRecipes.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 
 export default function ExploreRecipes() {
-  const handlerSurprise = () => {
+  const [surprise, setSurprise] = React.useState('');
+  const [redirect, setRedirect] = React.useState(false);
+  const handlerSurprise = async () => {
     const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const data = await response.json();
+    setSurprise(`/comidas/${data.meals[0].idMeal}`);
+    setRedirect(true);
   };
+
   return (
     <div>
       <Header title="Explorar Comidas" />
@@ -27,14 +34,14 @@ export default function ExploreRecipes() {
             Por Local de Origem
           </button>
         </Link>
-        <Link to="/explorar/comidas/ingredientes">
-          <button
-            data-testid="explore-surprise"
-            type="button"
-          >
-            Me Surpreenda!
-          </button>
-        </Link>
+        <button
+          data-testid="explore-surprise"
+          type="button"
+          onClick={ handlerSurprise }
+        >
+          Me Surpreenda!
+        </button>
+        {redirect && <Redirect to={ surprise } />}
       </div>
       <Footer />
     </div>
