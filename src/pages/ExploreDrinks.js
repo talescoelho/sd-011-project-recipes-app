8462925e2bb812c5bd3edec4a83f2c21ff266c03
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-04 19:09:54 -0300

    requisitos 70 e 71 concluidos

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -6,6 +6,18 @@ export default function ExploreDrinks() {
   return (
     <div>
       <Header title="Explorar Bebidas" search={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -6,6 +6,24 @@ export default function ExploreFoods() {
   return (
     <div>
       <Header title="Explorar Comidas" search={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
