50cbd256545ce64a2a6befeaed70f8a717636470
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-08-04 17:39:57 -0300

    Feat: done recipes - creating buttons

--- src/pages/RecipesDone.js
+++ src/pages/RecipesDone.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
 
-export default function RecipesDone() {
+function RecipesDone() {
   return (
     <div>
-      <Header />
+      <Header title="Receitas Feitas" isSearch={ false } />
+      <nav>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </nav>
     </div>
   );
 }
+
+export default RecipesDone;
