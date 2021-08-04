778bda30c6de857bcd8c6e4d0a2df1b07868a75f
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-04 19:37:46 -0300

    Criando arquivos

--- src/Pages/DetailsRecipes/DetailsRecipesFoods.jsx
+++ src/Pages/DetailsRecipes/DetailsRecipesFoods.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import MainContext from '../../Context/MainContext';
-import { getDetailsFoodById } from '../../Services/ApiFood';
 
 function DetailsRecipesFoods() {
   const location = useLocation();
@@ -11,38 +10,15 @@ function DetailsRecipesFoods() {
     const URL = location.pathname;
     const id = URL.replace('/comidas/', '');
     setIdFoods(id);
-    // console.log(id);
   }, [location, setIdFoods]);
-
-  // // &===========================================================================
-  // // async function getAPIById() {
-  // //   if (idFoods) {
-  // //     const foodByIngredientsAPI = await getDetailsFoodById(idFoods);
-  // //     setIdFoodsAPI(foodByIngredientsAPI);
-  // //   }
-  // // }
-
-  // useEffect(() => {
-  //   const getAPIById = async () => {
-  //     console.log(idFoods);
-  //     const foodByIngredientsAPI = await getDetailsFoodById(idFoods);
-  //     setIdFoodsAPI(foodByIngredientsAPI);
-  //   };
-  //   getAPIById();
-  // }, [idFoods]);
-  // // &===========================================================================
-  // useEffect(() => console.log(idFoodsAPI), [idFoodsAPI]);
 
   // !===========================================================================
   useEffect(() => {
     const getAPIById = async () => {
       const endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idFoods}`;
-      // const endpoint = 'www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
       const { meals } = await fetch(endpoint).then((data) => data.json());
-      // const results = await fetch(endpoint).then((data) => data.json());
       console.log(meals[0]);
       setIdFoodsAPI(meals[0]);
-      // console.log(idFoodsAPI);
     };
     getAPIById();
   }, [idFoods]);
