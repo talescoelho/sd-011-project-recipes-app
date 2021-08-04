ca5a1b2c2053652874d40df0b1fdfcde06651481
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-04 02:53:19 -0300

    add get itens by radio and text

---
+++ src/Context/RequestHook.js
@@ -0,0 +1,88 @@
+import React, { createContext, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  searchByFirstLetter,
+  searchByIngredient,
+  searchByName,
+} from '../services/RequestFood';
+
+import {
+  searchFirstLetter,
+  searchIngredient,
+  searchName,
+} from '../services/RequestDrinks';
+
+const RequestContext = createContext();
+
+export function RequestProvider({ children }) {
+  const [filteredFood, setFilteredFood] = useState([]);
+  const [filteredDrink, setFilteredDrink] = useState([]);
+
+  async function filterByNameFood(filterText) {
+    if (!filterText) return;
+    const items = await searchByName(filterText);
+
+    setFilteredFood(items);
+  }
+
+  async function filterByIngredientFood(filterText) {
+    if (!filterText) return;
+    const items = await searchByIngredient(filterText);
+
+    setFilteredFood(items);
+  }
+
+  async function filterByFirstLetterFood(filterText) {
+    if (!filterText) return;
+    const items = await searchByFirstLetter(filterText);
+
+    setFilteredFood(items);
+  }
+
+  async function filterByNameDrink(filterText) {
+    if (!filterText) return;
+    const items = await searchName(filterText);
+
+    setFilteredDrink(items);
+  }
+
+  async function filterByIngredientDrink(filterText) {
+    if (!filterText) return;
+    const items = await searchIngredient(filterText);
+
+    setFilteredDrink(items);
+  }
+
+  async function filterByFirstLetterDrink(filterText) {
+    if (!filterText) return;
+    const items = await searchFirstLetter(filterText);
+
+    setFilteredDrink(items);
+  }
+
+  const contextValues = {
+    filteredFood,
+    setFilteredFood,
+    filteredDrink,
+    setFilteredDrink,
+    filterByNameFood,
+    filterByIngredientFood,
+    filterByFirstLetterFood,
+    filterByNameDrink,
+    filterByIngredientDrink,
+    filterByFirstLetterDrink,
+  };
+
+  return (
+    <RequestContext.Provider value={ contextValues }>
+      { children }
+    </RequestContext.Provider>
+  );
+}
+
+RequestProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const RequestHook = () => useContext(RequestContext);
--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import { RequestHook } from '../Context/RequestHook';
+
 function drinkPage() {
+  const { filteredDrink } = RequestHook();
+
   return (
     <div>
       <Header title="Bebidas" search />
-      Page drink
+
+      { filteredDrink.length > 0
+        ? filteredDrink.map((drink) => (
+          <div key={ drink.strDrink }>
+            <h3>
+              { drink.strDrink }
+            </h3>
+          </div>
+        ))
+        : 'vazio' }
+
     </div>
   );
 }
