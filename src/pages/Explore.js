2172d7ebf0630ce09289c9f5295ff4a4327b51aa
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-04 17:47:23 -0300

    requisito 67 concluido

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -6,6 +6,8 @@ export default function Explore() {
   return (
     <div>
       <Header title="Explorar" search={ false } />
+      <button type="button" data-testid="explore-food">Explorar comidas</button>
+      <button type="button" data-testid="explore-drinks">Explorar bebidas</button>
       <Footer />
     </div>
   );
