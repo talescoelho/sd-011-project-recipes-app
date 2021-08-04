10f61fc62b92bce5bcd5d5a210599cb001a44638
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-08-04 17:47:13 -0300

    Feat: recipes done - get data from localStorage; filterByType

--- src/pages/RecipesDone.js
+++ src/pages/RecipesDone.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function RecipesDone() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+
+  const [filterRecipes, setFilterRecipes] = React.useState(doneRecipes);
+
+  function buttonsFilters(parameter) {
+    if (parameter === 'all') {
+      setFilterRecipes(doneRecipes && doneRecipes.filter(({ type }) => type));
+    }
+    if (parameter === 'food') {
+      setFilterRecipes(doneRecipes
+        && doneRecipes.filter(({ type }) => type === 'comida'));
+    } if (parameter === 'drinks') {
+      setFilterRecipes(doneRecipes
+        && doneRecipes.filter(({ type }) => type === 'bebida'));
+    }
+  }
+
   return (
     <div>
       <Header title="Receitas Feitas" isSearch={ false } />
@@ -9,22 +27,79 @@ function RecipesDone() {
         <button
           type="button"
           data-testid="filter-by-all-btn"
+          onClick={ () => buttonsFilters('all') }
         >
           All
         </button>
         <button
           type="button"
           data-testid="filter-by-food-btn"
+          onClick={ () => buttonsFilters('food') }
         >
           Food
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ () => buttonsFilters('drinks') }
         >
           Drinks
         </button>
       </nav>
+      {filterRecipes && filterRecipes
+        .map(
+          ({ id, type, area, category, alcoholicOrNot,
+            name, image, doneDate, tags }, index) => (
+            type === 'comida'
+              ? (
+                <div
+                  key={ id }
+                >
+                  <div>
+                    <Link to={ `/${type}s/${id}` }>
+                      <img
+                        src={ image }
+                        alt="recipe"
+                        data-testid={ `${index}-horizontal-image` }
+                      />
+                      <p
+                        data-testid={ `${index}-horizontal-name` }
+                      >
+                        {name}
+                      </p>
+                    </Link>
+                    <p data-testid={ `${index}-horizontal-top-text` }>
+                      {`${area} - ${category}`}
+                    </p>
+                    <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
+                    <p data-testid={ `${index}-${tags[0]}-horizontal-tag` }>
+                      {tags[0]}
+                    </p>
+                    <p data-testid={ `${index}-${tags[1]}-horizontal-tag` }>
+                      {tags[1]}
+                    </p>
+                  </div>
+                </div>)
+              : (
+                <div key={ id }>
+                  <Link to={ `/${type}s/${id}` }>
+                    <img
+                      src={ image }
+                      alt="recipe"
+                      data-testid={ `${index}-horizontal-image` }
+                    />
+                    <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+                  </Link>
+                  <p data-testid={ `${index}-horizontal-top-text` }>{alcoholicOrNot}</p>
+                  <p
+                    data-testid={ `${index}-horizontal-done-date` }
+                  >
+                    {doneDate}
+
+                  </p>
+                </div>)
+          ),
+        )}
     </div>
   );
 }
