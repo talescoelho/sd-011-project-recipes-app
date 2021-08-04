65f54d5cd3d794cd6b762a661a6e044b26e123e6
marlon307 <marlon-ramosb@hotmail.com>
2021-08-04 15:37:51 -0300

    [Group - 18] Recipies in progress-Marlon 50update

--- src/components/IngredientRecipes.js
+++ src/components/IngredientRecipes.js
@@ -3,34 +3,34 @@ import React from 'react';
 const IngredientRecipes = ({ ingredient, typeDrinkorMeal, idItem }) => {
   const typeDoM = typeDrinkorMeal === 'comidas' ? 'meals' : 'cocktails';
 
-  function addCheck(value) {
-    const info = JSON.parse(localStorage.getItem('inProgressRecipes'));
-    localStorage.setItem('inProgressRecipes', JSON.stringify({
-      ...info,
-      [typeDoM]: {
-        [idItem]: [...info[typeDoM][idItem], value],
-      },
-    }));
-  }
+  // function addCheck(value) {
+  //   const info = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  //   localStorage.setItem('inProgressRecipes', JSON.stringify({
+  //     ...info,
+  //     [typeDoM]: {
+  //       [idItem]: [...info[typeDoM][idItem], value],
+  //     },
+  //   }));
+  // }
 
-  function removeCheck(value) {
-    const info = JSON.parse(localStorage.getItem('inProgressRecipes'));
-    info[typeDoM][idItem].forEach((itemCheck) => {
-      if (itemCheck === value) {
-        localStorage.setItem('inProgressRecipes', JSON.stringify({
-          ...info,
-          [typeDoM]: {
-            [idItem]: [...info[typeDoM][idItem].filter((item) => item !== value)],
-          },
-        }));
-      }
-    });
-  }
+  // function removeCheck(value) {
+  //   const info = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  //   info[typeDoM][idItem].forEach((itemCheck) => {
+  //     if (itemCheck === value) {
+  //       localStorage.setItem('inProgressRecipes', JSON.stringify({
+  //         ...info,
+  //         [typeDoM]: {
+  //           [idItem]: [...info[typeDoM][idItem].filter((item) => item !== value)],
+  //         },
+  //       }));
+  //     }
+  //   });
+  // }
 
-  function recipiesPorgress(target, value) {
-    if (target.checked) addCheck(value);
-    else removeCheck(value);
-  }
+  // function recipiesPorgress(target, value) {
+  //   if (target.checked) addCheck(value);
+  //   else removeCheck(value);
+  // }
 
   function stateCheckd(value) {
     const info = JSON.parse(localStorage.getItem('inProgressRecipes'));
@@ -44,12 +44,11 @@ const IngredientRecipes = ({ ingredient, typeDrinkorMeal, idItem }) => {
         htmlFor={ index }
         data-testid={ `${index}-ingredient-step` }
       >
-
         <input
           type="checkbox"
           id={ index }
           checked={ () => stateCheckd(index) }
-        // onClick={ ({ target }) => recipiesPorgress(target, index) }
+        // onClick={ ({ target }) => recipiesPorgress({ target }, index) }
         />
         { item }
       </label>
