49f00ff61e8db5a7ddeb4cdf10ca99b6d62ea9ec
Wilquer82 <wilquer.fig@gmail.com>
2021-08-04 15:35:05 -0300

    perfil css acerto da imagem para pagina login

--- src/Pages/RecipesFoods.js
+++ src/Pages/RecipesFoods.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer'
+import './RecipesFoods.css';
 
 export default function RecipesFood() {
   return (
     <div>
-      <Header title="Comidas" searchIconAppears />
-      <h1>Tela principal de comidas</h1>
+      <Header className="title" title="Comidas" searchIconAppears />
+      <h2 className="title">Tela principal de comidas</h2>
+      
+      <Footer />
     </div>
   );
 }
