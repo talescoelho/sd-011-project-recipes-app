9056257a60d6960b9e2a86fa1ea40f978a75d86e
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-08-04 17:51:19 -0300

    requisito 69 concluido

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,8 +7,12 @@ export default function Explore() {
   return (
     <div>
       <Header title="Explorar" search={ false } />
-      <button type="button" data-testid="explore-food">Explorar Comidas</button>
-      <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
