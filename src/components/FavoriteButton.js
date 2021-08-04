3e17bf2f9daab09a53446264de83b7c95b2f719b
Nikolas <nikolas.silva1990@gmail.com>
2021-08-04 17:34:11 -0300

    [Recipe Details] Requisito 44 em andamento

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function FavoriteButton({ recipe, type }) {
   const [btnIsFavorited, setBtnIsFavorited] = useState(false);
+  const idType = type === 'bebidas' ? 'idDrink' : 'idMeal';
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('favoriteRecipes'))) {
+      const storageRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const verify = storageRecipes.some((recipee) => (
+        recipee[idType] === storageRecipes[idType]
+      ));
+      if (verify) {
+        setBtnIsFavorited(true);
+      } else {
+        setBtnIsFavorited(false);
+      }
+    }
+  }, [idType]);
+
   const [status, setStatus] = useState('despreenchido');
 
   function setFavoriteRecipeOnLocalStorage() {
@@ -29,9 +45,24 @@ function FavoriteButton({ recipe, type }) {
     };
 
     const recipeFavorited = type === 'bebida' ? drinkObj : mealObj;
+    const favoriteSupply = [];
+    if (status === 'despreenchido') {
+      const recipesStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      favoriteSupply.push(recipeFavorited);
+      if (recipesStorage) {
+        recipesStorage.forEach((element) => {
+          favoriteSupply.push(element);
+        });
+      }
+      localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteSupply));
+    } else {
+      const recipesStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const recipesFiltered = recipesStorage.filter((recipee) => {
+        return recipee[idType] !== recipe[idType];
+      });
+      localStorage.setItem('favoriteRecipes', JSON.stringify(recipesFiltered));
+    }
     console.log(recipe);
-
-    localStorage.setItem('favoriteRecipes', JSON.stringify([recipeFavorited]));
   }
 
   return (
@@ -57,6 +88,6 @@ function FavoriteButton({ recipe, type }) {
 export default FavoriteButton;
 
 FavoriteButton.propTypes = {
-  recipe: PropTypes.node.isRequired,
+  recipe: PropTypes.objectOf(PropTypes.string).isRequired,
   type: PropTypes.string.isRequired,
 };
