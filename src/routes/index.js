94b63d4c1c783547ed17186dc8896fccf93708d0
Luan Xavier <34629186+luan-vtx@users.noreply.github.com>
2021-08-04 20:00:31 -0300

    Revert "[Grupo 13] - Recipe in Progress"

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,9 +8,7 @@ import Perfil from '../pages/Perfil';
 import Recipes from '../pages/Recipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 import RecipeDrinkDetails from '../pages/RecipeDrinkDetails';
-import RecipeDrinkInProgress from '../pages/RecipeDrinkInProgress';
 import RecipeMealDetails from '../pages/RecipeMealDetails';
-import RecipeMealInProgress from '../pages/RecipeMealInProgress';
 
 function Routes() {
   return (
@@ -19,14 +17,8 @@ function Routes() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Foods } />
         <Route exact path="/comidas/:id" component={ RecipeMealDetails } />
-        <Route exact path="/comidas/:id/in-progress" component={ RecipeMealInProgress } />
         <Route exact path="/bebidas" component={ Drinks } />
         <Route exact path="/bebidas/:id" component={ RecipeDrinkDetails } />
-        <Route
-          exact
-          path="/bebidas/:id/in-progress"
-          component={ RecipeDrinkInProgress }
-        />
         <Route path="/explorar" component={ Explorer } />
         <Route path="/perfil" component={ Perfil } />
         <Route path="/receitas-feitas" component={ Recipes } />
