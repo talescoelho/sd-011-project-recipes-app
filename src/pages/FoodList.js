e44450dd9e772ea3180adafc91ad67722857feeb
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-04 17:44:46 -0300

    adiciona requisito 31 e primeira parte do 32

--- src/pages/FoodList.js
+++ src/pages/FoodList.js
@@ -20,14 +20,13 @@ const FoodList = () => {
       { receiveData.length < 1 || !receiveData.meals ? <RenderFoods /> : receiveData.meals
         .filter((item, index) => index < maxRender)
         .map((meal, index) => (
-          <div key={ index }>
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
             <h2 data-testid={ `${index}-card-name` }>{ meal.strMeal }</h2>
             <img
               src={ meal.strMealThumb }
               alt={ meal.strMeal }
               data-testid={ `${index}-card-img` }
             />
-            <h3 data-testid={ `${index}-recipe-card` }>{ meal.strInstructions }</h3>
           </div>
         )) }
       <FooterMenu />
