103e0a7f81c404286a7313ed1a9c4c376ead12a6
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-08-04 18:39:48 -0300

    Requisitos 82-84: Criando os botoes e seus ids

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -3,9 +3,15 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Perfil() {
+  const userEmail = JSON.parse(localStorage.user);
+  console.log(userEmail.email);
   return (
     <div>
       <Header title="Perfil" search={ false } />
+      <h2 data-testid="profile-email">{ userEmail.email }</h2>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
       <Footer />
     </div>
   );
