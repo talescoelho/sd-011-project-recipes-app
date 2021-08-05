333fa0d125e91534bdba4083201b719d1e16915a
marsleite <marsleite@gmail.com>
2021-08-05 16:55:51 -0300

    requirement 54

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -9,7 +9,41 @@ export default function DoneRecipes() {
   return (
     <div>
       <Header value={ pageTitle } />
-      Receitas Feitas
+      <div>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+        <img
+          src="http://"
+          alt="imagem do card"
+          data-testid={ `${0}-horizontal-image` }
+        />
+        <h4 data-testid={ `${0}-horizontal-top-text` }>Categorias</h4>
+        <span data-testid={ `${0}-horizontal-name` }>Nome</span>
+        <span data-testid={ `${0}-horizontal-done-date` }>Data</span>
+        <button
+          type="button"
+          data-testid={ `${0}-horizontal-share-btn` }
+        >
+          Share
+        </button>
+        <span data-testid={ `${0}-${'Pasta'}-horizontal-tag` }>Tag</span>
+      </div>
     </div>
   );
 }
