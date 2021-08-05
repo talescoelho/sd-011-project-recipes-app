25486c6fdc2b09bab75f64a8a76071a5e28d3112
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-05 14:40:50 -0300

    Add Estilos Telas Pefil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../css/Profile.css';
 
 function Perfil() {
   const user = localStorage.getItem('user');
@@ -9,36 +10,43 @@ function Perfil() {
 
   return (
     <div>
-      <h1>My Perfil Page</h1>
       <Header title="Perfil" />
-      <p data-testid="profile-email">{objUser.email}</p>
-      <Link to="/receitas-feitas">
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Receitas Feitas
-        </button>
-      </Link>
+      <div className="email-profile">
+        <p>Email: </p>
+        <p data-testid="profile-email">{objUser.email}</p>
+      </div>
+      <div className="profile-container">
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            className="profile-button"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
 
-      <Link to="/receitas-favoritas">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Receitas Favoritas
-        </button>
-      </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            className="profile-button"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
 
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => localStorage.clear() }
-        >
-          Sair
-        </button>
-      </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            className="profile-button"
+            onClick={ () => localStorage.clear() }
+          >
+            Sair
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/RecipesDone.js
+++ src/pages/RecipesDone.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 import RecipeAppContext from '../context/RecipeAppContext';
+import '../css/Profile.css';
 
 const copy = require('clipboard-copy');
 
@@ -37,75 +38,93 @@ function RecipesDone() {
 
   return (
     <div>
-      <h1>My Recipe Done Page</h1>
       <Header title="Receitas Feitas" />
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-        name="All"
-        onClick={ (e) => filterRecipesDone(e) }
-      >
-        All
-      </button>
+      <div className="favorite-recipes-buttons-container">
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          name="All"
+          className="favorite-recipes-buttons"
+          onClick={ (e) => filterRecipesDone(e) }
+        >
+          All
+        </button>
 
-      <button
-        type="button"
-        data-testid="filter-by-food-btn"
-        name="Food"
-        onClick={ (e) => filterRecipesDone(e) }
-      >
-        Food
-      </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          name="Food"
+          className="favorite-recipes-buttons"
+          onClick={ (e) => filterRecipesDone(e) }
+        >
+          Food
+        </button>
 
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-        name="Drink"
-        onClick={ (e) => filterRecipesDone(e) }
-      >
-        Drinks
-      </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          name="Drink"
+          className="favorite-recipes-buttons"
+          onClick={ (e) => filterRecipesDone(e) }
+        >
+          Drinks
+        </button>
+      </div>
+
+      <br />
       <span>{click ? <p>Link copiado!</p> : <div />}</span>
-      {filteredRecipesDone && filteredRecipesDone.map((recipes, index) => (
-        <div key={ index }>
-          <input
-            type="image"
-            data-testid={ `${index}-horizontal-share-btn` }
-            src={ shareIcon }
-            alt="card da receita"
-            onClick={ () => copyLink(recipes.type, recipes.id) }
-          />
-          <ul>
+
+      <div className="favorite-recipes-container">
+        {filteredRecipesDone && filteredRecipesDone.map((recipes, index) => (
+          <div key={ index } className="favorite-recipes-cards">
             <input
               type="image"
               data-testid={ `${index}-horizontal-image` }
               src={ recipes.image }
-              width="50px"
-              height="50px"
+              width="100px"
+              height="100px"
               alt={ recipes.name }
               onClick={ () => history.push(`/${recipes.type}s/${recipes.id}`) }
             />
-            <li data-testid={ `${index}-horizontal-top-text` }>
+
+            <p data-testid={ `${index}-horizontal-top-text` }>
               {`${recipes.area} - ${recipes.category} ${recipes.alcoholicOrNot}`}
-            </li>
+            </p>
+
             <a
               href={ `/${recipes.type}s/${recipes.id}` }
               data-testid={ `${index}-horizontal-name` }
+              className="profile-recipe-name"
             >
               { recipes.name }
             </a>
-            <li data-testid={ `${index}-horizontal-done-date` }>{ recipes.doneDate }</li>
-            {recipes.tags && recipes.tags.map((tag, indexTag) => (
-              <li
-                key={ indexTag }
-                data-testid={ `${index}-${tag}-horizontal-tag` }
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+
+            <p data-testid={ `${index}-horizontal-done-date` }>{ recipes.doneDate }</p>
+
+            <div className="profile-icons">
+              <div>
+                {recipes.tags && recipes.tags.map((tag, indexTag) => (
+                  <p
+                    key={ indexTag }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                  >
+                    {tag}
+                  </p>
+                ))}
+              </div>
+
+              <input
+                type="image"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="card da receita"
+                onClick={ () => copyLink(recipes.type, recipes.id) }
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
