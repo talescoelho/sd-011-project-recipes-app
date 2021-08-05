5a8235932d38323fc9a58cfbf5e47d558436a30e
Cris Lustosa <cmlustosa@gmail.com>
2021-08-05 15:56:05 -0300

    Adiciona requisitos 67-69

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../components/design/Button';
 import { Layout } from '../components';
 import { useTheme } from '../hooks';
 
@@ -15,7 +17,22 @@ function Explore() {
   return (
     <Layout title="Explorar">
       <main style={ styles.main }>
-        <p>ola mundo</p>
+        <Link to="/explorar/comidas">
+          <Button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </Button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <Button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </Button>
+        </Link>
       </main>
     </Layout>
   );
