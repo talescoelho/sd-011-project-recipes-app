e56e20f940ada3640856a68f8b195aa1a00ee417
Gisele Costa <80549895+giisele@users.noreply.github.com>
2021-08-04 21:16:31 -0300

    Update IngredientsFoods.jsx

--- src/Components/IngredientsFoods.jsx
+++ src/Components/IngredientsFoods.jsx
@@ -1,86 +1,53 @@
 import React, { useEffect, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
-import MainContext from '../../Context/MainContext';
-import IngredientsFoods from '../../Components/IngredientsFoods';
+import MainContext from '../Context/MainContext';
 
-function DetailsRecipesFoods() {
-  const location = useLocation();
-  const { idFoods, setIdFoods, idFoodsAPI, setIdFoodsAPI } = useContext(MainContext);
-
-  useEffect(() => {
-    const URL = location.pathname;
-    const id = URL.replace('/comidas/', '');
-    setIdFoods(id);
-  }, [location, setIdFoods]);
+function Ingredients() {
+  const {
+    DetailsIngredientsFiltered,
+    setDetailsIngredientsFiltered,
+    DetailsMeasuresFiltered,
+    setDetailsMeasuresFiltered,
+    idFoodsAPI,
+  } = useContext(MainContext);
 
   useEffect(() => {
-    const getAPIById = async () => {
-      const endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idFoods}`;
-      const { meals } = await fetch(endpoint).then((data) => data.json());
-      setIdFoodsAPI(meals[0]);
-    };
-    getAPIById();
-  }, [idFoods]);
+    if (idFoodsAPI) {
+      Object.entries(idFoodsAPI).forEach((o) => (
+        o[1] === null || o[1] === '' || o[1] === ' ' ? delete idFoodsAPI[o[0]] : 0));
 
-  const getYoutubeUrl = ({ strYoutube }) => {
-    if (strYoutube) {
-      const youtubeVideoId = strYoutube.split('?v=', 2)[1];
-      const iframeLink = `https://www.youtube.com/embed/${youtubeVideoId}`;
-      return iframeLink;
+      const listIngredients = Object.entries(idFoodsAPI)
+        .filter((recipe) => recipe[0].includes('Ingredient') && recipe[1]);
+      const ingredientsFinal = listIngredients.map((valor) => valor[1]);
+
+      const listMeasures = Object.entries(idFoodsAPI || {})
+        .filter((recipe) => recipe[0].includes('Measure') && recipe[1]);
+      const measuresFinal = listMeasures.map((valor) => valor[1]);
+
+      console.log(ingredientsFinal, measuresFinal);
+
+      setDetailsIngredientsFiltered(ingredientsFinal);
+      setDetailsMeasuresFiltered(measuresFinal);
     }
-  };
+  }, [idFoodsAPI]);
 
   return (
     <div>
-      <img
-        width="320"
-        src={ idFoodsAPI.strMealThumb }
-        alt={ `Comida selecionada: ${idFoodsAPI.strMeal}` }
-        data-testid="recipe-photo"
-      />
-      <h1 data-testid="recipe-title">
-        {idFoodsAPI.strMeal}
-      </h1>
-      <button
-        type="button"
-        data-testid="share-btn"
-      >
-        compartilhar
-      </button>
-      <button
-        type="button"
-        data-testid="favorite-btn"
-      >
-        favoritar
-      </button>
-      <p data-testid="recipe-category">
-        {idFoodsAPI.strCategory}
-      </p>
-      <IngredientsFoods />
-      <p data-testid="instructions">
-        {idFoodsAPI.strInstructions}
-      </p>
-      <iframe
-        data-testid="video"
-        width="280"
-        src={ getYoutubeUrl(idFoodsAPI) }
-        title="YouTube video player"
-        allowFullScreen
-      />
-      <p data-testid="0-recomendation-card">
-        teste
-      </p>
-      {/* <p data-testid=`${index}-recomendation-card`>
-        teste
-      </p> */}
-      <button
-        type="button"
-        data-testid="start-recipe-btn"
-      >
-        Iniciar receita
-      </button>
+      <ul>
+        { DetailsIngredientsFiltered.map((ing, i) => ((
+          <li
+            data-testid={ `${i}-ingredient-name-and-measure` }
+            key={ i }
+          >
+            { ing }
+            {' '}
+            -
+            { ' ' }
+            { DetailsMeasuresFiltered.map((mea, ind) => i === ind && (mea)) }
+          </li>
+        ))) }
+      </ul>
     </div>
   );
 }
 
-export default DetailsRecipesFoods;
+export default Ingredients;
