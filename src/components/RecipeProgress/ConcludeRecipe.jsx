e9eb6f8d30495f203c45317400b3be9e7724f9ce
MarceloPalochas <almeidadearaujomarcelo@gmail.com>
2021-08-05 16:39:20 -0300

    Adds const type to isRecipeDone function

--- src/components/RecipeProgress/ConcludeRecipe.jsx
+++ src/components/RecipeProgress/ConcludeRecipe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { arrayOf, string } from 'prop-types';
 
-isRecipeDone = (id, ingredients, recipeType) => {
+const isRecipeDone = (id, ingredients, recipeType) => {
   const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
   if (inProgressRecipes.$[recipeType].$[id].length === ingredients.length) return false;
   return true;
