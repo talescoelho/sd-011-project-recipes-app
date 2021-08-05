873b54689be6b78c1e7061787c5a5c06543fa371
jamneto <netomoura22@gmail.com>
2021-08-05 16:03:52 -0300

    criaçao do componente FavoriteCard e ediçao da pagina de Favoritos

---
+++ src/components/FavoriteCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ShareButton from './ShareButton';
+import FavoriteButton from './FavoriteButton';
+import DetailsProvider from '../context/detailsProvider';
+
+export default function FavoriteCard() {
+  if (recipe) {
+    const {
+      id, category, name, image, type: drinkOrFood,
+    } = getIds(type, recipe);
+    const path = `/${type}s/${id}`;
+    return (
+      <DetailsProvider>
+        <div>
+          <Link to={ path }>
+            <img data-testid={ `${index}-horizontal-image` } src={ image } alt={ name } />
+          </Link>
+          <h3 data-testid={ `${index}-horizontal-top-text` }>{ category }</h3>
+          <Link to={ path }>
+            <h2 data-testid={ `${index}-horizontal-name` }>{ name }</h2>
+          </Link>
+          <ShareButton
+            data-testid={ `${index}-horizontal-share-btn` }
+            type={ drinkOrFood }
+            id={ id }
+          />
+          <FavoriteButton recipe={ recipe } drinkOrFood={ drinkOrFood } />
+        </div>
+      </DetailsProvider>
+    );
+  }
+}
