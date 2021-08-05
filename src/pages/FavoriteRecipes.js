f1308d08619c47f9efc64f831d7cdbcbbfd8ffe2
anacarol-r-santos <kroline0693@gmail.com>
2021-08-05 16:36:39 -0300

    [FIX] inclui map para percorrer o objeto criado como substituto do local storage

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,38 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
-// import Header from '../components/Header';
+import Header from '../components/Header';
+// import blackHeartIcon from '../images/blackHeartIcon';
 
-// const objFavoriteRecipes = [{
-//   id: id-da-receita,
-//   type: comida-ou-bebida,
-//   area: area-da-receita-ou-texto-vazio,
-//   category: categoria-da-receita-ou-texto-vazio,
-//   alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
-//   name: nome-da-receita,
-//   image: imagem-da-receita
-// }];
+const favoriteRecipes = [{
+  id: '52785',
+  type: 'meal',
+  area: 'Indian',
+  category: 'Vegetarian',
+  alcoholicOrNot: '',
+  name: 'Dal fry',
+  image: 'https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg',
+},
+{
+  id: '178319',
+  type: 'drink',
+  area: '',
+  category: 'Cocktail',
+  alcoholicOrNot: 'Alcoholic',
+  name: 'Aquamarine',
+  image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+}];
+
+const renderParagraph = (
+  <p data-testid={ `${favoriteRecipes.index}-horizontal-top-text` }>
+    {`${favoriteRecipes.area}-${favoriteRecipes.category}`}
+  </p>);
+
+console.log(favoriteRecipes);
 
 export default function FavoriteRecipes() {
+  // const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  // console.log(favoriteRecipes);
+  const [setFilteredRecipes] = useState(favoriteRecipes);
+  const [type, setType] = useState('all');
+  useEffect(() => {
+    let newFilteredRecipes = [...favoriteRecipes];
+    if (type !== 'all') {
+      newFilteredRecipes = newFilteredRecipes.filter((recipe) => recipe.type === type);
+    }
+    setFilteredRecipes(newFilteredRecipes);
+  });
+
+  const handleChange = (btn) => setType(btn);
+
   return (
-    <div>
-      <section style={ { textAlign: 'center' } }>
-        <ToggleButtonGroup
-          type="radio"
-          name="type"
-          value={ type }
-          // onChange={ handleChange }
-        >
-          <ToggleButton name="type" data-testid="filter-by-all-btn" value="all">
-            All
-          </ToggleButton>
-          <ToggleButton name="type" data-testid="filter-by-food-btn" value="meal">
-            Food
-          </ToggleButton>
-          <ToggleButton name="type" data-testid="filter-by-drink-btn" value="drink">
-            Drink
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </section>
-    </div>
+    <>
+      <Header title="Receitas Favoritas" search={ false } />
+      <ToggleButtonGroup
+        type="radio"
+        name="type"
+        value={ type }
+        onChange={ handleChange }
+      >
+        <ToggleButton name="type" data-testid="filter-by-all-btn" value="all">
+          All
+        </ToggleButton>
+        <ToggleButton name="type" data-testid="filter-by-food-btn" value="meal">
+          Food
+        </ToggleButton>
+        <ToggleButton name="type" data-testid="filter-by-drink-btn" value="drink">
+          Drink
+        </ToggleButton>
+      </ToggleButtonGroup>
+      {favoriteRecipes.map((recipe, index) => (
+        <section key={ index }>
+          <img
+            src={ recipe.image }
+            alt="imagem da receita"
+            data-testid={ `${index}-horizontal-image` }
+          />
+          { recipe.area && renderParagraph }
+          {!recipe.area && <p>{ `${recipe.category}` }</p>}
+          <h2 data-testid={ `${index}-horizontal-name` }>
+            { recipe.name }
+          </h2>
+        </section>
+      ))}
+      ;
+    </>
   );
 }
