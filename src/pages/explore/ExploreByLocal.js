44dc770b247c16babeb3f4b01e1ef6afc89541c2
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-05 01:35:16 -0300

    creating selection for country

--- src/pages/explore/ExploreByLocal.js
+++ src/pages/explore/ExploreByLocal.js
@@ -3,9 +3,40 @@ import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header';
 
 export default function ExploreByLocal() {
+  const [country, setCountry] = React.useState([]);
+
+  const urlCountryList = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+  const getCountryList = async () => {
+    try {
+      const response = await fetch(urlCountryList);
+      const data = await response.json();
+      setCountry([...data.meals]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    getCountryList();
+  }, []);
+  const handlerCountries = () => {
+    const magicNumberFive = 12;
+    return (
+      <div>
+        <label >
+          <select>
+            {country.length
+        && country.slice(0, magicNumberFive).map(({ strArea }, index) => (
+          <option key={ index }>{strArea}</option>))}
+          </select>
+        </label>
+      </div>
+    );
+  };
   return (
     <div>
       <Header title="Explorar Origem" />
+      {handlerCountries()}
       <Footer />
     </div>
   );
