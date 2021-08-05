08618145256cf7a444783ab69c531eb419d1683d
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-08-05 02:38:33 -0300

    trata erro da api de 1 letra, 100%

--- src/helpers/ApiFunctions.jsx
+++ src/helpers/ApiFunctions.jsx
@@ -3,6 +3,11 @@ function alertNotRecipies(func, msg) {
   func(msg);
 }
 
+const msg = ('Sua busca deve conter somente 1 (um) caracter');
+const fnAlert = (func, message) => {
+  func(message);
+};
+
 export const handleFoods = async (radioButton, searchText, setDataFilter, setLoading) => {
   if (radioButton === 'ingrediente') {
     setLoading(true);
@@ -30,20 +35,19 @@ export const handleFoods = async (radioButton, searchText, setDataFilter, setLoa
     return setDataFilter(meals);
   }
 
-  const fnAlert = (func, message) => {
-    func(message);
-  };
-
   try {
     if (radioButton === 'primeira letra' && searchText.length > 1) {
-      const msg = ('Sua busca deve conter somente 1 (um) caracter');
-      fnAlert(alert, msg);
+      setDataFilter([]);
+      return fnAlert(alert, msg);
     }
+    setLoading(true);
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`);
     const { meals } = await response.json();
-    return setDataFilter(meals);
+    setDataFilter(meals);
+    setLoading(false);
   } catch (error) {
     console.log(error);
+    setLoading(false);
     return setDataFilter([]);
   }
 };
@@ -78,21 +82,19 @@ export const handleDrinks = async (radioButton, searchText, setDataFilter,
     return !drinks ? setDataFilter(drinks) : setDataFilter(drinks.slice(0, Max));
   }
 
-  const fnAlert = (func, message) => {
-    func(message);
-  };
-
-  if (radioButton === 'primeira letra' && searchText.length > 1) {
-    const msg = ('Sua busca deve conter somente 1 (um) caracter');
-    return fnAlert(alert, msg);
-  }
   try {
+    if (radioButton === 'primeira letra' && searchText.length > 1) {
+      setDataFilter([]);
+      return fnAlert(alert, msg);
+    }
+    setLoading(true);
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchText}`);
     const { drinks } = await response.json();
-    return setDataFilter(drinks);
+    setDataFilter(drinks);
+    setLoading(false);
   } catch (error) {
     console.log(error);
     setLoading(false);
-    return setDataFilter([]);
+    setDataFilter([]);
   }
 };
