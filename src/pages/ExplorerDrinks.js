d3ab1fd15c1ecc86e450211376313ab805fa357e
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-05 02:18:32 -0300

    Página de Explorar Comidas ou Bebidas - 70, 71, 72 e 73

--- src/pages/ExplorerDrinks.js
+++ src/pages/ExplorerDrinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,22 @@ export default function ExplorerDrinks() {
   return (
     <main>
       <Header title="Explorar Bebidas" disable />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/bebidas/:id">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </main>
   );
--- src/pages/ExplorerFoods.js
+++ src/pages/ExplorerFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,30 @@ export default function ExplorerFoods() {
   return (
     <main>
       <Header title="Explorar Comidas" disable />
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/comidas/:id">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </main>
   );
