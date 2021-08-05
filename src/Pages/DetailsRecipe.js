4196dc29b4c138e825cdec35355db8528d531e80
murilodospacos <murilopacos@gmail.com>
2021-08-05 17:02:21 -0300

    falta colocar recomendaçoes na pagina de detalhes da receita

--- src/Pages/DetailsRecipe.js
+++ src/Pages/DetailsRecipe.js
@@ -1,36 +1,91 @@
 import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import ShareButton from '../Components/ShareButton';
 import './Styles/detailsrecipe.css';
 
 function DetailsRecipe() {
   const [recipes, setRecipes] = useState([]);
-
+  // Didmount - Faz fetch trazendo a receita pelo id e seta o stado recipes com as receita
   useEffect(() => {
     const getApi = async () => {
       const endPoint = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
-      const { meals } = await fetch(endPoint).then((data) => data.json());
+      const response = await fetch(endPoint);
+      const results = await response.json();
+      const meals = results.meals[0];
       setRecipes(meals);
-      // console.log(meals);
     };
     getApi();
   }, []);
-  console.log(recipes[0]);
+  // retorna o array com os ingredientes
+  const ingredientsRecipe = () => {
+    const arrayIndredientKids = Object.keys(recipes)
+      .filter((item) => item
+        .includes('strIngredient'));
+    const ingredients = [];
+    arrayIndredientKids.forEach((key) => {
+      if (recipes[key]) {
+        ingredients.push(recipes[key]);
+      }
+    });
+    return ingredients;
+  };
+  // retorna um array com as medidas de cada ingredientes
+  const ingredientsMesure = () => {
+    const arrayIndredientKids = Object.keys(recipes)
+      .filter((item) => item
+        .includes('strMeasure'));
+    const ingredients2 = [];
+    arrayIndredientKids.forEach((key) => {
+      if (recipes[key]) {
+        ingredients2.push(recipes[key]);
+      }
+    });
+    return ingredients2;
+  };
+  // retorna a concatenação do retorno da função ingredientsMesure com ingredientsRecipe
+  const concatIngredientWithMesure = () => {
+    const newArray = [];
+    for (let index = 0; index <= ingredientsRecipe().length; index += 1) {
+      newArray.push(`-${ingredientsRecipe()[index]} - ${ingredientsMesure()[index]}`);
+    }
+    return newArray;
+  };
 
   return (
     <div>
-      <img id="img-recipe" src="" data-testid="recipe-photo" alt="" />
-      <h2 data-testid="recipe-title">oi</h2>
+      <img
+        id="img-recipe"
+        src={ recipes.strMealThumb }
+        data-testid="recipe-photo"
+        alt="Imagem da receita"
+      />
+      <h2 data-testid="recipe-title">{ recipes.strMeal }</h2>
       <ShareButton />
       <img src={ whiteHeartIcon } alt="Favoritar Coração" data-testid="favorite-btn" />
-      <h3 data-testid="recipe-category">Category</h3>
+      <h3 data-testid="recipe-category">{ recipes.strCategory }</h3>
       <h3>Ingredients</h3>
-      {/* <ul><li data-testid={ `${index}-ingredient-name-and-measure` }>Item</li></ul> */}
+      <ul>
+        { concatIngredientWithMesure()
+          .map((igredient, index) => <li key={ index }>{igredient}</li>) }
+      </ul>
       <h3 data-testid="instructions">Instructions</h3>
+      <p>{recipes.strInstructions}</p>
       <h3 data-testid="video">Video</h3>
+      <div>
+        <ReactPlayer
+          url={ recipes.strYoutube }
+        />
+      </div>
       {/* <h3 data-testid={ `${index}-recomendation-card"` }>Recomendadas</h3> */}
       <div id="recommended"><h4>oi</h4></div>
-      <button id="start-recipe-btn" type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      <button
+        id="start-recipe-btn"
+        type="button"
+        data-testid="start-recipe-btn"
+      >
+        Iniciar Receita
+      </button>
     </div>
   );
 }
