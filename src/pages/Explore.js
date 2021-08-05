d4748e155288d1476cd6c1cc5dd8137bca6d2ace
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-05 18:13:13 -0300

    Requisito 69 - redireciona para paginas de bebida ou comida

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
+  const history = useHistory();
+
+  function handleClick(path) {
+    history.push(path);
+  }
+
   return (
     <>
       <Header withSearch={ false } pageTitle="Explorar" />
@@ -11,6 +18,7 @@ function Explore() {
         <section>
           <button
             type="button"
+            onClick={ () => handleClick('/explorar/comidas') }
             data-testid="explore-food"
           >
             Explorar Comidas
@@ -19,6 +27,7 @@ function Explore() {
         <section>
           <button
             type="button"
+            onClick={ () => handleClick('/explorar/bebidas') }
             data-testid="explore-drinks"
           >
             Explorar Bebidas
