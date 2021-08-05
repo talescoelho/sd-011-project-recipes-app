9ccad2e397e225f08926064fd9fdb6c4fa664ae6
Matheus Costa <80587425+dcmatheus@users.noreply.github.com>
2021-08-05 16:08:14 -0300

    Revert "[Grupo 6] - Componente FavoriteCard e ediçao da pagina de Favoritos"

--- src/pages/FavouriteRecipes.jsx
+++ src/pages/FavouriteRecipes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import FavoriteCard from '../components/FavoriteCard';
 
 export default function FavouriteRecipes() {
   const name = 'Receitas Favoritas';
@@ -8,13 +7,6 @@ export default function FavouriteRecipes() {
     <div>
       <Header pageName={ name } />
       Receitas Favoritas
-      <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-food-btn">Food</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
-      </div>
-      {/* Fazer map dos favoritos */}
-      <FavoriteCard />
     </div>
   );
 }
