ee5a05bd35ee7e8fb07db751c5db9c33ad4a6e5a
Michel Pereira <michelbecauro@gmail.com>
2021-08-05 16:47:12 -0300

    Requisito 12 - Input Search dinâmico

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,46 @@ import search from '../images/searchIcon.svg';
 class Header extends Component {
   constructor() {
     super();
+
+    this.state = {
+      hidden: true,
+    };
+
     this.withSearch = this.withSearch.bind(this);
     this.withoutSearch = this.withoutSearch.bind(this);
   }
 
   withSearch() {
     const { pageTitle } = this.props;
+    const { hidden } = this.state;
+
     return (
       <header>
         <h1 data-testid="page-title">{pageTitle}</h1>
-        <input
-          type="image"
-          data-testid="profile-top-btn"
-          src={ profile }
-          alt=" Profile Icon"
-        />
+        <button type="button">
+          <img
+            type="image"
+            data-testid="profile-top-btn"
+            src={ profile }
+            alt=" Profile Icon"
+          />
+        </button>
         &nbsp;  &nbsp;  &nbsp;
-        <input
-          type="image"
-          data-testid="search-top-btn"
-          src={ search }
-          alt=" Search Icon"
-        />
+        <button
+          type="button"
+          onClick={ () => (hidden
+            ? this.setState({ hidden: false })
+            : this.setState({ hidden: true })) }
+        >
+          <img
+            type="image"
+            data-testid="search-top-btn"
+            src={ search }
+            alt=" Search Icon"
+          />
+        </button>
+       &nbsp;  &nbsp;  &nbsp;
+        {!hidden && <input data-testid="search-input" type="text" placeholder="Search" />}
       </header>
     );
   }
