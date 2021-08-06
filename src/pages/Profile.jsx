2dc4429a82cdeec6f7f8afc40bae2129d5099e9d
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-06 17:12:58 -0300

    Add rendering of user email on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/common/Footer';
 
-const Profile = () => (
-  <>
-    <div>Perfil</div>
-    <Footer />
-  </>
-);
+const Profile = ({ history: { push } }) => {
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    const localStorageEmail = JSON.parse(localStorage.getItem('user'));
+    if (localStorageEmail) {
+      setUserEmail(localStorageEmail.email);
+    } else {
+      push('/');
+    }
+  }, []);
+  return (
+    <>
+      <p data-testid="profile-email">{ userEmail }</p>
+      <Footer />
+    </>
+  );
+};
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
