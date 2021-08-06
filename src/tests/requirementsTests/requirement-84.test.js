d6b939a58565d75d70ebbbe2d312f240a728d77b
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-06 18:13:46 -0300

    Add requirement-84 test

--- src/tests/requirementsTests/requirement-84.test.js
+++ src/tests/requirementsTests/requirement-84.test.js
@@ -17,20 +17,21 @@ afterEach(() => {
   localStorage.clear();
 });
 
-describe(`82 - Implement the elements of the profile screen respecting the attributes 
-described in the prototype`, () => {
-  it('All the data-testid of the email and all the buttons', () => {
+describe(`84 - Implement 3 buttons: one named "Recipes Made", one named "Favorite 
+Recipes" and one named "Exit"`, () => {
+  it('The screen contains all 3 buttons.', () => {
     const history = createMemoryHistory();
     renderWithRouterAndStore(<Profile history={ history } />, '/perfil');
 
-    const profileEmail = screen.getByTestId('profile-email');
     const profileDoneBtn = screen.getByTestId('profile-done-btn');
     const profileFavoriteBtn = screen.getByTestId('profile-favorite-btn');
     const profileLogoutBtn = screen.getByTestId('profile-logout-btn');
 
-    expect(profileEmail).toBeInTheDocument();
     expect(profileDoneBtn).toBeInTheDocument();
+    expect(profileDoneBtn).toHaveTextContent('Receitas Feitas');
     expect(profileFavoriteBtn).toBeInTheDocument();
+    expect(profileFavoriteBtn).toHaveTextContent('Receitas Favoritas');
     expect(profileLogoutBtn).toBeInTheDocument();
+    expect(profileLogoutBtn).toHaveTextContent('Sair');
   });
 });
