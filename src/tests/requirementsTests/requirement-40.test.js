b68894a1bd1af1f784ac7f10898f2a6765fbbabf
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-06 09:14:27 -0300

    Add requirement-40 test

--- src/tests/requirementsTests/requirement-40.test.js
+++ src/tests/requirementsTests/requirement-40.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import { FoodDetails, DrinkDetails } from '../../pages';
 
 describe(`40 - Implement the solution so that if the recipe has been started but not 
 finished, the button text should read "Continuar Receita"`, () => {
   it('Check "Continuar Receita" button on a food details screen', () => {
-    renderWithRouterAndStore(<FoodDetails />, '/comidas/:id');
+    const inProgressRecipes = {
+      meals: {
+        52771: [],
+      },
+    };
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+    const match = { params: { id: '52771' }, url: '/comidas/52771' };
+
+    renderWithRouterAndStore(<FoodDetails match={ match } />, '/comidas/52771');
+
+    const startRecipeBtn = screen.queryByTestId('start-recipe-btn');
+    expect(startRecipeBtn).toHaveTextContent('Continuar Receita');
   });
 
   it('Check "Continuar Receita" button on a drink details screen', () => {
-    renderWithRouterAndStore(<DrinkDetails />, '/bebidas/:id');
+    const inProgressRecipes = {
+      cocktails: {
+        178319: [],
+      },
+    };
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+    const match = { params: { id: '178319' }, url: '/bebidas/178319' };
+
+    renderWithRouterAndStore(<DrinkDetails match={ match } />, '/bebidas/178319');
+
+    const startRecipeBtn = screen.queryByTestId('start-recipe-btn');
+    expect(startRecipeBtn).toHaveTextContent('Continuar Receita');
   });
 });
