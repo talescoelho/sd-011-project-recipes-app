10214257954ca7083246d95c343d2f98049da589
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-08-06 19:48:24 -0300

    Ajustes Lint

--- src/Pages/RecipesDrinks.js
+++ src/Pages/RecipesDrinks.js
@@ -1,49 +1,51 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import './RecipesFoods.css';
 import CardRecipes from '../Components/CardRecipes';
-import { Link } from 'react-router-dom';
-//import MyContext from '../Context/MyContext';
+// import MyContext from '../Context/MyContext';
 
 export default function RecipesFood() {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const getApi = async () => {
-    const endPoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-    const response = await fetch(endPoint);
-    const results = await response.json();
-    const drinks = results.drinks;
-    setRecipes(drinks);
+      const endPoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      const response = await fetch(endPoint);
+      const results = await response.json();
+      const { drinks } = results;
+      setRecipes(drinks);
     };
     getApi();
-  },[]);
+  }, []);
   const renderCardRecipes = () => {
     const showMaxRecipes = 12;
-    if (recipes) { 
-      let filteredRecipe = recipes.filter(
+    if (recipes) {
+      const filteredRecipe = recipes.filter(
         (drinks, index) => index < showMaxRecipes,
       );
-       return filteredRecipe;
+      return filteredRecipe;
     }
-  }
+  };
   return (
     <div>
       <Header className="title" title="Bebidas" searchIconAppears />
       <div className="cardlist">
-      {renderCardRecipes().map((recp, index) => (
-         <Link
-         to = '/bebidas/drink-details'>
-         <CardRecipes
-          key={ index }
-          index={ index }
-          thumb={ recp.strDrinkThumb }
-          title={ recp.strDrink }
-          />
-          </Link>))}  
+        {renderCardRecipes().map((recp, index) => (
+          <Link
+            key={ index }
+            to="/bebidas/drink-details"
+          >
+            <CardRecipes
+              key={ index }
+              index={ index }
+              thumb={ recp.strDrinkThumb }
+              title={ recp.strDrink }
+            />
+          </Link>))}
       </div>
-       <Footer />
+      <Footer />
     </div>
   );
 }
