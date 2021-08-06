8bd15b7be15887cbf02489edc9e28c306dc29485
OldRoge <rogeriomrjr@icloud.com>
2021-08-06 15:46:38 -0300

    Done tests about requirement 9

--- src/tests/Foods.test.js
+++ src/tests/Foods.test.js
@@ -4,6 +4,26 @@ import Foods from '../pages/Foods';
 
 import { renderWithRouterAndStore } from './testConfig';
 
+describe('Verify if the Header component have the atributs data-testid', () => {
+  it('Have data-testid "profile-top-btn"', () => {
+    renderWithRouterAndStore(<Foods />, '/comidas');
+    const profileButton = screen.getByTestId('profile-top-btn');
+    expect(profileButton).toBeInTheDocument();
+  });
+
+  it('Have data-testid "page-title"', () => {
+    renderWithRouterAndStore(<Foods />, '/comidas');
+    const pageTitle = screen.getByTestId('page-title');
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  it.only('Have data-testid "search-top-btn"', () => {
+    renderWithRouterAndStore(<Foods />, '/comidas');
+    const searchButton = screen.getByTestId('search-top-btn');
+    expect(searchButton).toBeInTheDocument();
+  });
+});
+
 describe('Tests Foods Page', () => {
   it('Checks if Footer is rendered', () => {
     renderWithRouterAndStore(<Foods />, '/comidas');
