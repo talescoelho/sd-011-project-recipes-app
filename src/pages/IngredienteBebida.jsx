4e34c8f422ab0c111484ce2a6f28cb3a8890e41b
marlon307 <marlon-ramosb@hotmail.com>
2021-08-06 10:20:59 -0300

    75-77

--- src/pages/IngredienteBebida.jsx
+++ src/pages/IngredienteBebida.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import searchCase from '../service/apiSearchBar';
+import IngredientCard from '../components/IngredientCard';
 
 function IngredienteBebida() {
+  const { dataApi: { drinks } } = useSelector(({ searchItems }) => searchItems);
+  const magicNumber = 13;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function getApi() {
+      dispatch(await searchCase('drink'));
+    }
+    getApi();
+  }, [dispatch]);
   return (
     <div>
       <Header title="Explorar Ingredientes" />
+      { drinks !== undefined
+        && drinks.slice(magicNumber).map((
+          { idDrink, strDrink, strDrinkThumb }, index,
+        ) => (
+          <IngredientCard
+            idItem={ idDrink }
+            name={ strDrink }
+            key={ index }
+            index={ index }
+            src={ strDrinkThumb }
+            comidasOuBebidas="comidas"
+          />
+        )) }
       <Footer />
     </div>
   );
--- src/pages/IngredienteComida.jsx
+++ src/pages/IngredienteComida.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import searchCase from '../service/apiSearchBar';
+import IngredientCard from '../components/IngredientCard';
 
 function IngredienteComida() {
+  const { dataApi: { meals } } = useSelector(({ searchItems }) => searchItems);
+  const magicNumber = 13;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function getApi() {
+      dispatch(await searchCase('meal'));
+    }
+    getApi();
+  }, [dispatch]);
   return (
     <div>
       <Header title="Explorar Ingredientes" />
+      { meals !== undefined
+        && meals.slice(magicNumber).map(({ idMeal, strMeal, strMealThumb }, index) => (
+          <IngredientCard
+            idItem={ idMeal }
+            name={ strMeal }
+            key={ index }
+            index={ index }
+            src={ strMealThumb }
+            comidasOuBebidas="comidas"
+          />
+        )) }
       <Footer />
     </div>
   );
