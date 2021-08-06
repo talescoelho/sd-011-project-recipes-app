ca14ca64a22700996b4f85b5a0dac4230a9e90c5
Rafael Schmitz <schimitaum@gmail.com>
2021-08-05 22:17:34 -0300

    reqs. 2 to 8 done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { UserHook } from '../Context/UserHook';
 
 function Login() {
-  const { email, password, disabled } = state;
-  const { setUserAction } = props;
-  const { data, setFiltered, filter, setFilter } = useContext(DataContext);
-  const [select1, setSelect1] = useState('');
+  const { email, setEmail } = UserHook();
+  const [password, setPassword] = useState('');
+  const [disabled, setdisabled] = useState(true);
+
+  function handleDisable() {
+    const reg = /\S+@\S+\.\S+/;
+    const min = 6;
+    setdisabled(!(email.match(reg) && password.length >= min));
+  }
+
+  function handleSubmit() {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+  }
+
   return (
     <div className="loginContainer">
-      <h3>App de Receitas</h3>
+      <h4>App de Receitas</h4>
       <div className="inputForm">
-        <h3>Login</h3>
-        <form>
-          <label htmlFor="email-input">
-            <input
-              type="email"
-              name="email"
-              data-testid="email-input"
-              placeholder="Email"
-              value={ email }
-              onChange={ (e) => handleChange(e.target) }
-            />
-          </label>
-          <label htmlFor="password-input">
-            <input
-              type="password"
-              name="password"
-              data-testid="password-input"
-              placeholder="Senha"
-              value={ password }
-              onChange={ (e) => handleChange(e.target) }
-            />
-          </label>
-        </form>
+        <h5>Login</h5>
+
+        <label htmlFor="email-input">
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Email"
+            onChange={ (e) => { setEmail(e.target.value); handleDisable(); } }
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Senha"
+            onChange={ (e) => { setPassword(e.target.value); handleDisable(); } }
+          />
+        </label>
       </div>
-      <Link to="/carteira">
+      <Link to="/comidas">
         <button
           type="button"
           data-testid="login-submit-btn"
           disabled={ disabled }
-          onClick={ () => setUserAction(email) }
+          onClick={ () => handleSubmit() }
         >
           Entrar
         </button>
