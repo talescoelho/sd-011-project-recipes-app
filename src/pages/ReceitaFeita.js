9c19c51e78c6340ad5f8128a921968f06798bf3e
Victor-Windows <czpask@gmail.com>
2021-08-05 21:13:43 -0300

    requisito 58

--- src/pages/ReceitaFeita.js
+++ src/pages/ReceitaFeita.js
@@ -2,28 +2,76 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 
+const copy = require('clipboard-copy');
+
 export default function ReceitaFeita() {
   const [doneRecipe, setDoneRecipe] = useState();
+  const [copyClip, setCopy] = useState(false);
+  const [backup, setBackup] = useState();
 
   const doneRecipesStorage = JSON.parse(localStorage.getItem('doneRecipes'));
 
-  function getStorage(func) {
+  function getStorage(func1, func2) {
     if (localStorage.doneRecipes) {
-      func(doneRecipesStorage);
+      func1(doneRecipesStorage);
+      func2(doneRecipesStorage);
     }
   }
 
   useEffect(() => {
-    getStorage(setDoneRecipe);
+    getStorage(setDoneRecipe, setBackup);
   }, []);
+
+  function filtersFunc(type) {
+    const foodFilter = backup.filter((value) => value.type === 'comida');
+    const drinkFilter = backup.filter((value) => value.type === 'bebida');
+    const allFilter = backup;
+    switch (type) {
+    case 'comida':
+      return setDoneRecipe(foodFilter);
+    case 'bebida':
+      return setDoneRecipe(drinkFilter);
+    case 'All':
+      return setDoneRecipe(allFilter);
+    default:
+      setDoneRecipe(allFilter);
+    }
+  }
 
   function renderBtns() {
     return (
       <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-food-btn">Food</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          value="All"
+          onClick={ (e) => filtersFunc(e.target.value) }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          value="comida"
+          onClick={ (e) => filtersFunc(e.target.value) }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          value="bebida"
+          onClick={ (e) => filtersFunc(e.target.value) }
+        >
+          Drinks
+        </button>
       </div>);
+  }
+
+  function handleClick(param) {
+    copy(`http://localhost:3000/comidas/${param}`);
+    setCopy(true);
+    alert('Link copiado!');
   }
 
   return (
@@ -32,7 +80,6 @@ export default function ReceitaFeita() {
       {renderBtns()}
       {doneRecipe && doneRecipe.map((rec, index) => {
         if (rec.type === 'comida') {
-          console.log(index)
           return (
             <div key={ index }>
               <img
@@ -41,12 +88,19 @@ export default function ReceitaFeita() {
                 data-testid={ `${index}-horizontal-image` }
                 width="100px"
               />
-              <p data-testid={ `${index}-horizontal-top-text` }>{`${rec.area} - ${rec.category}`}</p>
+              <p data-testid={ `${index}-horizontal-top-text` }>
+                {`${rec.area} - ${rec.category}`}
+              </p>
               <h3 data-testid={ `${index}-horizontal-name` }>{rec.name}</h3>
               <h4 data-testid={ `${index}-horizontal-done-date` }>{rec.doneDate}</h4>
-              <button type="button">
-                <img src={ shareIcon } alt="btnShare" data-testid={ `${index}-horizontal-share-btn` } />
+              <button type="button" onClick={ () => handleClick(rec.id) }>
+                <img
+                  src={ shareIcon }
+                  alt="btnShare"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                />
               </button>
+              {copyClip ? <span>Link copiado!</span> : null}
               {rec.tags ? rec.tags.map((tag, indexTag) => (
                 <span key={ indexTag } data-testid={ `${index}-${tag}-horizontal-tag` }>
                   {tag}
@@ -63,11 +117,17 @@ export default function ReceitaFeita() {
               width="100px"
             />
             <h4 data-testid={ `${index}-horizontal-top-text` }>{rec.category}</h4>
-            { rec.alcoholicOrNot !== '' ? <p data-testid={ `${index}-horizontal-top-text` }>{rec.alcoholicOrNot}</p> : null }
+            { rec.alcoholicOrNot !== '' ? (
+              <p data-testid={ `${index}-horizontal-top-text` }>{rec.alcoholicOrNot}</p>
+            ) : null }
             <h3 data-testid={ `${index}-horizontal-name` }>{rec.name}</h3>
             <h4 data-testid={ `${index}-horizontal-done-date` }>{rec.doneDate}</h4>
             <button type="button">
-              <img src={ shareIcon } alt="btnShare" data-testid={ `${index}-horizontal-share-btn` } />
+              <img
+                src={ shareIcon }
+                alt="btnShare"
+                data-testid={ `${index}-horizontal-share-btn` }
+              />
             </button>
             {rec.tags ? rec.tags.map((tag, indexTag) => (
               <span key={ indexTag } data-testid={ `${index}-${tag}-horizontal-tag` }>
