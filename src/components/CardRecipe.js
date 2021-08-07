2931cdcf70175ddb957c2bc7fcb5072dfb1823ab
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-06 22:13:46 -0300

    resolve errors lint

--- src/components/CardRecipe.js
+++ src/components/CardRecipe.js
@@ -13,27 +13,24 @@ function CardRecipe({ item, index }) {
             <p data-testid={ `${index}-card-name` }>
               { item.strMeal ? item.strMeal : item.strCategory }
             </p>
-            { item.strMealThumb
-              ?
-                <img
-                  data-testid={ `${index}-card-img` }
-                  src={ item.strMealThumb }
-                  alt={ item.strMeal }
-                  width="50px" />
-              : ''}
+            { !item.strMealThumb ? '' : (<img
+              data-testid={ `${index}-card-img` }
+              src={ item.strMealThumb }
+              alt={ item.strMeal }
+              width="50px"
+            />) }
           </div>
         ) : (
           <div>
             <p data-testid={ `${index}-card-name` }>
               { item.strDrink ? item.strDrink : item.strCategory }
             </p>
-            { item.strDrinkThumb ?
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ item.strDrinkThumb }
-                alt={ item.strDrink }
-                width="50px" />
-            : ''}
+            { !item.strDrinkThumb ? '' : (<img
+              data-testid={ `${index}-card-img` }
+              src={ item.strDrinkThumb }
+              alt={ item.strDrink }
+              width="50px"
+            />) }
           </div>
         ) }
     </div>
