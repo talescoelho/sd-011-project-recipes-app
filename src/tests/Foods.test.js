cc319ddf2ad1a32dce582406b0ea84f10adf0aca
OldRoge <rogeriomrjr@icloud.com>
2021-08-08 18:42:33 -0300

    Deleted Header component test and put the test on the Foods.test

--- src/tests/Foods.test.js
+++ src/tests/Foods.test.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import {
+  screen,
+  fireEvent,
+  queryByTestId,
+} from '@testing-library/react';
 import { renderWithRouterAndStore } from './testConfig';
 
 import Foods from '../pages/Foods';
+
+describe.only('Testing Header component functionalities', () => {
+  it('When click on the profile button render Profile', () => {
+    const { history } = renderWithRouterAndStore(<Foods />);
+    const profileButton = screen.getByTestId('profile-top-btn');
+
+    fireEvent.click(profileButton);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/perfil');
+  });
+
+  it('Show and hide when click on search button', () => {
+    renderWithRouterAndStore(<Foods />);
+    const searchButton = screen.getByTestId('search-top-btn');
+
+    expect(searchButton).toBeInTheDocument();
+
+    expect(queryByTestId(document.documentElement, 'search-input')).toBe(null);
+    fireEvent.click(searchButton);
+    expect(queryByTestId(document.documentElement, 'search-input')).toBeInTheDocument();
+  });
+
+  it('Should have the data tests id as search bar as all radio buttons', () => {
+
+  });
+});
 
 describe('Verify if the Header component have the atributs data-testid', () => {
   it('Have data-testid "profile-top-btn"', () => {
