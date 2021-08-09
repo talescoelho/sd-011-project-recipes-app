cb25e077fd65e89b4bc2bd70357dac3394f03ee6
Prih Scapin <prih.scapin@gmail.com>
2021-08-09 16:01:35 -0300

    Renderizaçao do botao favorito- sem logica de armazenamento de receitas favoritas

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -6,8 +6,6 @@ function FavoriteButton() {
   // Set do state inicial do favorito como false (como é iniciado o botão)
   const [fav, setFav] = useState(false);
 
-  // A condicional abaixo retorna setFav como true caso fav seja true (quando clicado) e false caso setFav
-  // seja false.
   const handleClick = () => {
     if (!fav) {
       setFav(true);
@@ -16,9 +14,7 @@ function FavoriteButton() {
     }
   };
 
-  // o ternário no src (atributo que especifica url da img) retorna blackHeart se color for true (onClick alterar o state)
-  // ou whiteHeart se color for false.
-  // A image não aceita um onClick como atributo. Para solucionar isso, foi criada uma tag input do tipo image (thanks Douglas <3);
+  // A tag image não aceita um onClick como atributo. Para solucionar isso, foi criada uma tag input do tipo image (thanks Douglas <3);
   return (
     <input
       type="image"
