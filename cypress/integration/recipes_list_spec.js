7cb314d0a0fe87af4e8b67a5e89b2a5ecb246652
Michel Pereira <michelbecauro@gmail.com>
2021-08-09 17:18:53 -0300

    Troca da pasta Cypress pela pasta da Branch Master

--- cypress/integration/recipes_list_spec.js
+++ cypress/integration/recipes_list_spec.js
@@ -129,7 +129,7 @@ describe('27 - Implemente os botões de categoria para serem utilizados como fil
   });
 });
 
-describe.skip('28 - Implemente o filtro das receitas através da API ao clicar no filtro de categoria', () => {
+describe('28 - Implemente o filtro das receitas através da API ao clicar no filtro de categoria', () => {
   it('Caso as receitas sejam de comida e a categoria seja "Beef", deve-se carregar as 12 primeiras receitas de "Beef"', () => {
     cy.visit('http://localhost:3000/comidas', {
       onBeforeLoad(win) {
@@ -251,7 +251,7 @@ describe.skip('28 - Implemente o filtro das receitas através da API ao clicar n
   });
 });
 
-describe.skip('29 - Implemente o filtro como um toggle, que se for selecionado de novo, o app deve retornar as receitas sem nenhum filtro', () => {
+describe('29 - Implemente o filtro como um toggle, que se for selecionado de novo, o app deve retornar as receitas sem nenhum filtro', () => {
   it('Caso as receitas sejam de comida e o filtro tenha sido selecionado novamente, deve-se retornar as 12 primeiras receitas sem filtro', () => {
     cy.visit('http://localhost:3000/comidas', {
       onBeforeLoad(win) {
@@ -279,7 +279,7 @@ describe.skip('29 - Implemente o filtro como um toggle, que se for selecionado d
   });
 });
 
-describe.skip('30 - Implemente o filtro de categoria para que apenas um seja selecionado por vez', () => {
+describe('30 - Implemente o filtro de categoria para que apenas um seja selecionado por vez', () => {
   it('Caso as receitas sejam de comida apenas um filtro de categoria deve poder ser selecionado por vez', () => {
     cy.visit('http://localhost:3000/comidas', {
       onBeforeLoad(win) {
@@ -309,7 +309,7 @@ describe.skip('30 - Implemente o filtro de categoria para que apenas um seja sel
   });
 });
 
-describe.skip('31 - Desenvolva o filtro de categorias com a opção de filtrar por todas as categorias', () => {
+describe('31 - Desenvolva o filtro de categorias com a opção de filtrar por todas as categorias', () => {
   it('Caso as receitas sejam de comida deve existir a opção de filtrar por todas as categorias', () => {
     cy.visit('http://localhost:3000/comidas', {
       onBeforeLoad(win) {
@@ -339,7 +339,7 @@ describe.skip('31 - Desenvolva o filtro de categorias com a opção de filtrar p
   });
 });
 
-describe.skip('32 - Redirecione a pessoa usuária, ao clicar no card, para a tela de detalhes, que deve mudar a rota e conter o id da receita na URL', () => {
+describe('32 - Redirecione a pessoa usuária, ao clicar no card, para a tela de detalhes, que deve mudar a rota e conter o id da receita na URL', () => {
   it('Caso as receitas sejam de comida a rota deve mudar para a tela de detalhes da receita', () => {
     cy.visit('http://localhost:3000/comidas', {
       onBeforeLoad(win) {
