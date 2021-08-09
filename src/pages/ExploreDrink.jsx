f1f4bc8fb29646d19595facb2538f9df5c484c18
israeltonholo <israeltonholo@hotmail.com>
2021-08-09 18:56:52 -0300

    adiciona requisito 74

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import { requestRandomDrinkRecipe } from '../services/requestRandomRecipe';
 
-const ExploreDrink = () => (
-  <>
-    <Link to="/explorar/bebidas/ingredientes">
-      <Button
-        type="submit"
-        data-testid="explore-by-ingredient"
-        variant="light"
-        size="lg"
-      >
-        Por Ingredientes
-      </Button>
-    </Link>
-    <Link to="/bebidas">
+export default function ExploreDrink({ history }) {
+  const callRandomAPIdrink = async () => {
+    const callAPI = await requestRandomDrinkRecipe();
+    const result = callAPI.drinks;
+    history.push(`/bebidas/${result[0].idDrink}`);
+  };
+  return (
+    <>
+      <Link to="/explorar/bebidas/ingredientes">
+        <Button
+          type="submit"
+          data-testid="explore-by-ingredient"
+          variant="light"
+          size="lg"
+        >
+          Por Ingredientes
+        </Button>
+      </Link>
       <Button
         type="submit"
         data-testid="explore-surprise"
         variant="light"
         size="lg"
+        onClick={ () => callRandomAPIdrink() }
       >
         Me Surpreenda!
       </Button>
-    </Link>
-  </>
-);
+    </>
+  );
+}
 
-export default ExploreDrink;
+ExploreDrink.propTypes = {
+  history: PropTypes.node.isRequired,
+};
--- src/services/requestRandomRecipe.js
+++ src/services/requestRandomRecipe.js
@@ -1,9 +1,13 @@
-export const requestRandomDrinkRecipe = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-  .then((products) => products.text())
-  .then((response) => console.log('Resposta postagem de sucesso:', response))
-  .catch((error) => console.log('error na postagem:', error));
+const URLDrink = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const URLFood = 'https://www.themealdb.com/api/json/v1/1/random.php';
+export const requestRandomDrinkRecipe = async () => {
+  const APIfetch = await fetch(URLDrink);
+  const APIjson = await APIfetch.json();
+  return APIjson;
+};
 
-export const requestRandomFoodRecipe = () => fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-  .then((products) => products.text())
-  .then((response) => console.log('Resposta postagem de sucesso:', response))
-  .catch((error) => console.log('error na postagem:', error));
+export const requestRandomFoodRecipe = async () => {
+  const APIfetch = await fetch(URLFood);
+  const APIjson = await APIfetch.json();
+  return APIjson;
+};
