33154ff409c3fd3ec103306e2fc80ea480875b6a
Luiz Otavio <luiz.otavio273@gmail.com>
2021-08-09 18:33:59 -0300

    req 62

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -104,7 +104,7 @@ function FavoriteRecipes() {
               <span
                 data-testid={ `${index}-horizontal-top-text` }
               >
-                { item.category }
+                { item.alcoholicOrNot }
               </span>
               <span data-testid={ `${index}-horizontal-name` }>{ item.name }</span>
               <button
