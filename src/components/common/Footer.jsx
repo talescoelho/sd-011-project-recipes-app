c62e4483c1c5055af18d2540ce2fdf29ab030229
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-09 17:22:46 -0300

    Resolve conflicts

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,24 +5,21 @@ import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
 const Footer = () => (
-  <footer data-testid="footer">
+  <footer data-testid="footer" style={ { position: 'fixed', bottom: '0' } }>
     <Link
       to="/bebidas"
-      data-testid="drinks-bottom-btn"
     >
-      <img src={ drinkIcon } alt="Drink Icon" />
+      <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Drink Icon" />
     </Link>
     <Link
       to="/explorar"
-      data-testid="explore-bottom-btn"
     >
-      <img src={ exploreIcon } alt="Search Icon" />
+      <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="Search Icon" />
     </Link>
     <Link
       to="/comidas"
-      data-testid="food-bottom-btn"
     >
-      <img src={ mealIcon } alt="Food Icon" />
+      <img data-testid="food-bottom-btn" src={ mealIcon } alt="Food Icon" />
     </Link>
   </footer>
 );
