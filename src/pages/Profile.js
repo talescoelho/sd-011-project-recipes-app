1a73ce6576d22743a97dad3e260ba672c9783913
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 14:39:43 -0300

    profile-done-btn

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,6 +12,11 @@ export default function Profile() {
       <h1 data-testid="profile-email">{email}</h1>
       <Link to="/receitas-favoritas">
         <button type="button" data-testid="profile-favorite-btn">
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">
           Receitas Feitas
         </button>
       </Link>
