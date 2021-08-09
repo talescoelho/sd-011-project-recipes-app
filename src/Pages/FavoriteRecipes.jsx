8cf5fdf5107306bae42db5130b7e76f16ad54d83
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-09 18:38:42 -0300

    Pausa no requisito

--- src/Pages/FavoriteRecipes.jsx
+++ src/Pages/FavoriteRecipes.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profileIcon from '../images/profileIcon.svg';
+import FoodOrDrinkFilter from './Components/FoodOrDrinkFilter';
 
 function FavoriteRecipes() {
+  const [filter, setFilter] = useState('all');
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
   return (
     <div>
       <h1 data-testid="page-title">Receitas Favoritas</h1>
@@ -10,6 +14,21 @@ function FavoriteRecipes() {
         src={ profileIcon }
         alt="Botão que direciona para a tela de perfil"
       />
+      <FoodOrDrinkFilter setFilter={ setFilter } />
+      <section className="favorites-recipes">
+        {favoriteRecipes.map(({ image, name, area, alcoholicOrNot }, index) => (
+          <div key>
+            <img src={ image } width="150" data-testid={ `${index}-horizontal-image` } alt="Imagem de comida" />
+            <div>
+              <p data-testid={ `${index}-horizontal-top-text` }>{area !== '' ? area : alcoholicOrNot }</p>
+              <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+              <p data-testid={ `${index}-horizontal-favorite-btn` }>Botão favoritar</p>
+              <p data-testid={ `${index}-horizontal-share-btn` }>Botão compartilhar</p>
+            </div>
+              
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
