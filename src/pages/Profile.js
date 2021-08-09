5f2f563402532b673c54ced55ddceab7473325c7
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 14:31:35 -0300

    rendering the localstorage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,10 +3,11 @@ import Footer from '../components/footer/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.user);
   return (
     <div>
       <Header title="Perfil" data-testid="page-title" />
-      
+      <h1>{email}</h1>
       <Footer />
     </div>
   );
