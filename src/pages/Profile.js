e1ffe3d228170fc628eb2232f08a70410ef2fb3e
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 14:26:50 -0300

    refactoring profile screen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,8 +5,8 @@ import Header from '../components/Header';
 export default function Profile() {
   return (
     <div>
-      <Header title="Perfil" />
-      <h1 data-testid="page-title">Perfil</h1>
+      <Header title="Perfil" data-testid="page-title" />
+      
       <Footer />
     </div>
   );
