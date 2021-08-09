7eb5c46f01926a52b14a6a5bf18781ba88c57a3b
juliofilizzola <juliofilizzola@Hotmail.com>
2021-08-09 14:38:07 -0300

    profile-favorite-btn

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,4 @@
+import { Button } from 'bootstrap';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/footer/Footer';
@@ -8,8 +9,12 @@ export default function Profile() {
   return (
     <div>
       <Header title="Perfil" data-testid="page-title" />
-      <h1 data-testid="profile-email" >{email}</h1>
-
+      <h1 data-testid="profile-email">{email}</h1>
+      <Link to="/receitas-favoritas">
+        <button type="button" data-testid="profile-favorite-btn">
+          Receitas Feitas
+        </button>
+      </Link>
       <Footer />
     </div>
   );
