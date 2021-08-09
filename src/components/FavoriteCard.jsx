377fd7b0f94397f9967cc1aeef516731d57b3177
jamneto <netomoura22@gmail.com>
2021-08-09 18:47:03 -0300

    criaçao da logica da pagina de favoritos

--- src/components/FavoriteCard.jsx
+++ src/components/FavoriteCard.jsx
@@ -3,31 +3,40 @@ import { Link } from 'react-router-dom';
 import ShareButton from './ShareButton';
 import FavoriteButton from './FavoriteButton';
 import DetailsProvider from '../context/detailsProvider';
-import { getIds } from '../services';
 
 export default function FavoriteCard({ recipe, index }) {
   if (recipe) {
-    const {
-      id, category, name, image, type: drinkOrFood,
-    } = getIds(recipe.type, recipe);
-    const path = `/${recipe.type}s/${id}`;
+    const path = `/${recipe.type}s/${recipe.id}`;
+    const category = recipe.area
+      ? (`${recipe.area} - ${recipe.category}`) : recipe.alcoholicOrNot;
     return (
       <DetailsProvider>
         <div>
           <Link to={ path }>
-            <img data-testid={ `${index}-horizontal-image` } src={ image } alt={ name } />
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt={ recipe.name }
+              width="100px"
+            />
           </Link>
-          <h3 data-testid={ `${index}-horizontal-top-text` }>{ category }</h3>
+          <h3 data-testid={ `${index}-horizontal-top-text` }>
+            { category }
+          </h3>
           <Link to={ path }>
-            <h2 data-testid={ `${index}-horizontal-name` }>{ name }</h2>
+            <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
           </Link>
           <ShareButton
-            data-testid={ `${index}-horizontal-share-btn` }
-            type={ drinkOrFood }
-            id={ id }
+            dataTestid={ `${index}-horizontal-share-btn` }
+            type={ recipe.type }
+            id={ recipe.id }
           />
-          <FavoriteButton recipe={ recipe } drinkOrFood={ drinkOrFood } />
         </div>
+        <FavoriteButton
+          recipe={ recipe }
+          drinkOrFood={ recipe.type }
+          dataTestid={ `${index}-horizontal-favorite-btn` }
+        />
       </DetailsProvider>
     );
   }
