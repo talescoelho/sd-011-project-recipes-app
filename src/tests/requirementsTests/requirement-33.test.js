a35d98cc3966fa477a5fbe4cf6f29182caaa1dbe
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-10 09:18:09 -0300

    Add requirement-33 test

--- src/tests/requirementsTests/requirement-33.test.js
+++ src/tests/requirementsTests/requirement-33.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import { FoodDetails, DrinkDetails } from '../../pages';
+import * as requestMenu from '../../services/requestMenu';
+import mealsFiltersByAll from '../mocks/meals/mockFilterMealsByAll';
+import drinksFiltersByAll from '../mocks/drinks/mockFilterDrinksByAll';
+
+jest
+  .spyOn(requestMenu, 'searchMealByName')
+  .mockImplementation(() => Promise.resolve(mealsFiltersByAll));
+
+jest
+  .spyOn(requestMenu, 'searchDrinkByName')
+  .mockImplementation(() => Promise.resolve(drinksFiltersByAll));
+
+afterEach(() => jest.clearAllMocks());
+beforeEach(() => jest.clearAllMocks());
 
 describe(`33 - Implement the elements of a recipe details screen respecting the
 attributes described in the prototype`, () => {
@@ -10,9 +24,11 @@ attributes described in the prototype`, () => {
     renderWithRouterAndStore(<FoodDetails match={ match } />, '/comidas/52771');
 
     const instructions = await screen.findByTestId('instructions');
+    const recomendationCard = await screen.findByTestId('0-recomendation-card');
     const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
 
     expect(instructions).toBeInTheDocument();
+    expect(recomendationCard).toBeInTheDocument();
     expect(startRecipeBtn).toBeInTheDocument();
   });
 
@@ -21,9 +37,11 @@ attributes described in the prototype`, () => {
     renderWithRouterAndStore(<DrinkDetails match={ match } />, '/bebidas/178319');
 
     const instructions = await screen.findByTestId('instructions');
+    const recomendationCard = await screen.findByTestId('0-recomendation-card');
     const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
 
     expect(instructions).toBeInTheDocument();
+    expect(recomendationCard).toBeInTheDocument();
     expect(startRecipeBtn).toBeInTheDocument();
   });
 });
