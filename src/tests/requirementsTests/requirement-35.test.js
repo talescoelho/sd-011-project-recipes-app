16a03a0f34a49e30cfa1fe7a2d4c1eb365de7829
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-10 10:55:59 -0300

    Add requirement-35 and requirement-36 tests

--- src/tests/requirementsTests/requirement-35.test.js
+++ src/tests/requirementsTests/requirement-35.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import { FoodDetails, DrinkDetails } from '../../pages';
 import * as requestMenu from '../../services/requestMenu';
@@ -36,12 +37,40 @@ describe(`35 - Design the screen so that it contains an image of the recipe, the
 the category (or whether or not you are alcoholic), a list of ingredients followed by the
 quantities, instructions, a "drunk" youtube video, and recommendations`, () => {
   it('Check if the elements described in the prototype exist in the food details screen',
-    () => {
+    async () => {
       renderWithRouterAndStore(<FoodDetails match={ mockMealsMatch } />, mockMealPath);
+
+      const firstRecommendationCard = await screen.findByTestId('0-recomendation-card');
+      const secondRecommendationCard = await screen.findByTestId('1-recomendation-card');
+      const thirdRecommendationCard = await screen.findByTestId('2-recomendation-card');
+      const fourthRecommendationCard = await screen.findByTestId('3-recomendation-card');
+      const fivethRecommendationCard = await screen.findByTestId('4-recomendation-card');
+      const sixthRecommendationCard = await screen.findByTestId('5-recomendation-card');
+
+      expect(firstRecommendationCard).toBeInTheDocument();
+      expect(secondRecommendationCard).toBeInTheDocument();
+      expect(thirdRecommendationCard).toBeInTheDocument();
+      expect(fourthRecommendationCard).toBeInTheDocument();
+      expect(fivethRecommendationCard).toBeInTheDocument();
+      expect(sixthRecommendationCard).toBeInTheDocument();
     });
 
   it(`Check if the elements described in the prototype exist in the drink details 
-  screen`, () => {
+  screen`, async () => {
     renderWithRouterAndStore(<DrinkDetails match={ mockDrinkMatch } />, mockDrinkPath);
+
+    const firstRecommendationCard = await screen.findByTestId('0-recomendation-card');
+    const secondRecommendationCard = await screen.findByTestId('1-recomendation-card');
+    const thirdRecommendationCard = await screen.findByTestId('2-recomendation-card');
+    const fourthRecommendationCard = await screen.findByTestId('3-recomendation-card');
+    const fivethRecommendationCard = await screen.findByTestId('4-recomendation-card');
+    const sixthRecommendationCard = await screen.findByTestId('5-recomendation-card');
+
+    expect(firstRecommendationCard).toBeInTheDocument();
+    expect(secondRecommendationCard).toBeInTheDocument();
+    expect(thirdRecommendationCard).toBeInTheDocument();
+    expect(fourthRecommendationCard).toBeInTheDocument();
+    expect(fivethRecommendationCard).toBeInTheDocument();
+    expect(sixthRecommendationCard).toBeInTheDocument();
   });
 });
--- src/tests/requirementsTests/requirement-36.test.js
+++ src/tests/requirementsTests/requirement-36.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import { FoodDetails, DrinkDetails } from '../../pages';
+import { testMealsRecipeCard, testDrinksRecipeCard } from '../helper/testRecipeCard';
 import * as requestMenu from '../../services/requestMenu';
 import mealsFiltersByAll from '../mocks/meals/mockFilterMealsByAll';
 import mealRecipeDetails from '../mocks/meals/mockMealRecipeDetails';
 import drinksFiltersByAll from '../mocks/drinks/mockFilterDrinksByAll';
 import drinkRecipeDetails from '../mocks/drinks/mockDrinkRecipeDetails';
+
+const maxDefaultCards = 6;
+const cardTestId = '-recomendation-card';
+const titleTestId = '-recomendation-title';
 
 const mockMealPath = '/comidas/52977';
 const mockMealsMatch = { params: { id: '52977' }, url: '/comidas/52977' };
@@ -13,11 +19,11 @@ const mockMealsMatch = { params: { id: '52977' }, url: '/comidas/52977' };
 const mockDrinkPath = '/bebidas/15997';
 const mockDrinkMatch = { params: { id: '15997' }, url: '/bebidas/15997' };
 
-jest
+const mockedSearchMealByName = jest
   .spyOn(requestMenu, 'searchMealByName')
   .mockImplementation(() => Promise.resolve(mealsFiltersByAll));
 
-jest
+const mockedSearchDrinkByName = jest
   .spyOn(requestMenu, 'searchDrinkByName')
   .mockImplementation(() => Promise.resolve(drinksFiltersByAll));
 
@@ -34,11 +40,35 @@ beforeEach(() => jest.clearAllMocks());
 
 describe(`36 - Implement the recommendations, for food recipes the recommendation should 
 be drink and vice versa`, () => {
-  it('Checks whether the request for the beverage API has been made', () => {
+  it('Check if the request for the food API has been made', async () => {
     renderWithRouterAndStore(<FoodDetails match={ mockMealsMatch } />, mockMealPath);
+
+    const instructions = await screen.findByTestId('instructions');
+    const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
+
+    expect(mockedSearchDrinkByName).toBeCalled();
+    expect(mockedSearchDrinkByName).toBeCalledTimes(1);
+
+    const { drinks } = drinksFiltersByAll;
+    await testDrinksRecipeCard(drinks, maxDefaultCards, cardTestId, titleTestId);
+
+    expect(instructions).toBeInTheDocument();
+    expect(startRecipeBtn).toBeInTheDocument();
   });
 
-  it('Check if the request for the food API has been made', () => {
+  it('Checks whether the request for the beverage API has been made', async () => {
     renderWithRouterAndStore(<DrinkDetails match={ mockDrinkMatch } />, mockDrinkPath);
+
+    const instructions = await screen.findByTestId('instructions');
+    const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
+
+    expect(mockedSearchMealByName).toBeCalled();
+    expect(mockedSearchMealByName).toBeCalledTimes(1);
+
+    const { meals } = mealsFiltersByAll;
+    await testMealsRecipeCard(meals, maxDefaultCards, cardTestId, titleTestId);
+
+    expect(instructions).toBeInTheDocument();
+    expect(startRecipeBtn).toBeInTheDocument();
   });
 });
