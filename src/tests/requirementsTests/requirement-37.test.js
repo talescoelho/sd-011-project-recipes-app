6ae9e35920fc7baf3f0ed0d2cc47cfd31422977a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-10 11:02:30 -0300

    Add requirement-37 test

--- src/tests/requirementsTests/requirement-37.test.js
+++ src/tests/requirementsTests/requirement-37.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import { FoodDetails, DrinkDetails } from '../../pages';
+import { testMealsRecipeCard, testDrinksRecipeCard } from '../helper/testRecipeCard';
 import * as requestMenu from '../../services/requestMenu';
 import mealsFiltersByAll from '../mocks/meals/mockFilterMealsByAll';
 import mealRecipeDetails from '../mocks/meals/mockMealRecipeDetails';
 import drinksFiltersByAll from '../mocks/drinks/mockFilterDrinksByAll';
 import drinkRecipeDetails from '../mocks/drinks/mockDrinkRecipeDetails';
+
+const maxDefaultCards = 6;
+const cardTestId = '-recomendation-card';
+const titleTestId = '-recomendation-title';
 
 const mockMealPath = '/comidas/52977';
 const mockMealsMatch = { params: { id: '52977' }, url: '/comidas/52977' };
@@ -34,11 +39,19 @@ beforeEach(() => jest.clearAllMocks());
 
 describe(`37 - Implement the recommendation cards, which will be 6 cards, but showing 
 only 2 and the scroll is horizontal, similar to a carousel`, () => {
-  it('Checks if there are any recommendations in the details screen of a food', () => {
-    renderWithRouterAndStore(<FoodDetails match={ mockMealsMatch } />, mockMealPath);
-  });
+  it('Checks if there are any recommendations in the details screen of a food',
+    async () => {
+      renderWithRouterAndStore(<FoodDetails match={ mockMealsMatch } />, mockMealPath);
 
-  it('Check if there are any recommendations in the details screen of a drink', () => {
-    renderWithRouterAndStore(<DrinkDetails match={ mockDrinkMatch } />, mockDrinkPath);
-  });
+      const { drinks } = drinksFiltersByAll;
+      await testDrinksRecipeCard(drinks, maxDefaultCards, cardTestId, titleTestId);
+    });
+
+  it('Check if there are any recommendations in the details screen of a drink',
+    async () => {
+      renderWithRouterAndStore(<DrinkDetails match={ mockDrinkMatch } />, mockDrinkPath);
+
+      const { meals } = mealsFiltersByAll;
+      await testMealsRecipeCard(meals, maxDefaultCards, cardTestId, titleTestId);
+    });
 });
