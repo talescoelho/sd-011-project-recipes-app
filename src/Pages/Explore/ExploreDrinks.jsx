14868c5f1a28c2bf95c4107f8d763c09f7c99f81
marcoswarmling <marcoswarmling15@gmail.com>
2021-08-10 16:13:43 -0300

    requisitos 70 a 74 terminados

--- src/Pages/Explore/ExploreDrinks.jsx
+++ src/Pages/Explore/ExploreDrinks.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import FooterBar from '../Components/FooterBar';
 
 function ExploreDrinks() {
+  const history = useHistory();
+  const [drinkId, setDrinkId] = React.useState();
+
+  async function getRandomMealAPI() {
+    const linkDrinkCategory = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const awaitDrink = await fetch(linkDrinkCategory);
+    const awaitDrinkToJSON = await awaitDrink.json();
+    setDrinkId(awaitDrinkToJSON.drinks[0].idDrink);
+  }
+
+  React.useEffect(() => {
+    getRandomMealAPI();
+  }, []);
+
   return (
     <>
       <div>
@@ -12,6 +27,24 @@ function ExploreDrinks() {
           src={ profileIcon }
           alt="Botão que direciona para a tela de perfil"
         />
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => history.push(`/bebidas/${drinkId}`) }
+        >
+          Me Surpreenda!
+        </button>
+
       </div>
       <FooterBar />
     </>
--- src/Pages/Explore/ExploreFoods.jsx
+++ src/Pages/Explore/ExploreFoods.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import FooterBar from '../Components/FooterBar';
 
 function ExploreFoods() {
+  const [MealId, setMealId] = React.useState();
+
+  const history = useHistory();
+
+  async function getRandomMealAPI() {
+    const linkFoodCategory = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const awaitFood = await fetch(linkFoodCategory);
+    const awaitFoodToJSON = await awaitFood.json();
+    setMealId(awaitFoodToJSON.meals[0].idMeal);
+  }
+
+  React.useEffect(() => {
+    getRandomMealAPI();
+  }, []);
+
   return (
     <>
       <div>
@@ -12,6 +28,29 @@ function ExploreFoods() {
           src={ profileIcon }
           alt="Botão que direciona para a tela de perfil"
         />
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explorar/comidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+          onClick={ () => history.push('/explorar/comidas/area') }
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => history.push(`/comidas/${MealId}`) }
+        >
+          Me Surpreenda!
+        </button>
       </div>
       <FooterBar />
     </>
