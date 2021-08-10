693d637a23e2709861b9347bc6ee1bfb5c8d30c7
guinhoanjos <iago.anjos@hotmail.com>
2021-08-09 21:50:46 -0300

    csszin no card

--- src/components/DoneRecipesRender.js
+++ src/components/DoneRecipesRender.js
@@ -36,47 +36,51 @@ function DoneRecipesRender({ filter }) {
     <div>
       {copyLink ? <p>Link copiado!</p> : null}
       {recipes.map((recipe, index) => (
-        <div key={ index }>
+        <div key={ index } className="cartao">
           <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <Image
+            <img
+              className="imagem"
               alt="imagem da receita"
               src={ recipe.image }
               data-testid={ `${index}-horizontal-image` }
             />
           </Link>
           <div>
-            {recipe.tags.map((tag) => (
-              <span
-                key={ tag }
-                data-testid={ `${index}-${tag}-horizontal-tag` }
-              >
-                {tag}
-              </span>
-            ))}
+
+            <div>
+              {recipe.tags.map((tag) => (
+                <span
+                  key={ tag }
+                  data-testid={ `${index}-${tag}-horizontal-tag` }
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+            </Link>
+            {recipe.type === 'comida'
+              ? (
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  {`${recipe.area} - ${recipe.category}`}
+                </span>
+              ) : (
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  {recipe.alcoholicOrNot}
+                </span>
+              )}
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+            <input
+              data-recipetype={ recipe.type }
+              onClick={ (event) => copyUrl(event) }
+              data-recipeId={ recipe.id }
+              alt="botao de compartilhar"
+              type="image"
+              src={ shareIcon }
+              data-testid={ `${index}-horizontal-share-btn` }
+            />
           </div>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
-          </Link>
-          {recipe.type === 'comida'
-            ? (
-              <span data-testid={ `${index}-horizontal-top-text` }>
-                {`${recipe.area} - ${recipe.category}`}
-              </span>
-            ) : (
-              <span data-testid={ `${index}-horizontal-top-text` }>
-                {recipe.alcoholicOrNot}
-              </span>
-            )}
-          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-          <input
-            data-recipetype={ recipe.type }
-            onClick={ (event) => copyUrl(event) }
-            data-recipeId={ recipe.id }
-            alt="botao de compartilhar"
-            type="image"
-            src={ shareIcon }
-            data-testid={ `${index}-horizontal-share-btn` }
-          />
         </div>
       ))}
     </div>
