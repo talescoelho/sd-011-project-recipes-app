d9b313642b55cdcc8b79194330c620ab997e7fb5
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-09 21:14:02 -0300

    Remove comentario

--- src/Pages/Components/ShareButton.jsx
+++ src/Pages/Components/ShareButton.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import copy from 'clipboard-copy';
 import shareIcon from '../../images/shareIcon.svg';
-// O parâmetro recebido em ShareButton, simboliza quando é hora de usar esse componente
-// Na parte de receitas finalizadas ou receitas favoritadas.
-// Ela recebe como parâmetro um true e um id, que são originado de receitas-feitas ou recetas-favoritas.
 
 function ShareButton() {
   const { id } = useParams();
