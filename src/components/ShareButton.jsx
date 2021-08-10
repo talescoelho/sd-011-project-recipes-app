a4c9ffb7a915180ee7573a77ea541f1d89a09da9
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-10 17:47:54 -0300

    Adicionada logica do btn share. Trabalhando condicional da mensagem Link copiado!

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
 
 const copy = require('clipboard-copy');
 
 function ShareButton() {
-  function copyURL() {
+  const URL = useLocation().pathname;
+  const [click, setClick] = useState(false);
 
+  function copyURL() {
+    copy(URL);
+    setClick(true);
   }
+
   return (
-    <input
-      type="image"
-      src={ shareIcon }
-      alt="Icone share"
-      data-testid="share-btn"
-      onClick={ copyURL }
-    />
+    <>
+      <input
+        type="image"
+        src={ shareIcon }
+        alt="Icone share"
+        data-testid="share-btn"
+        onClick={ copyURL }
+      />
+      <span>{click ? <p>Link copiado!</p> : null}</span>
+    </>
   );
   // button.addEventListener('click', function () {
   //   copy('This is some cool text')
