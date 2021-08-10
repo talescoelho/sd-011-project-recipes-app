46ed384fe3076719e05d6b5069398b997e453602
Bruno Duarte <brunoarduarte@gmail.com>
2021-08-10 17:05:48 -0300

    Corrigindo erros de lint

--- src/pages/InProgress/DrinkInProgress.jsx
+++ src/pages/InProgress/DrinkInProgress.jsx
@@ -2,23 +2,20 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function toList(line) {
-  const ingredientList = new Array(20).fill().map((_, i) => {
-    const ingredientKey = `strIngredient${i + 1}`;
-    const measureKey = `strMeasure${i + 1}`;
-    console.table({ ingredientKey: line[ingredientKey], measureKey: line[measureKey] });
-    return [line[ingredientKey], line[measureKey]];
-  }).filter(([ingredient, measure]) => {
-    if (ingredient && measure) {
-      return [ingredient, measure];
-    }
-  });
-  return { ...line, ingredientList };
-}
-
 function DrinkInProgress({ match }) {
   const { id } = match.params;
   const [ingredients, setIngredients] = useState([]);
+  const [checked, setChecked] = useState({});
+
+  useEffect(() => {
+    const cached = localStorage.getItem('checkings');
+    const parsed = JSON.parse(cached);
+    if (parsed) setChecked(parsed);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('checkings', JSON.stringify(checked));
+  }, [checked]);
 
   useEffect(() => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
@@ -28,34 +25,50 @@ function DrinkInProgress({ match }) {
       });
   }, [id]);
 
+  function toList(line) {
+    const magicNumber = 20;
+    const ingredientList = new Array(magicNumber).fill().map((_, i) => {
+      const ingredientKey = `strIngredient${i + 1}`;
+      const measureKey = `strMeasure${i + 1}`;
+      return [line[ingredientKey], line[measureKey]];
+    }).filter(([ingredient, measure]) => {
+      if (ingredient && measure) {
+        return [ingredient, measure];
+      }
+      return null;
+    });
+    return { ...line, ingredientList };
+  }
+
   return (
     <div>
       {ingredients && ingredients.map(toList)
-        .map(({ ingredientList, ...drink }, index) => {
-          if (ingredientList) {
-            return (
-              <div key={ index }>
-                <img data-testid="recipe-photo" src={ drink.strDrinkThumb } alt="" />
-                <p data-testid="recipe-title">{drink.strMeal}</p>
-                <p data-testid="recipe-category">{drink.strCategory}</p>
-                <button type="button" data-testid="share-btn">share</button>
-                <button type="button" data-testid="favorite-btn">favorite</button>
-                {ingredientList.map(([ingredient, measure], i) => (
-                  <div key={ i } data-testid={ `${i}-ingredient-step` }>
-                    <span>{ingredient}</span>
-                    :
-                    <span>{ measure }</span>
-                    <input type="checkbox" />
-                  </div>
-                ))}
-                <p>instructions:</p>
-                <p data-testid="instructions">{drink.strInstructions}</p>
-                <Link to="/receitas-feitas" data-testid="finish-recipe-btn">finish</Link>
-                <br />
+        .map(({ ingredientList, ...drink }, index) => (
+          <div key={ index }>
+            <img data-testid="recipe-photo" src={ drink.strDrinkThumb } alt="" />
+            <p data-testid="recipe-title">{drink.strMeal}</p>
+            <p data-testid="recipe-category">{drink.strCategory}</p>
+            <button type="button" data-testid="share-btn">share</button>
+            <button type="button" data-testid="favorite-btn">favorite</button>
+            {ingredientList.map(([ingredient, measure], i) => (
+              <div key={ i } data-testid={ `${i}-ingredient-step` }>
+                <span>{ingredient}</span>
+                :
+                <span>{ measure }</span>
+                <input
+                  type="checkbox"
+                  checked={ checked[i.toString()] }
+                  onClick={ (event) => setChecked({ ...checked,
+                    [i]: event.target.checked }) }
+                />
               </div>
-            );
-          }
-        })}
+            ))}
+            <p>instructions:</p>
+            <p data-testid="instructions">{drink.strInstructions}</p>
+            <Link to="/receitas-feitas" data-testid="finish-recipe-btn">finish</Link>
+            <br />
+          </div>
+        ))}
     </div>
   );
 }
--- src/pages/InProgress/FoodInProgress.jsx
+++ src/pages/InProgress/FoodInProgress.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function toList(line) {
-  const ingredientList = new Array(20).fill().map((_, i) => {
+  const magicNumber = 20;
+  const ingredientList = new Array(magicNumber).fill().map((_, i) => {
     const ingredientKey = `strIngredient${i + 1}`;
     const measureKey = `strMeasure${i + 1}`;
     console.table({ ingredientKey: line[ingredientKey], measureKey: line[measureKey] });
@@ -12,6 +13,7 @@ function toList(line) {
     if (ingredient && measure) {
       return [ingredient, measure];
     }
+    return null;
   });
   return { ...line, ingredientList };
 }
@@ -19,6 +21,17 @@ function toList(line) {
 function FoodInProgress({ match }) {
   const { id } = match.params;
   const [ingredients, setIngredients] = useState([]);
+  const [added, setAdd] = useState({});
+
+  useEffect(() => {
+    const cached = localStorage.getItem('adding');
+    const parsed = JSON.parse(cached);
+    if (parsed) setAdd(parsed);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('adding', JSON.stringify(added));
+  }, [added]);
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
@@ -32,31 +45,32 @@ function FoodInProgress({ match }) {
   return (
     <div>
       {ingredients && ingredients.map(toList)
-        .map(({ ingredientList, ...meal }, index) => {
-          if (ingredientList) {
-            return (
-              <div key={ index }>
-                <img data-testid="recipe-photo" src={ meal.strMealThumb } alt="" />
-                <p data-testid="recipe-title">{meal.strMeal}</p>
-                <p data-testid="recipe-category">{meal.strCategory}</p>
-                <button type="button" data-testid="share-btn">share</button>
-                <button type="button" data-testid="favorite-btn">favorite</button>
-                {ingredientList.map(([ingredient, measure], i) => (
-                  <div key={ i } data-testid={ `${i}-ingredient-step` }>
-                    <span>{ingredient}</span>
-                    :
-                    <span>{ measure }</span>
-                    <input type="checkbox" />
-                  </div>
-                ))}
-                <p>instructions:</p>
-                <p data-testid="instructions">{meal.strInstructions}</p>
-                <Link to="/receitas-feitas" data-testid="finish-recipe-btn">finish</Link>
-                <br />
+        .map(({ ingredientList, ...meal }, index) => (
+          <div key={ index }>
+            <img data-testid="recipe-photo" src={ meal.strMealThumb } alt="" />
+            <p data-testid="recipe-title">{meal.strMeal}</p>
+            <p data-testid="recipe-category">{meal.strCategory}</p>
+            <button type="button" data-testid="share-btn">share</button>
+            <button type="button" data-testid="favorite-btn">favorite</button>
+            {ingredientList.map(([ingredient, measure], i) => (
+              <div key={ i } data-testid={ `${i}-ingredient-step` }>
+                <span>{ingredient}</span>
+                :
+                <span>{ measure }</span>
+                <input
+                  type="checkbox"
+                  checked={ added[i.toString()] }
+                  onClick={ (event) => setAdd({ ...added,
+                    [i]: event.target.checked }) }
+                />
               </div>
-            );
-          }
-        })}
+            ))}
+            <p>instructions:</p>
+            <p data-testid="instructions">{meal.strInstructions}</p>
+            <Link to="/receitas-feitas" data-testid="finish-recipe-btn">finish</Link>
+            <br />
+          </div>
+        ))}
     </div>
   );
 }
