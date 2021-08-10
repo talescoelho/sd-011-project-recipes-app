27b458ff5cada1b872d9b9763a574532c6ec1b64
Reginara Gomes <refagomes@gmail.com>
2021-08-10 16:05:56 -0300

    Adiciona requisitos 70 e 71

--- src/Pages/ExploreDrinks.js
+++ src/Pages/ExploreDrinks.js
@@ -6,6 +6,18 @@ export default function ExploreDrinks() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
--- src/Pages/ExploreFoods.js
+++ src/Pages/ExploreFoods.js
@@ -6,6 +6,24 @@ export default function ExploreFood() {
   return (
     <div>
       <Header title="Explorar Comidas" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        data-testid="explore-by-area"
+        type="button"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
