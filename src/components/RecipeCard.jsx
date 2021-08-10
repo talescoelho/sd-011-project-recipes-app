46bad5e7bec249f35fa5463c8c94fc5d54dd4072
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-08-09 23:04:41 -0300

    implementa estilizaçao nas paginas principais

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Card, Button, Badge } from 'react-bootstrap';
+import copy from 'clipboard-copy';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import './styles/details.css';
+import MyContext from '../context/MyContext';
 
-export default function RecipeCard() {
+function RecipeCard(product) {
+  const {
+    id,
+    type,
+    area,
+    category,
+    alcoholicOrNot,
+    name,
+    image,
+  } = product;
+
+  const {
+    removeLocalStorage,
+  } = useContext(MyContext);
+
+  const { pathname } = useLocation();
+  const [copied, setCopied] = useState('');
+
+  const shareRecipe = () => {
+    copy(`http://localhost:3000${pathname}`);
+    setCopied('Link copiado!');
+  };
+
   return (
-    <div />
+    <Card>
+      <Card.Img
+        src={ image }
+        alt={ `A very Good Looking ${name}` }
+        data-testid="recipe-photo"
+      />
+      <Card.Body>
+        <Card.Title data-testid="recipe-title" className="detail-title">
+          { name }
+        </Card.Title>
+        <Badge
+          bg="info"
+          text="dark"
+          data-testid="recipe-category"
+        >
+          {
+            type === 'comidas'
+              ? (`${alcoholicOrNot}`)
+              : (`${category} ${area}`)
+          }
+        </Badge>
+      </Card.Body>
+      <Button variant="success" type="button" onClick={ () => shareRecipe() }>
+        <img data-testid="share-btn" src={ shareIcon } alt="Icon Share" />
+      </Button>
+      {copied && <p>{copied}</p>}
+      <Button
+        variant="danger"
+        type="button"
+        onClick={ () => removeLocalStorage(id) }
+      >
+        <img
+          data-testid="favorite-btn"
+          src={ blackHeartIcon }
+          alt="Icon Like"
+        />
+      </Button>
+    </Card>
   );
 }
+export default RecipeCard;
