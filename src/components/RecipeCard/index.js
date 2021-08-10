67b109119092d22359fecee2c3b45ee40f88afaa
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-10 18:41:27 -0300

    adiciona requisito 32

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 class RecipeCard extends React.Component {
+  constructor() {
+    super();
+
+    this.handleRecipeClick = this.handleRecipeClick.bind(this);
+  }
+
+  handleRecipeClick() {
+    const { history, type, recipe } = this.props;
+    const id = recipe.idMeal || recipe.idDrink;
+
+    history.push(`/${type}/${id}`);
+  }
+
   render() {
     const { index, recipe } = this.props;
 
@@ -9,17 +24,36 @@ class RecipeCard extends React.Component {
     const recipeName = recipe.strMeal || recipe.strDrink;
 
     return (
-      <section data-testid={ `${index}-recipe-card` }>
-        <img src={ imageSrc } alt={ recipeName } data-testid={ `${index}-card-img` } />
-        <h1 data-testid={ `${index}-card-name` }>{recipeName}</h1>
+      <section>
+        <button
+          type="button"
+          onClick={ this.handleRecipeClick }
+          data-testid={ `${index}-recipe-card` }
+        >
+          <img
+            src={ imageSrc }
+            alt={ recipeName }
+            width="200px"
+            data-testid={ `${index}-card-img` }
+          />
+          <h1 data-testid={ `${index}-card-name` }>{recipeName}</h1>
+        </button>
       </section>
     );
   }
 }
 
-export default RecipeCard;
+const mapStateToProps = ({ recipesCategoriesReducer: { type } }) => ({
+  type,
+});
+
+export default connect(mapStateToProps)(withRouter(RecipeCard));
 
 RecipeCard.propTypes = {
   index: PropTypes.number,
   recipe: PropTypes.objectOf(PropTypes.string),
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+  type: PropTypes.string,
 }.isRequired;
