0a7453e1189b771cfcc7546c5e753282e1ea0630
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-11 17:37:14 -0300

    [Tela de Explorar] Concluida

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -11,6 +12,22 @@ function Explorer() {
   return (
     <div>
       <Header props={ headerProps } />
+      <Link to="/explorar/comidas">
+        <button
+          type="button"
+          data-testid="explore-food"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </div>
   );
