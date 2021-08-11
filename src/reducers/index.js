198bea349aad357d6428e861032c4bd75c0e4eb2
Michel Pereira <michelbecauro@gmail.com>
2021-08-11 04:34:47 -0300

    Requisito 36 - Adiciona recommendedsReducer.js e recipeDetail_actions.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import recipesReducer from './recipesReducer';
 import recipesCategoriesReducer from './recipesCategoriesReducer';
 import headerSearchReducer from './headerSearchReducer';
 import recipeDetailReducer from './recipeDetailReducer';
+import recommendedsReducer from './recommendedsReducer';
 
 const rootReducer = combineReducers({ recipesReducer,
   recipesCategoriesReducer,
   headerSearchReducer,
-  recipeDetailReducer });
+  recipeDetailReducer,
+  recommendedsReducer });
 
 export default rootReducer;
