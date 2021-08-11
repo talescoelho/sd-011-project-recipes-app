ba1edf1c1d6208260da61c58b31bd523b33d7907
benlopes <rubenssouss@hotmail.com>
2021-08-11 09:52:09 -0300

    adiciona 100% dos tests para o component login

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import App from './App';
-import Login from './Pages/Login';
+import userEvent from '@testing-library/user-event';
 
 describe('Farewell, front-end', () => {
-  // afterEach(() => {
-  //   cleanup();
-  // });
+  afterEach(() => {
+    cleanup();
+  });
 
-
-  it('should display Trybe title on rendering', () => {
+  it('should display the Login title on rendering', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/TRYBE/i);
+    const linkElement = getByText(/Login/i);
     expect(linkElement).toBeInTheDocument();
   });
 
@@ -19,9 +18,24 @@ describe('Farewell, front-end', () => {
     const { getByLabelText, getByTestId } = render(<App />);
     const emailInput = getByLabelText(/Email:/i);
     const passwordInput = getByLabelText(/Senha:/i);
-    // const btnNode = getByTestId('login-submit-btn');
+    const btnNode = getByTestId('login-submit-btn');
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
-    // expect(btnNode).toBeInTheDocument();
+    expect(btnNode).toBeInTheDocument();
+  });
+  
+  it('should enable the login button when inputs are typed', () => {
+    const { getByLabelText, getByTestId } = render(<App />);
+    const setLocalStorage = jest.fn();
+    const emailInput = getByLabelText(/Email:/i);
+    const passwordInput = getByLabelText(/Senha:/i);
+    const btnNode = getByTestId('login-submit-btn');
+    expect(btnNode).toBeDisabled();
+    const mockMail = 'jon@m.com';
+    const mockPass = '102384198';
+    userEvent.type(emailInput, mockMail);
+    userEvent.type(passwordInput, mockPass);
+    expect(btnNode).not.toBeDisabled();
+    userEvent.click(btnNode);
   });
 });
