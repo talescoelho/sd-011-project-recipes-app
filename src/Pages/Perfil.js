f882613ec926fec6ba8dc12c135e2bc399887b4d
FABIO3750 <fabnhm@hotmail.com>
2021-08-11 20:35:13 -0300

    Implementando a tela de perfil

--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Perfil({ title }) {
+function Perfil() {
+  const [userEmail, setUserEmail] = useState('');
+  useEffect(() => {
+  const email = JSON.parse(localStorage.getItem('user'));
+  console.log(email);
+    setUserEmail(email);
+  }, []);
+
   return (
     <div>
-      { console.log('rendered perfil ')}
-      <Header title={ title } />
+      <h1>Pagina de  Perfil</h1>
+      <Header title="Perfil" />
+      <p data-testid="profile-email">{userEmail}</p>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
