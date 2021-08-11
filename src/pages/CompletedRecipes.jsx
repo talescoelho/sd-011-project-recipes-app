89741cc4f2a3bc07aebefe39e3f951e213556d21
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-11 15:09:07 -0300

    initial

--- src/pages/CompletedRecipes.jsx
+++ src/pages/CompletedRecipes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
+// initial commit
 
 const CompletedRecipes = () => (
   <>
