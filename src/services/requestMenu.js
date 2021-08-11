128932188b8e116b2bd892f62ca0e5dc2a8623a7
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-11 11:47:00 -0300

    Add endpoint to search list of areas and meals by area

--- src/services/requestMenu.js
+++ src/services/requestMenu.js
@@ -47,3 +47,13 @@ export const listAllDrinksIngredients = () => (
   fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
     .then((response) => response.json())
 );
+
+export const listAllMealsAreas = () => (
+  fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+    .then((response) => response.json())
+);
+
+export const listMealsByArea = (area) => (
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
+    .then((response) => response.json())
+);
