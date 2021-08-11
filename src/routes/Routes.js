094114877a8bfd4554eccdd4dcbd77e1f1dc1456
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-08-11 17:37:46 -0300

    Corrigidos erros

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import Login from '../pages/Login';
 import Foods from '../pages/Foods';
 import Drinks from '../pages/Drinks';
