f4b34f30b43e1c776501d7bb99ce019d1ad717c9
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-08-10 23:31:42 -0300

    implementa a tela de favoritos

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,75 +1,85 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Card, Button, Badge } from 'react-bootstrap';
 import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import './styles/details.css';
-import MyContext from '../context/MyContext';
 
-function RecipeCard(product) {
+function RecipeCard(props) {
   const {
-    id,
-    type,
-    area,
-    category,
-    alcoholicOrNot,
-    name,
-    image,
-  } = product;
+    product: {
+      id,
+      type,
+      area,
+      category,
+      alcoholicOrNot,
+      name,
+      image,
+    },
+    onClickHearth,
+    index,
+  } = props;
 
-  const {
-    removeLocalStorage,
-  } = useContext(MyContext);
-
-  const { pathname } = useLocation();
   const [copied, setCopied] = useState('');
 
   const shareRecipe = () => {
-    copy(`http://localhost:3000${pathname}`);
+    copy(`http://localhost:3000/${type}s/${id}`);
     setCopied('Link copiado!');
   };
 
   return (
+
     <Card>
-      <Card.Img
-        src={ image }
-        alt={ `A very Good Looking ${name}` }
-        data-testid="recipe-photo"
-      />
-      <Card.Body>
-        <Card.Title data-testid="recipe-title" className="detail-title">
-          { name }
-        </Card.Title>
-        <Badge
-          bg="info"
-          text="dark"
-          data-testid="recipe-category"
-        >
-          {
-            type === 'comidas'
-              ? (`${alcoholicOrNot}`)
-              : (`${category} ${area}`)
-          }
-        </Badge>
-      </Card.Body>
+      <Link to={ `/${type}s/${id}` }>
+        <Card.Img
+          src={ image }
+          alt={ `A very Good Looking ${name}` }
+          data-testid={ `${index}-horizontal-image` }
+        />
+        <Card.Body>
+          <Card.Title data-testid={ `${index}-horizontal-name` } className="detail-title">
+            { name }
+          </Card.Title>
+          <Badge
+            bg="info"
+            text="dark"
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {
+              type === 'bebida'
+                ? (`${alcoholicOrNot}`)
+                : (`${area} - ${category}`)
+            }
+          </Badge>
+        </Card.Body>
+      </Link>
       <Button variant="success" type="button" onClick={ () => shareRecipe() }>
-        <img data-testid="share-btn" src={ shareIcon } alt="Icon Share" />
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          alt="Icon Share"
+        />
       </Button>
       {copied && <p>{copied}</p>}
       <Button
         variant="danger"
         type="button"
-        onClick={ () => removeLocalStorage(id) }
+        onClick={ () => onClickHearth() }
       >
         <img
-          data-testid="favorite-btn"
+          data-testid={ `${index}-horizontal-favorite-btn` }
           src={ blackHeartIcon }
           alt="Icon Like"
         />
       </Button>
     </Card>
+
   );
 }
 export default RecipeCard;
+RecipeCard.propTypes = {
+  product: PropTypes.objectOf(PropTypes.string),
+  index: PropTypes.string,
+}.isRequired;
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
 import Header from '../components/Header';
+import RecipeCard from '../components/RecipeCard';
 
-export default function ExplorerFoodsIngredients() {
+export default function FavoriteRecipes() {
+  function getFavorites() {
+    const favoritesList = localStorage.getItem('favoriteRecipes');
+    const json = JSON.parse(favoritesList);
+    return json;
+  }
+
+  const [favorites, setFavorites] = useState(getFavorites);
+
+  function removeLocalStorage(id) {
+    let favs = localStorage.getItem('favoriteRecipes');
+    const json = JSON.parse(favs);
+
+    const newjson = json.filter((recipe) => recipe.id !== id);
+
+    favs = JSON.stringify(newjson);
+    localStorage.setItem('favoriteRecipes', favs);
+
+    setFavorites(getFavorites());
+  }
+
+  function filterBy({ target: { value } }) {
+    const filteredFavorites = value === 'all'
+      ? getFavorites()
+      : favorites.filter(
+        ({ type }) => type === value,
+      );
+    setFavorites(filteredFavorites);
+  }
+
   return (
     <main>
       <Header title="Receitas Favoritas" disable />
+      <ButtonGroup>
+        <Button
+          type="button"
+          value="all"
+          onClick={ (e) => filterBy(e) }
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </Button>
+
+        <Button
+          type="button"
+          value="comida"
+          onClick={ (e) => filterBy(e) }
+          data-testid="filter-by-food-btn"
+        >
+          Comidas
+        </Button>
+
+        <Button
+          type="button"
+          value="bebida"
+          onClick={ (e) => filterBy(e) }
+          data-testid="filter-by-drink-btn"
+        >
+          Bebidas
+        </Button>
+      </ButtonGroup>
+      {
+        favorites.length > 0
+        && favorites.map(
+          (favorite, index) => (<RecipeCard
+            key={ favorite.id }
+            product={ favorite }
+            onClickHearth={ () => removeLocalStorage(favorite.id) }
+            index={ index }
+          />),
+        )
+      }
     </main>
   );
 }
