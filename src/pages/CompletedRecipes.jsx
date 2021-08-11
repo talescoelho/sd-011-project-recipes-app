5f9bf8d5a61cd05d3606bbdd031f6f197340073e
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-08-11 15:26:45 -0300

    task 54 initial structures

--- src/pages/CompletedRecipes.jsx
+++ src/pages/CompletedRecipes.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
-// initial commit
 
-const CompletedRecipes = () => (
-  <>
-    <Header
-      page="Receitas Feitas"
-      showSearchBtn={ false }
-    />
-    <div>Receitas Finalizadas</div>
-  </>
-);
+const CompletedRecipes = () => {
+  function handleClickFilter({ target: { value } }) {
+    if (value === 'comida') {
+      setFilteredRecipe(recipes.filter((recipe) => recipe.type === 'comida'));
+    } else if (value === 'bebida') {
+      setFilteredRecipe(recipes.filter((recipe) => recipe.type === 'bebida'));
+    } else {
+      setFilteredRecipe(recipes);
+    }
+  }
+
+  return (
+    <div>
+      <Header
+        page="Receitas Feitas"
+        showSearchBtn={ false }
+      />
+      <button
+        type="button"
+        value="all"
+        data-testid="filter-by-all-btn"
+        onClick={ handleClickFilter }
+      >
+        All
+      </button>
+    </div>
+  );
+};
 
 export default CompletedRecipes;
