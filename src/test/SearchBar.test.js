2dd9094941258b8c720bb3b23d3aa84c8bfaced2
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-11 11:17:30 -0700

    Fixing lint errors

--- src/test/SearchBar.test.js
+++ src/test/SearchBar.test.js
@@ -21,6 +21,11 @@ jest.spyOn(actions, 'drinkRecipesByName');
 jest.spyOn(actions, 'drinkRecipesByLetter');
 
 const history = createMemoryHistory({ initialEntries: ['/comidas'] });
+const SEARCH_INPUT = 'search-input';
+const INGREDIENT_SEARCH_RADIO = 'ingredient-search-radio';
+const NAME_SEARCH_RADIO = 'name-search-radio';
+const FIRST_LETTER_SEARCH_RADIO = 'first-letter-search-radio';
+const EXECT_SEARCH_BTN = 'exec-search-btn';
 
 describe('SearchBar que é responsável pela pesquisa da pessoa usuária', () => {
   test('Elemento de input deve estar renderizando', () => {
@@ -28,7 +33,7 @@ describe('SearchBar que é responsável pela pesquisa da pessoa usuária', () =>
       mode="comidas"
       history={ history }
     />);
-    const tapInput = getByTestId('search-input');
+    const tapInput = getByTestId(SEARCH_INPUT);
     expect(tapInput).toBeInTheDocument();
   });
   test('Elemento radio btn de ingrediente deve estar renderizando', () => {
@@ -36,31 +41,31 @@ describe('SearchBar que é responsável pela pesquisa da pessoa usuária', () =>
       mode="comidas"
       history={ history }
     />);
-    const radioBtnIngredient = getByTestId('ingredient-search-radio');
+    const radioBtnIngredient = getByTestId(INGREDIENT_SEARCH_RADIO);
     expect(radioBtnIngredient).toBeInTheDocument();
   });
   test('Elemento de input do nome deve ser renderizado', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
-    mode="comidas"
-    history={history}
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
+      mode="comidas"
+      history={ history }
     />);
-    const radioBtnName = getByTestId('name-search-radio');
+    const radioBtnName = getByTestId(NAME_SEARCH_RADIO);
     expect(radioBtnName).toBeInTheDocument();
   });
   test('Elemento de busca por primeira letra deve ser renderizado', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
-    mode="comidas"
-    history={history}
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
+      mode="comidas"
+      history={ history }
     />);
-    const radioBtnFirstLetter = getByTestId('first-letter-search-radio');
+    const radioBtnFirstLetter = getByTestId(FIRST_LETTER_SEARCH_RADIO);
     expect(radioBtnFirstLetter).toBeInTheDocument();
   });
   test('Botão de busca deve ser renderizado e a lógica deve funcionar', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
-    mode="comidas"
-    history={history}
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
+      mode="comidas"
+      history={ history }
     />);
-    const btnSearch = getByTestId('exec-search-btn');
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
     expect(btnSearch).toBeInTheDocument();
   });
 });
@@ -68,74 +73,74 @@ describe('SearchBar que é responsável pela pesquisa da pessoa usuária', () =>
 describe('Lógica do fetch do botão search', () => {
   beforeEach(() => {
     window.alert.mockClear();
-  })
+  });
   test('Com o modo comidas. Busca por ingrediente.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="comidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('ingredient-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `butter`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(INGREDIENT_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'butter' } });
     expect(tapInput).toHaveValue('butter');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.foodRecipesByIngredient).toHaveBeenCalledTimes(1);
-    expect(actions.foodRecipesByIngredient.mock.calls[0][0]).toBe(`butter`);
+    expect(actions.foodRecipesByIngredient.mock.calls[0][0]).toBe('butter');
   });
 
   test('Com o modo comidas. Busca por nome.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="comidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('name-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `noodle`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(NAME_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'noodle' } });
     expect(tapInput).toHaveValue('noodle');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.foodRecipesByName).toHaveBeenCalledTimes(1);
-    expect(actions.foodRecipesByName.mock.calls[0][0]).toBe(`noodle`);
+    expect(actions.foodRecipesByName.mock.calls[0][0]).toBe('noodle');
   });
 
   test('Com o modo comidas. Busca por primeira letra.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="comidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('first-letter-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `a`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(FIRST_LETTER_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'a' } });
     expect(tapInput).toHaveValue('a');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.foodRecipesByLetter).toHaveBeenCalledTimes(1);
-    expect(actions.foodRecipesByLetter.mock.calls[0][0]).toBe(`a`);
+    expect(actions.foodRecipesByLetter.mock.calls[0][0]).toBe('a');
   });
 
   test('Com o modo comidas. Busca por primeira letra com alerta.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="comidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('first-letter-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `ab`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(FIRST_LETTER_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'ab' } });
     expect(tapInput).toHaveValue('ab');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
@@ -145,72 +150,72 @@ describe('Lógica do fetch do botão search', () => {
   });
 
   test('Com o modo bebidas. Busca por ingrediente.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="bebidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('ingredient-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `orange`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(INGREDIENT_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'orange' } });
     expect(tapInput).toHaveValue('orange');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.drinkRecipesByIngredient).toHaveBeenCalledTimes(1);
-    expect(actions.drinkRecipesByIngredient.mock.calls[0][0]).toBe(`orange`);
+    expect(actions.drinkRecipesByIngredient.mock.calls[0][0]).toBe('orange');
   });
 
   test('Com o modo bebidas. Busca por nome.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="bebidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('name-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `moscow`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(NAME_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'moscow' } });
     expect(tapInput).toHaveValue('moscow');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.drinkRecipesByName).toHaveBeenCalledTimes(1);
-    expect(actions.drinkRecipesByName.mock.calls[0][0]).toBe(`moscow`);
+    expect(actions.drinkRecipesByName.mock.calls[0][0]).toBe('moscow');
   });
 
   test('Com o modo bebidas. Busca por primeira letra.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="bebidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('first-letter-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `a`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(FIRST_LETTER_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'a' } });
     expect(tapInput).toHaveValue('a');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
 
     fireEvent.click(btnSearch);
     expect(actions.drinkRecipesByLetter).toHaveBeenCalledTimes(1);
-    expect(actions.drinkRecipesByLetter.mock.calls[0][0]).toBe(`a`);
+    expect(actions.drinkRecipesByLetter.mock.calls[0][0]).toBe('a');
   });
 
   test('Com o modo bebidas. Busca por primeira letra com alerta.', () => {
-    const { getByTestId } = renderWithRouterAndStore(<SearchBar 
+    const { getByTestId } = renderWithRouterAndStore(<SearchBar
       mode="bebidas"
-      history={history}
-      />);
-    const tapInput = getByTestId('search-input');
-    const radioBtnIngredient = getByTestId('first-letter-search-radio');
-    const btnSearch = getByTestId('exec-search-btn');
-    
-    fireEvent.change(tapInput, { target: { value: `ab`}});
+      history={ history }
+    />);
+    const tapInput = getByTestId(SEARCH_INPUT);
+    const radioBtnIngredient = getByTestId(FIRST_LETTER_SEARCH_RADIO);
+    const btnSearch = getByTestId(EXECT_SEARCH_BTN);
+
+    fireEvent.change(tapInput, { target: { value: 'ab' } });
     expect(tapInput).toHaveValue('ab');
     fireEvent.click(radioBtnIngredient);
     expect(radioBtnIngredient.checked).toEqual(true);
@@ -218,4 +223,4 @@ describe('Lógica do fetch do botão search', () => {
     fireEvent.click(btnSearch);
     expect(window.alert).toHaveBeenCalledTimes(1);
   });
-})
+});
