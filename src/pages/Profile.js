5d83301e909b85fdb07d039790a54fa38b81d1da
Diogo Augusto <diogotrescastro@gmail.com>
2021-08-11 15:55:26 -0300

    Pagina de Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
+import LSContext from '../context/LSContext';
 
-const Profile = () => (
-  <>
-    <Header title="Perfil" />
-    <FooterMenu />
-  </>
-);
+const Profile = () => {
+  const { LSValues: { user: { email } } } = useContext(LSContext);
+  const { LSFunctions: {
+    setEmail,
+    setDoneRecipes,
+    setFavoriteRecipes,
+    setInProgressRecipes } } = useContext(LSContext);
+
+  const userEmail = typeof (email) === 'string' ? email : '';
+
+  function resetContext() {
+    localStorage.clear();
+    setEmail('');
+    setDoneRecipes([]);
+    setFavoriteRecipes([]);
+    setInProgressRecipes({ cocktails: {}, meals: {} });
+  }
+
+  return (
+    <>
+      <Header title="Perfil" />
+      <section>
+        <h3 data-testid="profile-email">{ userEmail }</h3>
+        <Link to="/receitas-feitas" data-testid="profile-done-btn">
+          Receitas Feitas
+        </Link>
+        <br />
+        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
+          Receitas Favoritas
+        </Link>
+        <br />
+        <Link to="/">
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={ () => resetContext() }
+          >
+            Sair
+          </button>
+        </Link>
+      </section>
+      <FooterMenu />
+    </>
+  );
+};
 
 export default Profile;
