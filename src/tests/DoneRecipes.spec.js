47dc16c4b79fbadd89ac34ae5b72f7eb08658e09
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-08-12 11:05:41 -0300

    Resolvido 6k + problemas de lint

--- src/tests/DoneRecipes.spec.js
+++ src/tests/DoneRecipes.spec.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
+import fetchData from './mocks/fetch';
 
+const mockFetch = () => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementation((url) => fetchData(url));
+};
