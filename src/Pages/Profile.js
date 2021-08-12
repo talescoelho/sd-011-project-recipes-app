b50b86a1cfe659a8d67e1675dab97f157335848c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-08-12 07:47:13 -0300

    alteraçao de span para input no elemento e-mail

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -9,16 +9,23 @@ function Profile() {
   return (
     <div>
       <Header title="Perfil" />
-      <span data-testid="profile-email">{userEmail.email}</span>
+      <input
+        type="email"
+        value={ userEmail.email }
+        style={ { margin: 10 } }
+      />
       <button
         type="submit"
         data-testid="profile-done-btn"
+        style={ { margin: 5 } }
       >
         <Link to="/receitas-feitas">Receitas Feitas</Link>
       </button>
       <button
         type="submit"
         data-testid="profile-favorite-btn"
+        style={ { margin: 5 } }
+
       >
         <Link to="/receitas-favoritas">Receitas Favoritas</Link>
       </button>
@@ -26,6 +33,7 @@ function Profile() {
         type="submit"
         data-testid="profile-logout-btn"
         onClick={ () => localStorage.clear() }
+        style={ { margin: 5 } }
       >
         <Link to="/">Sair</Link>
       </button>
