6e79435fcee8189e8ca3b482ee0a43c982f2032d
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-12 18:17:57 -0300

    70-71-72-73 done

--- src/Pages/Explorer/Drinks/ExplorerDrinks.jsx
+++ src/Pages/Explorer/Drinks/ExplorerDrinks.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LowerMenu from '../../../Components/footer/LowerMenu';
 import HeaderExplore from '../../../Components/headers/HeaderExploreDrinks';
 
 const ExplorerDrinks = () => (
   <div>
     <HeaderExplore />
+    <section>
+      <Link to="/explorar/bebidas/ingredientes">
+        <button type="button" data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+      </Link>
+    </section>
     <footer>
       <LowerMenu />
     </footer>
--- src/Pages/Explorer/Foods/ExplorerFoods.jsx
+++ src/Pages/Explorer/Foods/ExplorerFoods.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderExploreFoods from '../../../Components/headers/HeaderExploreFoods';
 import LowerMenu from '../../../Components/footer/LowerMenu';
 
 const ExplorerFoods = () => (
   <div>
     <HeaderExploreFoods />
+    <section>
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
+    </section>
     <footer>
       <LowerMenu />
     </footer>
