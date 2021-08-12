913f6fb526057673b87430e3cef3cf525399e548
Renan <renan_menezes07@hotmail.com>
2021-08-12 16:44:10 -0300

    Resoluçao do requisito 79.

--- src/Pages/ExploreMealsByArea.js
+++ src/Pages/ExploreMealsByArea.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import requestByMeal, { requestArea } from '../Services/Data';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -7,6 +8,7 @@ import searchIcon from '../images/searchIcon.svg';
 function ExploreMealsByArea() {
   const [listOfAreas, setListOfAreas] = React.useState('');
   const [listOfMeals, setListOfMeals] = React.useState([]);
+  const [listOfFilteredMeals, setListOfFilteredMeals] = React.useState(listOfMeals);
 
   useEffect(() => {
     (async function resolve() {
@@ -14,8 +16,15 @@ function ExploreMealsByArea() {
       const mealList = await requestByMeal();
       setListOfAreas(areaList.meals);
       setListOfMeals(mealList.meals);
+      setListOfFilteredMeals(mealList.meals);
     }());
   }, []);
+
+  async function filterMealsByArea({ target }) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${target.value}`);
+    const newMeals = await response.json();
+    setListOfFilteredMeals(newMeals.meals);
+  }
 
   const numberOfMealCards = 12;
 
@@ -27,22 +36,33 @@ function ExploreMealsByArea() {
         src={ searchIcon }
         alt="imagem lupa"
       />
-      <select data-testid="explore-by-area-dropdown">
+      <select
+        data-testid="explore-by-area-dropdown"
+        onChange={ (e) => filterMealsByArea(e) }
+      >
         {Array.from(listOfAreas)
           .map(({ strArea }) => (
-            <option key={ strArea } data-testid={ `${strArea}-option` }>{strArea}</option>
+            <option
+              key={ strArea }
+              data-testid={ `${strArea}-option` }
+              value={ strArea }
+            >
+              {strArea}
+            </option>
           ))}
       </select>
       <section>
-        {Array.from(listOfMeals).slice(0, numberOfMealCards)
-          .map(({ strMealThumb, strMeal }, index) => (
+        {listOfFilteredMeals.slice(0, numberOfMealCards)
+          .map(({ idMeal, strMealThumb, strMeal }, index) => (
             <div key={ strMeal } data-testid={ `${index}-recipe-card` }>
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ strMealThumb }
-                alt="imagem da comida"
-              />
-              <h4 data-testid={ `${index}-card-name` }>{strMeal}</h4>
+              <Link to={ `/comidas/${idMeal}` }>
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ strMealThumb }
+                  alt="imagem da comida"
+                />
+                <h4 data-testid={ `${index}-card-name` }>{strMeal}</h4>
+              </Link>
             </div>
           ))}
       </section>
