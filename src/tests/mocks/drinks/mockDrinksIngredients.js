f0b6cc7be7032b01c43eca6d3beb38e9e10dfd5f
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-12 08:47:44 -0300

    Add requirement-75 test

---
+++ src/tests/mocks/drinks/mockDrinksIngredients.js
@@ -0,0 +1,66 @@
+const drinksIngredients = {
+  drinks: [
+    {
+      strIngredient1: 'Light rum',
+    },
+    {
+      strIngredient1: 'Applejack',
+    },
+    {
+      strIngredient1: 'Gin',
+    },
+    {
+      strIngredient1: 'Dark rum',
+    },
+    {
+      strIngredient1: 'Sweet Vermouth',
+    },
+    {
+      strIngredient1: 'Strawberry schnapps',
+    },
+    {
+      strIngredient1: 'Scotch',
+    },
+    {
+      strIngredient1: 'Apricot brandy',
+    },
+    {
+      strIngredient1: 'Triple sec',
+    },
+    {
+      strIngredient1: 'Southern Comfort',
+    },
+    {
+      strIngredient1: 'Orange bitters',
+    },
+    {
+      strIngredient1: 'Brandy',
+    },
+    {
+      strIngredient1: 'Lemon vodka',
+    },
+    {
+      strIngredient1: 'Blended whiskey',
+    },
+    {
+      strIngredient1: 'Dry Vermouth',
+    },
+    {
+      strIngredient1: 'Amaretto',
+    },
+    {
+      strIngredient1: 'Tea',
+    },
+    {
+      strIngredient1: 'Champagne',
+    },
+    {
+      strIngredient1: 'Coffee liqueur',
+    },
+    {
+      strIngredient1: 'Bourbon',
+    },
+  ],
+};
+
+export default drinksIngredients;
