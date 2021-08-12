595ed0d76526ce29ae51e20d7f5bbda7efddd8f9
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-08-12 16:18:44 -0300

    Tela explorar origem - Concluida

--- src/Components/FilterAreaExplore.jsx
+++ src/Components/FilterAreaExplore.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function FilterAreaExplore() {
   const [areas, setAreas] = useState([]);
-  console.log(areas);
+  const [optionValue, setOptionValue] = useState('All');
+  const [recipes, setRecipes] = useState([]);
+  const twelve = 12;
+  const url = optionValue === 'All'
+    ? 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+    : `https://www.themealdb.com/api/json/v1/1/filter.php?a=${optionValue}`;
 
   useEffect(() => {
     const fetchArea = async () => {
@@ -14,9 +20,56 @@ export default function FilterAreaExplore() {
     fetchArea();
   }, []);
 
+  useEffect(() => {
+    const fetchArea = async () => {
+      const requisition = await fetch(url);
+      const { meals } = await requisition.json();
+      setRecipes(meals);
+    };
+
+    fetchArea();
+  }, [url]);
+
   return (
     <div>
-      <select>Canada</select>
+      <select
+        data-testid="explore-by-area-dropdown"
+        onChange={ ({ target }) => setOptionValue(target.value) }
+      >
+        <option
+          data-testid="All-option"
+        >
+          All
+        </option>
+        {areas.map((area, index) => (
+          <option
+            key={ index }
+            data-testid={ `${area.strArea}-option` }
+          >
+            {area.strArea}
+          </option>
+        ))}
+      </select>
+      {recipes && recipes
+        .filter((_, index) => index < twelve)
+        .map((recipe, index) => (
+          <Link to={ `/comidas/${recipe.idMeal}` } key={ index }>
+            <div data-testid={ `${index}-recipe-card` } className="cardRecipesContainer">
+              <img
+                src={ recipe.strMealThumb }
+                alt={ recipe.strMealThumb }
+                className="recipesImg"
+                data-testid={ `${index}-card-img` }
+              />
+              <p
+                data-testid={ `${index}-card-name` }
+              >
+                {recipe.strMeal}
+              </p>
+            </div>
+          </Link>
+
+        ))}
     </div>
   );
 }
