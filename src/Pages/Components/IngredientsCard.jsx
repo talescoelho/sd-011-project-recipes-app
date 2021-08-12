2b4c8d83c333d2706c4bdefd7bc7e25aec8388a7
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-12 15:58:13 -0300

    Implementa componente do requisito 75

--- src/Pages/Components/IngredientsCard.jsx
+++ src/Pages/Components/IngredientsCard.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export default function IngredientsCard({ingredient}) {
-    const [IngredientImageId, setIngredientImageId] = React.useState();
+export default function IngredientsCard({ ingredient }) {
+  // const [IngredientImageId, setIngredientImageId] = React.useState();
 
-    async function IngredientsFoodImageApi() {
-        const linkIngredientImage = `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}.png`;
-        const awaitIngredient = await fetch(linkIngredientImage);
-        const awaitIngredientToJSON = await awaitIngredient.json();
-        setIngredientImageId(awaitIngredientToJSON)
-      }
-   
-      React.useEffect(() => {
-        IngredientsFoodImageApi();
-      }, []);
+  // async function IngredientsFoodImageApi() {
+  //   const linkIngredientImage = `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}.png`;
+  //   const awaitIngredient = await fetch(linkIngredientImage);
+  //   const awaitIngredientToJSON = await awaitIngredient.json();
+  //   setIngredientImageId(awaitIngredientToJSON);
+  // }
 
-      
-    return (
-        <div>
-            <img src={IngredientImageId ? IngredientImageId : null} alt="" />
-            <div>{ingredient.strIngredient}</div>
+  // React.useEffect(() => {
+  //   IngredientsFoodImageApi();
+  // }, []);
 
-        </div>
-    )
+  return (
+    <div data-testid={ `${index}-ingredient-card` }>
+      <img /* src={ IngredientImageId || null } alt="" */
+        data-testid={ `${index}-card-img` }
+        alt=""
+      />
+      <div>
+        <p data-testid={ `${index}-card-name` }>Nome do card</p>
+      </div>
+    </div>
+  );
 }
