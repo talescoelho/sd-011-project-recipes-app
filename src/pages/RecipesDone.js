5c9c7648ca4a7674bf9280cd4ac78bf65f0c1a93
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-08-12 19:29:52 -0300

    adiciona requisito 56

--- src/pages/RecipesDone.js
+++ src/pages/RecipesDone.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import MealDoneCard from '../components/MealDoneCard';
 import RecipeDoneCard from '../components/RecipeDoneCard';
 import { getFromStorage } from '../helpers/utils';
 
@@ -19,9 +18,7 @@ function RecipesDone() {
         <section>
           {
             recipesDone && recipesDone.map((recipe, index) => (
-              recipe.type === 'comida'
-                ? <MealDoneCard key={ index } recipe={ recipe } count={ index } />
-                : <RecipeDoneCard key={ index } recipe={ recipe } count={ index } />
+              <RecipeDoneCard key={ index } recipe={ recipe } count={ index } />
             ))
           }
         </section>
