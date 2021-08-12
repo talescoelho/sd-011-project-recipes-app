e4534d123a7b18442c317b04e4a4200e578b5d68
Marcos Mantovani <mvmes23@gmail.com>
2021-08-12 17:12:11 -0300

    att 17:11PM 12/08

--- src/pages/DrinksRecipiesInProcess.js
+++ src/pages/DrinksRecipiesInProcess.js
@@ -14,23 +14,21 @@ class DrinksRecipiesInProcess extends React.Component {
     };
     this.test = this.test.bind(this);
     this.changeRow = this.changeRow.bind(this);
-    this.renderAll = this.renderAll.bind(this);
   }
 
   componentDidMount() {
     this.test();
   }
 
-  componentDidUpdate() {
-    // if (localStorage.inProgressRecipes) {
-    //   const { match: { params: { id } } } = this.props;
-    //   const recoveredInfo = JSON.parse(localStorage.inProgressRecipes);
-    //   const ote = Object.entries(recoveredInfo);
+  // componentDidUpdate() {
+  //   if (localStorage.inProgressRecipes) {
 
-    //   console.log(ote);
-    //   // console.log(recoveredInfo);
-    // }
-  }
+  //     const recoveredInfo = JSON.parse(localStorage.inProgressRecipes).cocktails[id];
+  //     // this.setStateFunc(recoveredInfo);
+  //     console.log(recoveredInfo);
+  //     // console.log(recoveredInfo);
+  //   }
+  // }
 
   async test() {
     const { match: { params: { id } } } = this.props;
@@ -41,44 +39,57 @@ class DrinksRecipiesInProcess extends React.Component {
     });
   }
 
-  changeRow(event, index) {
+  changeRow(event, index, name) {
     const { stockDrinks } = this.state;
     let filter = [];
-    if (stockDrinks.some((i) => i === index)) {
-      filter = stockDrinks.filter((ell) => ell !== index);
+    if (stockDrinks.some((i) => i === name)) {
+      filter = stockDrinks.filter((ell) => ell !== name);
     } else {
-      filter = [...stockDrinks, index];
+      filter = [...stockDrinks, name];
     }
     this.setState({
       stockDrinks: filter,
     }, () => {
       const { stockDrinks: newStockDrinks } = this.state;
       const { match: { params: { id } } } = this.props;
-      const drinks = {
+
+      const drinks2 = {
         cocktails: {
           [id]: newStockDrinks,
         },
       };
       if (localStorage.inProgressRecipes) {
         if (JSON.parse(localStorage.inProgressRecipes).cocktails) {
-          const prev = JSON.parse(localStorage.inProgressRecipes).cocktails;
-          localStorage.inProgressRecipes = JSON.stringify(...prev, drinks);
+          const prev = JSON.parse(localStorage.inProgressRecipes);
+          console.log(prev);
+          const drinks = {
+            cocktails: {
+              ...prev.cocktails,
+              [id]: newStockDrinks,
+            },
+          };
+          localStorage.inProgressRecipes = JSON.stringify(drinks);
         }
-      } else { localStorage.inProgressRecipes = JSON.stringify(drinks); }
+      } else { localStorage.inProgressRecipes = JSON.stringify(drinks2); }
     });
     event.target.parentNode.classList.toggle('do-row');
   }
 
   renderAll() {
     const { DoRecipe } = this.state;
+    let recoveredInfo = [];
+    if (localStorage.inProgressRecipes) {
+      const { match: { params: { id } } } = this.props;
+      recoveredInfo = JSON.parse(localStorage.inProgressRecipes).cocktails[id];
+    }
     return (
       <div>
-        <h1 data-testid="recipe-title">{ DoRecipe.drinks[0].strDrink }</h1>
         <img
           src={ DoRecipe.drinks[0].strDrinkThumb }
           alt={ DoRecipe.drinks[0].strDrink }
           data-testid="recipe-photo"
         />
+        <h1 data-testid="recipe-title">{ DoRecipe.drinks[0].strDrink }</h1>
         <ShareBtn />
         <FavoriteBtn />
         <p data-testid="recipe-category">{ DoRecipe.drinks[0].strCategory }</p>
@@ -89,20 +100,21 @@ class DrinksRecipiesInProcess extends React.Component {
                 .includes('strIngredient') && igredients[1])
               .map((e, index) => (
                 <li
-                  // className={}
+                  id={ index }
                   data-testid={ `${index}-ingredient-step` }
                   key={ index }
                 >
                   <label
-                    className="li-test"
+                    className={ recoveredInfo.some((item) => item === e[1]) ? 'do-row' : '' }
                     id={ `id1${index}` }
                     htmlFor={ `for${index}` }
                   >
                     {e[1]}
                     <input
+                      checked={ recoveredInfo.some((item) => item === e[1]) }
                       id={ `for${index}` }
                       type="checkbox"
-                      onClick={ (event) => this.changeRow(event, index) }
+                      onClick={ (event) => this.changeRow(event, index, e[1]) }
                     />
                   </label>
                 </li>
