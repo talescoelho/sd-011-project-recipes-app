bbbbf57d6eb134e6474b6e823495f606d6777ecf
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-08-13 18:04:31 -0300

    Corrige alteraçoes req 10 & 11

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -4,11 +4,39 @@ import HeaderPerfil from '../Components/headers/HeaderPerfil';
 import LowerMenu from '../Components/footer/LowerMenu';
 
 const User = () => {
-  const { email } = JSON.parse(localStorage.getItem('user'));
-  return (
+  if (localStorage.getItem('user')) {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    return (
+      <div>
+        <HeaderPerfil />
+        <p data-testid="profile-email">{ email }</p>
+        <div>
+          <Link to="/receitas-feitas">
+            <button type="button" data-testid="profile-done-btn">
+              Receitas Feitas
+            </button>
+          </Link>
+          <Link to="/receitas-favoritas">
+            <button type="button" data-testid="profile-favorite-btn">
+              Receitas Favoritas
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="profile-logout-btn"
+              onClick={ () => localStorage.clear() }
+            >
+              Sair
+            </button>
+          </Link>
+        </div>
+        <LowerMenu />
+      </div>
+    );
+  } return (
     <div>
       <HeaderPerfil />
-      <p data-testid="profile-email">{ email }</p>
       <div>
         <Link to="/receitas-feitas">
           <button type="button" data-testid="profile-done-btn">
