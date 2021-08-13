b48ed38a5ff27029932a922dcb738da5cb6c8e9e
jamneto <netomoura22@gmail.com>
2021-08-13 18:21:21 -0300

    criaçao de funçoes de fetch para comidas e bebidas aleatorias

--- src/pages/ExploreDynamic.jsx
+++ src/pages/ExploreDynamic.jsx
@@ -1,34 +1,46 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreDynamic({ type }) {
   const showSearchButton = false;
-  if (type.includes('omida')) {
-    return (
-      <div>
-        <Header pageName={ type } showSearchButton={ showSearchButton } />
+  const drinkOrFood = type.includes('ebida') ? 'bebidas' : 'comidas';
+  // function randomRedirect() {
+
+  // }
+  return (
+    <div>
+      <Header pageName={ type } showSearchButton={ showSearchButton } />
+      <Link to={ `/explorar/${drinkOrFood}/ingredientes` }>
         <button
           type="button"
           data-testid="explore-by-ingredient"
         >
-          {' '}
           Por Ingredientes
         </button>
-        <Footer />
-      </div>
-    );
-  }
-  return (
-    <div>
-      <Header pageName={ type } showSearchButton={ showSearchButton } />
-      { type }
-      <Footer />
+      </Link>
+      { (type.includes('omida')) && (
+        <Link to={ `/explorar/${drinkOrFood}/area` }>
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+      ) }
+      <button
+        type="button"
+        testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
+      <Footer color={ type.includes('ebida') ? '#a73d7e' : null } />
     </div>
   );
 }
-
 ExploreDynamic.propTypes = {
   type: PropTypes.string.isRequired,
 };
