d592bc2863ff283fdb0104a581693189ac564ff1
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-08-13 15:10:42 -0300

    Feature: Tela de Perfil

--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -6,44 +6,44 @@ import Footer from '../components/Footer';
 function Perfil() {
   const [userEmail, setUserEmail] = useState('');
   useEffect(() => {
-    const email = JSON.parse(localStorage.getItem('user'));
-    console.log(email);
+    const email = JSON.parse(localStorage.getItem('user')) || { email: 'Se você chegou aqui até aqui, parabéns' };
     setUserEmail(email);
   }, []);
 
   return (
     <div>
-      <h1>Pagina de  Perfil</h1>
       <Header title="Perfil" />
-      <p data-testid="profile-email">{userEmail}</p>
-      <Link to="/receitas-feitas">
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Receitas Feitas
-        </button>
-      </Link>
+      <div>
+        <p data-testid="profile-email">{userEmail.email}</p>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
 
-      <Link to="/receitas-favoritas">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Receitas Favoritas
-        </button>
-      </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
 
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => localStorage.clear() }
-        >
-          Sair
-        </button>
-      </Link>
-      <Footer />
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Sair
+          </button>
+        </Link>
+        <Footer />
+      </div>
     </div>
   );
 }
