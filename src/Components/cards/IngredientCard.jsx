b35f4a55103fb89752255211d569a24aefcc0d68
Gisele Costa <jikinha@gmail.com>
2021-08-13 18:43:47 -0300

    flex

--- src/Components/cards/IngredientCard.jsx
+++ src/Components/cards/IngredientCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function IngredientCard(props) {
   const { ingredientsList, drink } = props;
@@ -13,20 +14,30 @@ export default function IngredientCard(props) {
       return (
         ingredientsListRender.filter((__, index) => index < maxListRender)
           .map((ingredient, index) => (
-            <div key={ index }>
-              <div key={ index } data-testid={ `${index}-ingredient-card` }>
-                <h5
-                  data-testid={ `${index}-card-name` }
+            <Link
+              key={ index }
+              to={ { pathname: '/comidas', ingredient: ingredient.strIngredient1 } }
+            >
+              <div key={ index }>
+                <div
+                  key={ index }
+                  data-testid={ `${index}-ingredient-card` }
+                  style={{display: "flex"}}
                 >
-                  {ingredient.strIngredient1}
-                </h5>
-                <img
-                  src={ `${imgDrink}${ingredient.strIngredient1}-Small.png` }
-                  alt={ ingredient.strIngredient1 }
-                  data-testid={ `${index}-card-img` }
-                />
+                  <h5
+                    data-testid={ `${index}-card-name` }
+                  >
+                    {ingredient.strIngredient1}
+                  </h5>
+                  <img
+                    src={ `${imgDrink}${ingredient.strIngredient1}-Small.png` }
+                    alt={ ingredient.strIngredient1 }
+                    data-testid={ `${index}-card-img` }
+                    style={{width: "30px"}}
+                  />
+                </div>
               </div>
-            </div>
+            </Link>
           ))
       );
     }
@@ -34,16 +45,26 @@ export default function IngredientCard(props) {
       return (
         ingredientsListRender.filter((__, index) => index < maxListRender)
           .map((ingredient, index) => (
-            <div key={ index }>
-              <div key={ index } data-testid={ `${index}-ingredient-card` }>
-                <h5 data-testid={ `${index}-card-name` }>{ingredient.strIngredient}</h5>
-                <img
-                  src={ `${imgFood}${ingredient.strIngredient}-Small.png` }
-                  alt={ ingredient.strIngredient }
-                  data-testid={ `${index}-card-img` }
-                />
+            <Link
+              key={ index }
+              to={ { pathname: '/comidas', ingredient: ingredient.strIngredient1 } }
+            >
+              <div key={ index }>
+                <div
+                  key={ index }
+                  data-testid={ `${index}-ingredient-card` }
+                  style={{display: "flex"}}
+                >
+                  <h5 data-testid={ `${index}-card-name` }>{ingredient.strIngredient}</h5>
+                  <img
+                    src={ `${imgFood}${ingredient.strIngredient}-Small.png` }
+                    alt={ ingredient.strIngredient }
+                    data-testid={ `${index}-card-img` }
+                    style={{width: "30px"}}
+                  />
+                </div>
               </div>
-            </div>
+            </Link>
           ))
       );
     }
