12baafcccbf909cf5aa3c188bbc30043004833ac
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-13 16:05:34 -0300

    Estrutura do Footer pronta

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+import recipesContext from '../provider/recipesContext';
+
+import '../styles/Footer.css';
+
+function Footer() {
+  const { setType, setUpdate } = useContext(recipesContext);
+  return (
+    <div
+      className="footer-wrapper"
+      data-testid="footer"
+    >
+      <Link
+        to="/bebidas"
+        onClick={ () => {
+          setType('cocktail');
+          setUpdate(true);
+        } }
+      >
+        <button type="button">
+          <img
+            data-testid="drinks-bottom-btn"
+            src={ drinkIcon }
+            alt="bebidas"
+          />
+        </button>
+      </Link>
+      <Link
+        to="/explorar"
+        onClick={ () => setUpdate(true) }
+      >
+        <button type="button">
+          <img
+            data-testid="explore-bottom-btn"
+            src={ exploreIcon }
+            alt="bebidas"
+          />
+        </button>
+      </Link>
+      <Link
+        to="/comidas"
+        onClick={ () => {
+          setType('meal');
+          setUpdate(true);
+        } }
+      >
+        <button type="button">
+          <img
+            data-testid="food-bottom-btn"
+            src={ mealIcon }
+            alt="bebidas"
+          />
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default Footer;
