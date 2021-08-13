bfb9551b0cac7d8978e220d950406a981c54a1cb
Diego Ribeiro <dihrm2001@gmail.com>
2021-08-13 15:43:10 -0300

    Estrutura da pagina de login pronta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import RecipesProvider from './provider/recipesProvider';
 import Login from './pages/Login';
 import TelaPrincipal from './pages/TelaPrincipal';
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import useLoginForm from '../hooks/useLoginForm';
 
+import '../styles/Login.css';
+
 export default function Login() {
   // Faz uso do hook personalizado
   const [redirect, valid, handleChange, handleSubmit] = useLoginForm();
 
   return (
-    <div>
+    <div className="login-page">
       { redirect ? <Redirect to="/comidas" /> : null }
-      <form>
+      <h1 className="login-title">Login</h1>
+      <form className="login-form">
         <input
           data-testid="email-input"
           type="email"
           name="email"
+          className="login-input"
           onKeyUp={ handleChange }
           placeholder="Insira seu email"
         />
@@ -21,12 +25,14 @@ export default function Login() {
           data-testid="password-input"
           type="password"
           name="password"
+          className="login-input"
           onKeyUp={ handleChange }
           placeholder="Insira sua senha"
         />
         <button
           data-testid="login-submit-btn"
           type="button"
+          className="btn"
           disabled={ !valid }
           onClick={ handleSubmit }
         >
