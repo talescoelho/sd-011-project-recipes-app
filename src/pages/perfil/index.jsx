cd1f1e2a314844b02d25512a7c5685706bee09db
Amandha W. Barok <amandha.barok@gmail.com>
2021-08-13 16:57:42 -0700

    Finishing the page

--- src/pages/perfil/index.jsx
+++ src/pages/perfil/index.jsx
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
-export default class Perfil extends Component {
-  render() {
-    return (
-      <div>
-        <Header title="Perfil" />
-        Perfil
-        <Footer />
-      </div>
-    );
-  }
+export default function Perfil() {
+  let user = localStorage.getItem('user');
+  user = JSON.parse(user);
+
+  return (
+    <div>
+      <Header title="Perfil" />
+      <p data-testid="profile-email">{user.email}</p>
+      <Link to="receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="receitas-favoritas">
+        <button type="button" data-testid="profile-favorite-btn">
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
+      <Footer />
+    </div>
+  );
 }
+// npm run cy:open --spec cypress/integration/profile_spec.js
