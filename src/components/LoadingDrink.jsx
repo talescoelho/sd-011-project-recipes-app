b9a50bc20509f2ff78125520a4e82a9750458029
Felipe Buso <felipe_buso@hotmail.com>
2021-08-13 16:27:13 -0300

    fix continuaçao

--- src/components/LoadingDrink.jsx
+++ src/components/LoadingDrink.jsx
@@ -3,7 +3,7 @@ import '../styles/LoadingDrink.css';
 
 export default function LoadingDrink() {
   return (
-    <div className="loading-Drink">
+    <div className="loading-drink">
       {/* <img src={ loadingMeal } alt="gif" /> */}
       <h2>bebidas...</h2>
     </div>
