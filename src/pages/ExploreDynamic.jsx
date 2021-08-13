85465e869dafaac39b81bc6e989ab6c7cb272ec8
jamneto <netomoura22@gmail.com>
2021-08-13 18:01:59 -0300

    começando a pagina de explorar

--- src/pages/ExploreDynamic.jsx
+++ src/pages/ExploreDynamic.jsx
@@ -5,6 +5,21 @@ import Header from '../components/Header';
 
 export default function ExploreDynamic({ type }) {
   const showSearchButton = false;
+  if (type.includes('omida')) {
+    return (
+      <div>
+        <Header pageName={ type } showSearchButton={ showSearchButton } />
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          {' '}
+          Por Ingredientes
+        </button>
+        <Footer />
+      </div>
+    );
+  }
   return (
     <div>
       <Header pageName={ type } showSearchButton={ showSearchButton } />
