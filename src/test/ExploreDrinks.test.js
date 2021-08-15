83d8fb968e81a8075eb513ca9e28f4168551a512
Savio Moraes <smmsavio@hotmail.com>
2021-08-15 18:43:48 -0300

    fix linter error

--- src/test/ExploreDrinks.test.js
+++ src/test/ExploreDrinks.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+// import { fireEvent } from '@testing-library/react';
+// import { createMemoryHistory } from 'history';
 import ExplorarBebidas from '../pages/explorar/bebidas';
 import renderWithRouterAndStore from './testConfig';
 
-const history = createMemoryHistory({ initialEntries: ['/'] });
+// const history = createMemoryHistory({ initialEntries: ['/'] });
 
 describe('Testa a página ExplorarBebidas', () => {
   it('Tem o botão Por Ingredientes', () => {
@@ -27,7 +27,7 @@ describe('Testa a página ExplorarBebidas', () => {
   });
 
   it('Redireciona para a página BebidasIngredientes', () => {
-    const { history } = renderWithRouterAndStore(<ExplorarBebidas />, history);
+    // const { history } = renderWithRouterAndStore(<ExplorarBebidas />, history);
     const buttons = getAllByRole('button');
     expect(buttons[0].innerHTML).toBe('Por Ingredientes');
     expect(buttons[1].innerHTML).toBe('Me Surpreenda');
