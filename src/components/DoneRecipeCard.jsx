1174948d3263fa3a663ff71bef59d0baa6592d78
Matheus Costa <mathws.costag@gmail.com>
2021-08-15 18:54:39 -0300

    Lint

--- src/components/DoneRecipeCard.jsx
+++ src/components/DoneRecipeCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import ShareButton from './ShareButton';
 
 export default function DoneRecipeCard({ recipe, index }) {
