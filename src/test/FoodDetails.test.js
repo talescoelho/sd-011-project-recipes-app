08336137bed97a824e3999826662f0d8465ca606
Víctor Faria <vitilevi@hotmail.com>
2021-08-15 17:42:49 -0300

    Iniciado mock do storage

--- src/test/FoodDetails.test.js
+++ src/test/FoodDetails.test.js
@@ -26,12 +26,28 @@ const INSTRUCTIONS = 'instructions';
 const VIDEO_SRC = 'video';
 const START_RECIPE_BTN = 'start-recipe-btn';
 const INGREDIENTS = ['0', '1', '2', '3', '4', '5', '6', '7'];
-const RECOMENDATIONS = ['0', '1', '2', '3', '4', '5', '6'];
+const RECOMENDATION0 = '0-recomendation-card';
+const RECOMENDATION1 = '1-recomendation-card';
+const RECOMENDATION2 = '2-recomendation-card';
+const RECOMENDATION3 = '3-recomendation-card';
+const RECOMENDATION4 = '4-recomendation-card';
+const RECOMENDATION5 = '5-recomendation-card';
+const IN_PROGRESS_PATH = '/comidas/52771/in-progress';
 let MEAL_PATH = '/comidas/52771';
 // const CHECKBOX0 = '0-checkbox';
 
 const testHistory = createMemoryHistory({ initialEntries: [MEAL_PATH] });
 describe('Testa a página de detalhes da receita', () => {
+  const store = {
+    inProgressRecipes: {
+      cocktails: {},
+      meals: {
+        52771: [0],
+      },
+    },
+  };
+  spyOn(localStorage, 'getItem').mockImplementation((key) => store[key]);
+
   beforeEach(() => {
     MEAL_PATH = '/comidas/52771';
   });
@@ -45,30 +61,30 @@ describe('Testa a página de detalhes da receita', () => {
   });
 
   it('Testa se os elementos corretos estão todos na tela', async () => {
-    await act(async () => {
-      renderWithRouter(<FoodDetails match={ match } />, testHistory);
-    });
-    const ingredientsString = await screen.findByText(/Ingredientes/i);
-    const img = await screen.findByTestId(PHOTO_TEST_ID);
-    const title = await screen.findByTestId(RECIPE_TITLE);
-    const shareBtn = await screen.findByTestId(SHARE_BTN);
-    const favBtn = await screen.findByTestId(FAV_BTN);
-    const category = await screen.findByTestId(RECIPE_CATEGORY);
-    const instructions = await screen.findByTestId(INSTRUCTIONS);
-    const videoSrc = await screen.findByTestId(VIDEO_SRC);
-    const startBtn = await screen.findByTestId(START_RECIPE_BTN);
+    const {
+      findByTestId,
+      findByText,
+    } = renderWithRouter(<FoodDetails match={ match } />, testHistory);
+    const ingredientsString = await findByText(/Ingredientes/i);
+    const img = await findByTestId(PHOTO_TEST_ID);
+    const title = await findByTestId(RECIPE_TITLE);
+    const shareBtn = await findByTestId(SHARE_BTN);
+    const favBtn = await findByTestId(FAV_BTN);
+    const category = await findByTestId(RECIPE_CATEGORY);
+    const instructions = await findByTestId(INSTRUCTIONS);
+    const videoSrc = await findByTestId(VIDEO_SRC);
+    const startBtn = await findByTestId(START_RECIPE_BTN);
+    const recomendation0 = await findByTestId(RECOMENDATION0);
+    const recomendation1 = await findByTestId(RECOMENDATION1);
+    const recomendation2 = await findByTestId(RECOMENDATION2);
+    const recomendation3 = await findByTestId(RECOMENDATION3);
+    const recomendation4 = await findByTestId(RECOMENDATION4);
+    const recomendation5 = await findByTestId(RECOMENDATION5);
 
     await act(async () => {
       INGREDIENTS.forEach(async (el) => {
         const ingredient = await screen.findByTestId(`${el}-ingredient-name-and-measure`);
         expect(ingredient).toBeInTheDocument();
-      });
-    });
-
-    act(() => {
-      RECOMENDATIONS.forEach(async (el) => {
-        const recomendation = await screen.findByTestId(`${el}-recomendation-card`);
-        expect(recomendation).toBeInTheDocument();
       });
     });
 
@@ -82,6 +98,12 @@ describe('Testa a página de detalhes da receita', () => {
     expect(category).toBeInTheDocument();
     expect(instructions).toBeInTheDocument();
     expect(videoSrc).toBeInTheDocument();
+    expect(recomendation0).toBeInTheDocument();
+    expect(recomendation1).toBeInTheDocument();
+    expect(recomendation2).toBeInTheDocument();
+    expect(recomendation3).toBeInTheDocument();
+    expect(recomendation4).toBeInTheDocument();
+    expect(recomendation5).toBeInTheDocument();
     expect(startBtn).toBeInTheDocument();
     // expect(mockOneMeal).toBeCalled();
   });
@@ -97,12 +119,13 @@ describe('Testa a página de detalhes da receita', () => {
 
       fireEvent.click(startBtnBefore);
 
-      expect(history.location.pathname).toEqual('/comidas/52771/in-progress');
-      MEAL_PATH = '/comidas/52771/in-progress';
-      const el = container.querySelectorAll('input');
-      console.log(el[0]);
-      // const checkBox = await findByTestId('0-checkbox');
-      // fireEvent(checkBox);
+      expect(history.location.pathname).toEqual(IN_PROGRESS_PATH);
+      history.push(IN_PROGRESS_PATH);
+      MEAL_PATH = IN_PROGRESS_PATH;
+      // const el = container.querySelectorAll('input');
+      // console.log(el[0]);
+      const checkBox = await findByTestId('0-checkbox');
+      fireEvent(checkBox);
       // const x = findByTestId('finish-recipe-btn');
       // await wait(() => expect(x).toBeInTheDocument());
       // console.log(Array.of(container.parentNode));
