7983bd76fc5d089a5ddf0ba7bfd38ceb5c51042f
pmaiamateus <pmaiamateus@gmail.com>
2021-08-15 20:01:33 -0300

    requisito 70

--- src/pages/ExplorerDrinks.js
+++ src/pages/ExplorerDrinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,16 @@ export default function ExplorerDrinks() {
   return (
     <main>
       <Header haveSearchBtn={ false } title="Explorar Bebidas" />
+      <Link to="/explorar/comidas/ingredientes">
+        <button type="button" data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-surprise">
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </main>
   );
--- src/pages/ExplorerFoods.js
+++ src/pages/ExplorerFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,23 @@ export default function ExplorerFoods() {
   return (
     <main>
       <Header haveSearchBtn={ false } title="Explorar Comidas" />
+      <div>
+        <Link to="/explorar/comidas/ingredientes">
+          <button type="button" data-testid="explore-by-ingredient">
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-by-area">
+            Explorar Bebidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-surprise">
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
     </main>
   );
