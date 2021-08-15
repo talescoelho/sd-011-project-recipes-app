62e86cd9c0215ee0c2cf7531bae59c304e8c64d6
pmaiamateus <pmaiamateus@gmail.com>
2021-08-15 19:47:37 -0300

    requisito 85 e 86

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
   const email = Object.values(JSON.parse(localStorage.getItem('user')))[0];
-  console.log(email)
   return (
     <main>
       <Header haveSearchBtn={ false } title="Perfil" />
@@ -12,15 +12,21 @@ export default function Profile() {
         <p data-testid="profile-email">{ email }</p>
       </div>
       <div>
-        <button type="button" data-testid="profile-done-btn">
-          Receitas Feitas
-        </button>
-        <button type="button" data-testid="profile-favorite-btn">
-          Receitas Favoritas
-        </button>
-        <button type="button" data-testid="profile-logout-btn">
-          Sair
-        </button>
+        <Link to="/receitas-feitas">
+          <button type="button" data-testid="profile-done-btn">
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button type="button" data-testid="profile-favorite-btn">
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button type="button" data-testid="profile-logout-btn">
+            Sair
+          </button>
+        </Link>
       </div>
       <Footer />
     </main>
