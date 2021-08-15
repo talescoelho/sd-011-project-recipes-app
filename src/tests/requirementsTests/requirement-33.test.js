5447217e79f0f4b23ce366a542d6e1ba42232f3b
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-15 10:03:18 -0300

    Add requirement-33 teste

--- src/tests/requirementsTests/requirement-33.test.js
+++ src/tests/requirementsTests/requirement-33.test.js
@@ -8,9 +8,6 @@ import drinksFiltersByAll from '../mocks/drinks/mockFilterDrinksByAll';
 import drinkRecipeDetails from '../mocks/drinks/mockDrinkRecipeDetails';
 import App from '../../App';
 
-const mockMealPath = '/comidas/52977';
-const mockDrinkPath = '/bebidas/15997';
-
 jest
   .spyOn(requestMenu, 'searchMealByName')
   .mockImplementation(() => Promise.resolve(mealsFiltersByAll));
@@ -33,30 +30,50 @@ beforeEach(() => jest.clearAllMocks());
 describe(`33 - Implement the elements of a recipe details screen respecting the
 attributes described in the prototype`, () => {
   it('The food screen has all data-testid attributes.', async () => {
-    renderWithRouterAndStore(<App />, { route: mockMealPath });
+    renderWithRouterAndStore(<App />, { route: '/comidas/52977' });
 
-    const instructions = await screen.findByTestId('instructions');
-    const recipeVideo = await screen.findByTestId('video');
-    const recomendationCard = await screen.findByTestId('0-recomendation-card');
-    const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
-
-    expect(instructions).toBeInTheDocument();
-    expect(recipeVideo).toBeInTheDocument();
-    expect(recomendationCard).toBeInTheDocument();
-    expect(startRecipeBtn).toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-photo'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-title'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('share-btn'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('favorite-btn'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-category'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('0-ingredient-name-and-measure'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('instructions'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('video'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('0-recomendation-card'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('start-recipe-btn'))
+      .toBeInTheDocument();
   });
 
   it('The drinks screen has all data-tested attributes.', async () => {
-    renderWithRouterAndStore(<App />, { route: mockDrinkPath });
+    renderWithRouterAndStore(<App />, { route: '/bebidas/15997' });
 
-    const instructions = await screen.findByTestId('instructions');
-    const recipeVideo = screen.queryByTestId('video');
-    const recomendationCard = await screen.findByTestId('0-recomendation-card');
-    const startRecipeBtn = await screen.findByTestId('start-recipe-btn');
-
-    expect(instructions).toBeInTheDocument();
-    expect(recipeVideo).not.toBeInTheDocument();
-    expect(recomendationCard).toBeInTheDocument();
-    expect(startRecipeBtn).toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-photo'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-title'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('share-btn'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('favorite-btn'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('recipe-category'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('0-ingredient-name-and-measure'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('instructions'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('0-recomendation-card'))
+      .toBeInTheDocument();
+    expect(await screen.findByTestId('start-recipe-btn'))
+      .toBeInTheDocument();
   });
 });
