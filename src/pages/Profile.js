ebae9173fccf9aaf176c1f1780b1d00894494116
pmaiamateus <pmaiamateus@gmail.com>
2021-08-15 19:42:17 -0300

    requisitos 82 e 83

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,25 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const email = Object.values(JSON.parse(localStorage.getItem('user')))[0];
+  console.log(email)
   return (
     <main>
       <Header haveSearchBtn={ false } title="Perfil" />
+      <div>
+        <p data-testid="profile-email">{ email }</p>
+      </div>
+      <div>
+        <button type="button" data-testid="profile-done-btn">
+          Receitas feitas
+        </button>
+        <button type="button" data-testid="profile-favorite-btn">
+          Receitas favoritas
+        </button>
+        <button type="button" data-testid="profile-logout-btn">
+          log out
+        </button>
+      </div>
       <Footer />
     </main>
   );
