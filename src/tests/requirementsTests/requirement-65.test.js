2b2d716c689b11ac3d84145fc9e496e3bedc0061
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-16 10:31:09 -0300

    Remove unnecessary mock for requirement 58 test and add test requirement 65

--- src/tests/requirementsTests/requirement-65.test.js
+++ src/tests/requirementsTests/requirement-65.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
 import App from '../../App';
 
-const doneRecipes = [
+const favoriteRecipes = [
   {
     id: '52771',
     type: 'comida',
@@ -12,8 +12,6 @@ const doneRecipes = [
     alcoholicOrNot: '',
     name: 'Spicy Arrabiata Penne',
     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
   },
   {
     id: '178319',
@@ -23,25 +21,23 @@ const doneRecipes = [
     alcoholicOrNot: 'Alcoholic',
     name: 'Aquamarine',
     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
   },
 ];
 
 beforeEach(() => {
-  localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+  localStorage.setItem('doneRecipes', JSON.stringify(favoriteRecipes));
 });
 
 afterEach(() => {
   localStorage.clear();
 });
 
-const pathname = '/receitas-feitas';
+const pathname = '/receitas-favoritas';
 const firstHorizontalName = '0-horizontal-name';
 const secondHorizontalName = '1-horizontal-name';
 
-describe(`58 - Implement 2 buttons that filter recipes by food or drink and a third one
-that removes all filters`, () => {
+describe(`65 - Implement 2 buttons that filter recipes by food or drink and a third
+one that removes all filters`, () => {
   it('By clicking on the "Food" button the recipes must be filtered by food',
     async () => {
       renderWithRouterAndStore(<App />, { route: pathname });
@@ -49,7 +45,7 @@ that removes all filters`, () => {
       fireEvent.click(await screen.findByTestId('filter-by-food-btn'));
 
       expect(await screen.findByTestId(firstHorizontalName))
-        .toHaveTextContent(doneRecipes[0].name);
+        .toHaveTextContent(favoriteRecipes[0].name);
       expect(screen.queryByTestId(secondHorizontalName)).toBeNull();
     });
 
@@ -60,19 +56,20 @@ that removes all filters`, () => {
       fireEvent.click(await screen.findByTestId('filter-by-drink-btn'));
 
       expect(await screen.findByTestId(firstHorizontalName))
-        .toHaveTextContent(doneRecipes[1].name);
+        .toHaveTextContent(favoriteRecipes[1].name);
       expect(screen.queryByTestId(secondHorizontalName)).toBeNull();
     });
 
-  it('By clicking on the "All" button the filter must be removed', async () => {
-    renderWithRouterAndStore(<App />, { route: pathname });
+  it('By clicking on the "All" button the filter must be removed',
+    async () => {
+      renderWithRouterAndStore(<App />, { route: pathname });
 
-    fireEvent.click(await screen.findByTestId('filter-by-food-btn'));
-    fireEvent.click(await screen.findByTestId('filter-by-all-btn'));
+      fireEvent.click(await screen.findByTestId('filter-by-food-btn'));
+      fireEvent.click(await screen.findByTestId('filter-by-all-btn'));
 
-    expect(await screen.findByTestId(firstHorizontalName))
-      .toHaveTextContent(doneRecipes[0].name);
-    expect(await screen.findByTestId(secondHorizontalName))
-      .toHaveTextContent(doneRecipes[1].name);
-  });
+      expect(await screen.findByTestId(firstHorizontalName))
+        .toHaveTextContent(favoriteRecipes[0].name);
+      expect(await screen.findByTestId(secondHorizontalName))
+        .toHaveTextContent(favoriteRecipes[1].name);
+    });
 });
