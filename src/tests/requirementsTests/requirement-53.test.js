5ec44aeed14e6a8f1467da8e3b1a667bdc1ad170
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-16 09:30:37 -0300

    Add requirement-53 test

--- src/tests/requirementsTests/requirement-53.test.js
+++ src/tests/requirementsTests/requirement-53.test.js
@@ -1,16 +1,65 @@
 import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithRouterAndStore } from '../helper/testConfig';
+import * as requestMenu from '../../services/requestMenu';
+import mealRecipeDetails from '../mocks/meals/mockMealRecipeDetails';
+import drinkRecipeDetails from '../mocks/drinks/mockDrinkRecipeDetails';
 import App from '../../App';
+
+jest
+  .spyOn(requestMenu, 'mealsRecipeDetails')
+  .mockImplementation(() => Promise.resolve(mealRecipeDetails));
+
+jest
+  .spyOn(requestMenu, 'drinksRecipeDetails')
+  .mockImplementation(() => Promise.resolve(drinkRecipeDetails));
+
+afterEach(() => {
+  jest.clearAllMocks();
+  localStorage.clear();
+});
+beforeEach(() => {
+  jest.clearAllMocks();
+  localStorage.clear();
+});
 
 describe(`53 - Redirect the user after clicking the "Finish recipe" button to the 
 recipes made page, whose route should be /recipes-made`, () => {
   it('Redirects after completing a food recipe',
-    () => {
-      renderWithRouterAndStore(<App />, { route: '/comidas/52977/in-progress' });
+    async () => {
+      const { history } = renderWithRouterAndStore(
+        <App />, { route: '/comidas/52977/in-progress' },
+      );
+
+      const ingredientStep = await screen.findAllByTestId('ingredient-step');
+
+      ingredientStep.forEach((input) => {
+        fireEvent.click(input);
+        expect(input).toBeChecked();
+      });
+
+      fireEvent.click(await screen.findByTestId('finish-recipe-btn'));
+
+      const { location: { pathname } } = history;
+      expect(pathname).toBe('/receitas-feitas');
     });
 
   it('Redirects after completing a drink recipe',
-    () => {
-      renderWithRouterAndStore(<App />, { route: '/bebidas/15997/in-progress' });
+    async () => {
+      const { history } = renderWithRouterAndStore(
+        <App />, { route: '/bebidas/15997/in-progress' },
+      );
+
+      const ingredientStep = await screen.findAllByTestId('ingredient-step');
+
+      ingredientStep.forEach((input) => {
+        fireEvent.click(input);
+        expect(input).toBeChecked();
+      });
+
+      fireEvent.click(await screen.findByTestId('finish-recipe-btn'));
+
+      const { location: { pathname } } = history;
+      expect(pathname).toBe('/receitas-feitas');
     });
 });
